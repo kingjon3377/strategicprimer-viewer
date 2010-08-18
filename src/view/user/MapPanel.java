d80048eb04061a88d3362f37ee8db3c8e6d89f4d
Jonathan Lovelace <kingjon3377@gmail.com>
2010-08-17 20:39:13 -0400

    Warning fixes

--- src/view/user/MapPanel.java
+++ src/view/user/MapPanel.java
@@ -29,40 +29,40 @@ public class MapPanel extends JPanel {
 	/**
 	 * Constructor.
 	 * 
-	 * @param map
+	 * @param _map
 	 *            The map object this panel is representing
 	 */
-	public MapPanel(final SPMap map) {
+	public MapPanel(final SPMap _map) {
 		super();
 		selListener = new SelectionListener();
-		loadMap(map);
+		loadMap(_map);
 	}
 
 	/**
 	 * Load and draw a map
 	 * 
-	 * @param map
+	 * @param _map
 	 *            the map to load
 	 */
-	public final void loadMap(final SPMap map) {
-		if (map != null) {
+	public final void loadMap(final SPMap _map) {
+		if (_map != null) {
 			final Logger LOGGER = Logger.getLogger(MapPanel.class
 					.getName());
 			removeAll();
-			setLayout(new GridLayout(map.rows(), 0));
-			for (int row = 0; row < map.rows(); row++) {
-				for (int col = 0; col < map.cols(); col++) {
-					if (map.getTile(row, col) == null) {
+			setLayout(new GridLayout(_map.rows(), 0));
+			for (int row = 0; row < _map.rows(); row++) {
+				for (int col = 0; col < _map.cols(); col++) {
+					if (_map.getTile(row, col) == null) {
 						addTile(new NullGUITile(row, col)); // NOPMD
 					} else {
-						addTile(new GUITile(map.getTile(row, col))); // NOPMD
+						addTile(new GUITile(_map.getTile(row, col))); // NOPMD
 					}
 				}
 				LOGGER.fine("Added row ");
 				LOGGER.fine(Integer.toString(row));
 				LOGGER.fine("\n");
 			}
-			this.map = map;
+			this.map = _map;
 		}
 	}
 
