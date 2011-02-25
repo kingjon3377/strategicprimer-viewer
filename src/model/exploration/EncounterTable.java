60d94479956d2196194c801b6613a81fd502aba8
Jonathan Lovelace <kingjon3377@gmail.com>
2011-02-24 20:01:11 -0500

    An interface for all kinds of encounter tables.

---
+++ src/model/exploration/EncounterTable.java
@@ -0,0 +1,23 @@
+package model.exploration;
+
+import model.viewer.Tile;
+
+/**
+ * An interface for encounter tables, both quadrant and random-event tables. At
+ * present we assume this is for the Judge's use; to produce output a player can
+ * see unmodified we need to be able to know the explorer's Perception modifier
+ * and perhaps other data.
+ * 
+ * @author Jonathan Lovelace
+ * 
+ */
+public interface EncounterTable {
+	/**
+	 * Generates an "encounter." For QuadrantTables this is always the same for
+	 * each tile; for random event tables the result will be randomly selected
+	 * from that table.
+	 * @param tile a tile
+	 * @return an appropriate event for that tile 
+	 */
+	String generateEvent(final Tile tile);
+}
