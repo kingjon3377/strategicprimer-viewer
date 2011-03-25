8e5d429914a1e97f53d33508a9abe4de76912eab
Jonathan Lovelace <kingjon3377@gmail.com>
2011-03-25 17:00:29 -0400

    Added a TODO/FIXME item to the comment header.

--- src/model/exploration/ExplorationRunner.java
+++ src/model/exploration/ExplorationRunner.java
@@ -17,6 +17,10 @@ import controller.exploration.TableLoader;
  * A class to create exploration results. The initial implementation is a bit
  * hackish, and should be generalized and improved.
  * 
+ * FIXME: So that this could eventually run in a server environment, make
+ * everything here non-static, so we can have multiple instances with different
+ * data.
+ * 
  * @author Jonathan Lovelace
  * 
  */
