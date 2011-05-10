583ebf4ffa1defbd99008bd1a5296af8622e668b
Jonathan Lovelace <kingjon3377@gmail.com>
2011-05-09 23:16:46 -0400

    Remove unused import.

--- src/view/map/main/ViewerFrame.java
+++ src/view/map/main/ViewerFrame.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
