2dd27340ce23722cc1ab1829af8cea8af55754f5
Jonathan Lovelace <kingjon3377@gmail.com>
2011-05-23 16:33:02 -0400

    Move Point to its own file; this fixes the 'too-many-methods' warning on SPMap.

---
+++ src/model/viewer/Point.java
@@ -0,0 +1,63 @@
+package model.viewer;
+
+/**
+ * A structure encapsulating two coordinates.
+ * 
+ * @author Jonathan Lovelace
+ * 
+ */
+public class Point {
+	/**
+	 * The first coordinate.
+	 */
+	private final int myRow;
+	/**
+	 * The second coordinate.
+	 */
+	private final int myCol;
+
+	/**
+	 * @return the first coordinate.
+	 */
+	public final int row() {
+		return myRow;
+	}
+
+	/**
+	 * @return the second coordinate.
+	 */
+	public final int col() {
+		return myCol;
+	}
+
+	/**
+	 * Constructor.
+	 * 
+	 * @param row
+	 *            The first coordinate
+	 * @param col
+	 *            The second coordinate
+	 */
+	public Point(final int row, final int col) {
+		myRow = row;
+		myCol = col;
+	}
+
+	/**
+	 * @return whether this object equals another.
+	 * @param obj
+	 *            the other object
+	 */
+	@Override
+	public final boolean equals(final Object obj) {
+		return this == obj || (obj instanceof Point && (((Point) obj).myRow == myRow && ((Point) obj).myCol == myCol));
+	}
+
+	/**
+	 * @return a hash code.
+	 */
+	@Override
+	public final int hashCode() {
+		return myRow | myCol;
+	}
+}
--- src/model/viewer/SPMap.java
+++ src/model/viewer/SPMap.java
@@ -101,68 +101,6 @@ public class SPMap implements Serializable {
 	}
 
 	/**
-	 * A structure encapsulating two coordinates.
-	 * 
-	 * @author Jonathan Lovelace
-	 * 
-	 */
-	public static class Point {
-		/**
-		 * The first coordinate.
-		 */
-		private final int myRow;
-		/**
-		 * The second coordinate.
-		 */
-		private final int myCol;
-
-		/**
-		 * @return the first coordinate.
-		 */
-		public final int row() {
-			return myRow;
-		}
-
-		/**
-		 * @return the second coordinate.
-		 */
-		public final int col() {
-			return myCol;
-		}
-
-		/**
-		 * Constructor.
-		 * 
-		 * @param row
-		 *            The first coordinate
-		 * @param col
-		 *            The second coordinate
-		 */
-		public Point(final int row, final int col) {
-			myRow = row;
-			myCol = col;
-		}
-
-		/**
-		 * @return whether this object equals another.
-		 * @param obj
-		 *            the other object
-		 */
-		@Override
-		public final boolean equals(final Object obj) {
-			return this == obj || (obj instanceof Point && (((Point) obj).myRow == myRow && ((Point) obj).myCol == myCol));
-		}
-
-		/**
-		 * @return a hash code.
-		 */
-		@Override
-		public final int hashCode() {
-			return myRow | myCol;
-		}
-	}
-
-	/**
 	 * @param row
 	 *            the row
 	 * @param col
