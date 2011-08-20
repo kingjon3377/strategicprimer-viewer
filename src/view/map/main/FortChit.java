aabdff953f67996019adf62be895ad98637cb97d
Jonathan Lovelace <kingjon3377@gmail.com>
2011-08-20 15:57:34 -0400

    Treat the empty string like a null string in handling a Fortress's description.

--- src/view/map/main/FortChit.java
+++ src/view/map/main/FortChit.java
@@ -32,7 +32,7 @@ public class FortChit extends Chit {
 	public FortChit(final Fortress fort, final MouseListener listener) {
 		super(listener);
 		final StringBuilder sbuild = new StringBuilder("<html><p>Fortress ");
-		if (fort.getName() != null) {
+		if (fort.getName() != null && !"".equals(fort.getName())) {
 			sbuild.append(fort.getName());
 			sbuild.append(", ");
 		}
