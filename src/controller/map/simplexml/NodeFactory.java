78175f6c48afae94282e2daf4082cc1dca579371
Jonathan Lovelace <kingjon3377@gmail.com>
2011-08-23 14:18:15 -0400

    A factory for Nodes. We look in a Map to determine what category a tag is, then based on that construct a Node for it.
    If a single Node type is used by multiple tags with different minimum
    attributes---all rivers have to have a direction, for example, except that
    lakes are treated as a special case of rivers and don't have to have a
    direction---the category-to-Node step is where we do the extra setup required.
    After the Factory returns an object, what XML was behind it is a black box. On
    the other hand, while I haven't done this yet, it should be possible to have a
    single tag map to multiple categories (which may or may not be a good thing to
    allow).
    
    The other benefit of this Map- and Factory-based approach, as opposed to the
    previous model where enum values knew what tags they represented, is that it
    lets us define new tags within existing categories at runtime, or at least
    elsewhere in the project.
    
    Of course, all this is a poor-man's substitute for true algebraic data types,
    like Haskell (which I'm trying to learn) or Ceylon (which I am eagerly
    awaiting) ...

---
+++ src/controller/map/simplexml/NodeFactory.java
@@ -0,0 +1,197 @@
+package controller.map.simplexml;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.lang.NotImplementedException;
+
+/**
+ * A class to create properly-typed Nodes (but *not* their contents) based on
+ * the tags that represent them.
+ * 
+ * @author Jonathan Lovelace
+ * 
+ */
+public final class NodeFactory { // NOPMD
+	/**
+	 * Do not instantiate.
+	 */
+	private NodeFactory() {
+		// Do nothing.
+	}
+
+	/**
+	 * A mapping from XML tags to tag types.
+	 */
+	private static final Map<String, Tag> TAGS = new HashMap<String, Tag>();
+
+	/**
+	 * The kinds of tags the Factory knows how to parse. Note that multiple XML
+	 * tags can get mapped to a single enumerated tag-type.
+	 * 
+	 * @author kingjon
+	 * 
+	 */
+	private enum Tag {
+		/**
+		 * The main map tag.
+		 */
+		Map,
+		/**
+		 * Row and column tags, and perhaps others we'd rather ignore.
+		 */
+		Skippable,
+		/**
+		 * An individual tile.
+		 */
+		Tile,
+		/**
+		 * A player.
+		 */
+		Player,
+		/**
+		 * A fortress.
+		 */
+		Fortress,
+		/**
+		 * A unit.
+		 */
+		Unit,
+		/**
+		 * A river.
+		 */
+		River,
+		/**
+		 * A lake. Since lakes have no attributes, we can't make them just a
+		 * special case of rivers at this stage without just saying that any
+		 * river tag without a direction is a lake.
+		 */
+		Lake,
+		/**
+		 * An Event.
+		 */
+		Event,
+		/**
+		 * A battlefield.
+		 */
+		Battlefield,
+		/**
+		 * A cave.
+		 */
+		Cave,
+		/**
+		 * A city.
+		 */
+		City,
+		/**
+		 * A fortification. FIXME: Again, we want this to use the Fortress tag
+		 * instead, eventually.
+		 */
+		Fortification,
+		/**
+		 * A stone deposit.
+		 */
+		Stone,
+		/**
+		 * A non-stone mineral deposit.
+		 */
+		Mineral,
+		/**
+		 * A town.
+		 */
+		Town;
+	}
+
+	/**
+	 * Set up a tag.
+	 * 
+	 * @param string
+	 *            an XML tag
+	 * @param tag
+	 *            a corresponding tag category
+	 */
+	private static void addTag(final String string, final Tag tag) {
+		TAGS.put(string, tag);
+	}
+
+	static {
+		addTag("map", Tag.Map);
+		addTag("row", Tag.Skippable);
+		addTag("tile", Tag.Tile);
+		addTag("player", Tag.Player);
+		addTag("fortress", Tag.Fortress);
+		addTag("unit", Tag.Unit);
+		addTag("river", Tag.River);
+		addTag("lake", Tag.Lake);
+		addTag("event", Tag.Event);
+		addTag("battlefield", Tag.Battlefield);
+		addTag("cave", Tag.Cave);
+		addTag("city", Tag.City);
+		addTag("fortification", Tag.Fortification);
+		addTag("stone", Tag.Stone);
+		addTag("mineral", Tag.Mineral);
+		addTag("town", Tag.Town);
+	}
+
+	/**
+	 * Create a Node from a tag.
+	 * 
+	 * @param tag
+	 *            the tag.
+	 * @param line
+	 *            the line of the file it's on ... just in case.
+	 * @return a Node representing the tag, but not its contents yet.
+	 * @throws SPFormatException
+	 *             on unrecognized tag
+	 */
+	public static AbstractChildNode<?> create(final String tag, final int line) //NOPMD
+			throws SPFormatException {
+		if (!TAGS.containsKey(tag)) {
+			throw new SPFormatException("Unknown tag " + tag, line);
+		}
+		// ESCA-JAVA0177:
+		final AbstractChildNode<?> node; // NOPMD
+		// ESCA-JAVA0040:
+		switch (TAGS.get(tag)) {
+		case Battlefield:
+			throw new NotImplementedException("Battlefield not implemented");
+		case Cave:
+			throw new NotImplementedException("Cave not implemented");
+		case City:
+			throw new NotImplementedException("City not implemented");
+		case Event:
+			throw new NotImplementedException("Event not implemented");
+		case Fortification:
+			throw new NotImplementedException("Fortification not implemented");
+		case Fortress:
+			throw new NotImplementedException("Fortress not implemented");
+		case Lake:
+			node = new RiverNode();
+			node.addProperty("direction", "lake");
+			break;
+		case Map:
+			node = new MapNode();
+			break;
+		case Mineral:
+			throw new NotImplementedException("Mineral not implemented yet");
+		case Player:
+			throw new NotImplementedException("Player not implemented yet");
+		case River:
+			node = new RiverNode();
+			break;
+		case Skippable:
+			throw new NotImplementedException("Skippable not implemented yet");
+		case Stone:
+			throw new NotImplementedException("Stone not implemented yet");
+		case Tile:
+			throw new NotImplementedException("Tile not implemented yet");
+		case Town:
+			throw new NotImplementedException("Town not implemented yet");
+		case Unit:
+			throw new NotImplementedException("Unit not implemented yet");
+		default:
+			throw new IllegalStateException("Shouldn't get here!");
+		}
+		return node;
+	}
+}
