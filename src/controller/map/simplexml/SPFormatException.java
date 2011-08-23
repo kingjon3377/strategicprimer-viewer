187bf8375132dce7097344c562ae5a6c1ec3bd4a
Jonathan Lovelace <kingjon3377@gmail.com>
2011-08-23 13:01:41 -0400

    A new custom exception for illegal XML constructs (like rivers outside the map).
    This is the first commit of yet another XML-map-reading attempt. This time, I'm
    trying to avoid mixing reading data in with checking that it makes sense for
    our model so much; instead, we'll read the XML into a tree of nodes, which will
    then validate themselves and turn themselves into a map. Some of what I plan to
    do in this version will also make adding dynamic data types (custom units,
    etc., from file ...) much easier.

---
+++ src/controller/map/simplexml/SPFormatException.java
@@ -0,0 +1,27 @@
+package controller.map.simplexml;
+/**
+ * A custom exception for XML format errors.
+ * @author Jonathan Lovelace
+ *
+ */
+public class SPFormatException extends Exception {
+	/**
+	 * The line of the XML file containing the mistake.
+	 */
+	private final int line;
+	/**
+	 * @return the line of the XML file containing the mistake
+	 */
+	public int getLine() {
+		return line;
+	}
+	/**
+	 * Constructor.
+	 * @param message a message describing what's wrong with the XML.
+	 * @param errorLine the line containing the error.
+	 */
+	public SPFormatException(final String message, final int errorLine) {
+		super("Incorrect SP XML at line " + errorLine + ": " + message);
+		line = errorLine;
+	}
+}
