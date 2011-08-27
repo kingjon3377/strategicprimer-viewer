763cea73131ee9d12a50279d4de537fb8441b009
Jonathan Lovelace <kingjon3377@gmail.com>
2011-08-26 23:42:47 -0400

    Never let a Selectable be null; use a Null object instead.

--- src/view/map/main/SelectionListener.java
+++ src/view/map/main/SelectionListener.java
@@ -1,5 +1,6 @@
 package view.map.main;
 
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -26,14 +27,10 @@ public class SelectionListener implements MouseListener {
 	 * @param sel the new currently-selected item
 	 */
 	protected void setSelection(final Selectable sel) {
-		if ((sel == null ? selection != null : !sel.equals(selection))) {
-			if (selection != null) {
+		if (!sel.equals(selection)) {
 				selection.setSelected(false);
-			}
 			selection = sel;
-			if (selection != null) {
 				selection.setSelected(true);
-			}
 		}
 	}
 	/**
@@ -41,7 +38,7 @@ public class SelectionListener implements MouseListener {
 	 */
 	public SelectionListener() {
 		super();
-		selection = null; // NOPMD
+		selection = NullSelection.EMPTY;
 	}
 
 	/**
@@ -53,7 +50,8 @@ public class SelectionListener implements MouseListener {
 	@Override
 	public void mouseClicked(final MouseEvent event) {
 		if (event.getComponent() instanceof Selectable) {
-			setSelection((Selectable) (event.getComponent()));
+			setSelection(event.getComponent() == null ? NullSelection.EMPTY
+					: (Selectable) (event.getComponent()));
 		}
 	}
 
@@ -106,6 +104,31 @@ public class SelectionListener implements MouseListener {
 	 * care of clearing the view of the selection.
 	 */
 	public void clearSelection() {
-		selection = null; // NOPMD
+		selection = NullSelection.EMPTY;
+	}
+	/**
+	 * A class to replace all cases where a Selectable variable is null.
+	 * @author Jonathan Lovelace
+	 */
+	public static final class NullSelection extends Selectable {
+		/**
+		 * Singleton.
+		 */
+		public static final NullSelection EMPTY = new NullSelection();
+		/**
+		 * Singleton.
+		 */
+		private NullSelection() {
+			super();
+		}
+		/**
+		 * Don't waste resources doing any painting.
+		 * @param pen ignored
+		 */
+		@Override
+		// ESCA-JAVA0025:
+		public void paint(final Graphics pen) {
+			// Do nothing.
+		}
 	}
 }
