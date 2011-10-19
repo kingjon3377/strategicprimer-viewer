ab92cfa7097e67d6d552eaa373d02dac6da8c4f7
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-18 20:04:05 -0400

    Extract the tests into their own methods, reducing duplication.

--- src/controller/map/DrawHelperComparator.java
+++ src/controller/map/DrawHelperComparator.java
@@ -24,15 +24,131 @@ import controller.map.simplexml.SimpleXMLReader;
  */
 public class DrawHelperComparator {
 	/**
+	 * Label to put before every direct-helper test result.
+	 */
+	private static final String DIRECT_NAME = "Direct:";
+	/**
+	 * Label to put before every caching-helper test result.
+	 */
+	private static final String CACHING_NAME = "Caching:";
+	/**
+	 * Constructor.
+	 * @param map the map we'll be drawing in the tests
+	 * @param repetitions how many times to repeat each test
+	 */
+	public DrawHelperComparator(final SPMap map, final int repetitions) {
+		spmap = map;
+		reps = repetitions;
+	}
+	/**
+	 * The map.
+	 */
+	private final SPMap spmap;
+	/**
+	 * How many times to repeat each test.
+	 */
+	private final int reps;
+	/**
+	 * The size of a tile, factored out to reduce number of lines a line of code
+	 * has to use.
+	 */
+	private final int tsize = MapComponent.getTileSize();
+	/**
+	 * The first test: all in one place.
+	 * @param helper the helper to test
+	 * @return how long the test took, in ns.
+	 */
+	public long first(final TileDrawHelper helper) {
+		final BufferedImage image = new BufferedImage(tsize, tsize,
+				BufferedImage.TYPE_INT_RGB);
+		final long start = System.nanoTime();
+		for (int rep = 0; rep < reps; rep++) {
+			image.flush();
+			for (int i = 0; i < spmap.rows(); i++) {
+				for (int j = 0; j < spmap.cols(); j++) {
+					helper.drawTile(image.createGraphics(),
+							spmap.getTile(i, j), tsize, tsize);
+				}
+			}
+		}
+		final long end = System.nanoTime();
+		return end - start;
+	}
+	/**
+	 * The second test: Translating.
+	 * @param helper the helper to test
+	 * @return how long the test took, in ns.
+	 */
+	public long second(final TileDrawHelper helper) {
+		final BufferedImage image = new BufferedImage(tsize * spmap.cols(),
+				tsize * spmap.rows(), BufferedImage.TYPE_INT_RGB);
+		final long start = System.nanoTime();
+		for (int rep = 0; rep < reps; rep++) {
+			image.flush();
+			for (int i = 0; i < spmap.rows(); i++) {
+				for (int j = 0; j < spmap.cols(); j++) {
+					helper.drawTile(image.createGraphics(),
+							spmap.getTile(i, j), i * tsize, j * tsize, tsize,
+							tsize);
+				}
+			}
+		}
+		final long end = System.nanoTime();
+		return end - start;
+	}
+	/**
+	 * Third test: in-place, reusing Graphics.
+	 * @param helper the helper to test
+	 * @return how long the test took, in ns.
+	 */
+	public long third(final TileDrawHelper helper) {
+		final BufferedImage image = new BufferedImage(tsize, tsize,
+				BufferedImage.TYPE_INT_RGB);
+		final long start = System.nanoTime();
+		for (int rep = 0; rep < reps; rep++) {
+			image.flush();
+			final Graphics pen = image.createGraphics();
+			for (int i = 0; i < spmap.rows(); i++) {
+				for (int j = 0; j < spmap.cols(); j++) {
+					helper.drawTile(pen, spmap.getTile(i, j), tsize, tsize);
+				}
+			}
+		}
+		final long end = System.nanoTime();
+		return end - start;
+	}
+	/**
+	 * Third test: translating, reusing Graphics.
+	 * @param helper the helper to test
+	 * @return how long the test took, in ns.
+	 */
+	public long fourth(final TileDrawHelper helper) {
+		final BufferedImage image = new BufferedImage(tsize * spmap.cols(),
+				tsize * spmap.rows(), BufferedImage.TYPE_INT_RGB);
+		final long start = System.nanoTime();
+		for (int rep = 0; rep < reps; rep++) {
+			image.flush();
+			final Graphics pen = image.createGraphics();
+			for (int i = 0; i < spmap.rows(); i++) {
+				for (int j = 0; j < spmap.cols(); j++) {
+					helper.drawTile(pen, spmap.getTile(i, j), i * tsize, j
+							* tsize, tsize, tsize);
+				}
+			}
+		}
+		final long end = System.nanoTime();
+		return end - start;
+	}
+	/**
 	 * A driver method to compare the two helpers, and the two map-GUI implementations.
 	 * @param args the command-line arguments.
 	 */
 	public static void main(final String[] args) { // NOPMD
 		final Logger logger = Logger.getLogger(DrawHelperComparator.class.getName());
 		// ESCA-JAVA0177:
-		final SPMap map; // NOPMD
+		final DrawHelperComparator comp; // NOPMD
 		try {
-			map = new SimpleXMLReader().readMap(args[0]);
+			comp = new DrawHelperComparator(new SimpleXMLReader().readMap(args[0]), 50);
 		} catch (IOException e) {
 			logger.log(Level.SEVERE, "I/O error reading map", e);
 			return; // NOPMD
@@ -45,151 +161,32 @@ public class DrawHelperComparator {
 		}
 		final TileDrawHelper helperOne = new CachingTileDrawHelper();
 		final TileDrawHelper helperTwo = new DirectTileDrawHelper();
-		BufferedImage image = new BufferedImage(MapComponent.getTileSize(), MapComponent.getTileSize(), BufferedImage.TYPE_INT_RGB);
-		final int reps = 50; // NOPMD
-		long start, end;
-		Graphics pen;
 		SystemOut.SYS_OUT.println("1. All in one place:");
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperOne.drawTile(image.createGraphics(),
-							map.getTile(i, j), MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Caching:");
-		printStats(end - start, reps);
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperTwo.drawTile(image.createGraphics(),
-							map.getTile(i, j), MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Direct: ");
-		printStats(end - start, reps);
+		SystemOut.SYS_OUT.print(CACHING_NAME);
+		comp.printStats(comp.first(helperOne));
+		SystemOut.SYS_OUT.print(DIRECT_NAME);
+		comp.printStats(comp.first(helperTwo));
 		SystemOut.SYS_OUT.println("2. Translating:");
-		image = new BufferedImage(MapComponent.getTileSize() * map.cols(),
-				MapComponent.getTileSize() * map.rows(),
-				BufferedImage.TYPE_INT_RGB);
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperOne.drawTile(image.createGraphics(),
-							map.getTile(i, j), i * MapComponent.getTileSize(),
-							j * MapComponent.getTileSize(),
-							MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Caching: ");
-		printStats(end - start, reps);
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperTwo.drawTile(image.createGraphics(),
-							map.getTile(i, j), i * MapComponent.getTileSize(),
-							j * MapComponent.getTileSize(),
-							MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Direct: ");
-		printStats(end - start, reps);
+		SystemOut.SYS_OUT.print(CACHING_NAME);
+		comp.printStats(comp.second(helperOne));
+		SystemOut.SYS_OUT.print(DIRECT_NAME);
+		comp.printStats(comp.second(helperTwo));
 		SystemOut.SYS_OUT.println("3. In-place, reusing Graphics:");
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			pen = image.createGraphics();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperOne.drawTile(pen, map.getTile(i, j),
-							MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Caching: ");
-		printStats(end - start, reps);
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			pen = image.createGraphics();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperTwo.drawTile(pen, map.getTile(i, j),
-							MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Direct: ");
-		printStats(end - start, reps);
+		SystemOut.SYS_OUT.print(CACHING_NAME);
+		comp.printStats(comp.third(helperOne));
+		SystemOut.SYS_OUT.print(DIRECT_NAME);
+		comp.printStats(comp.third(helperTwo));
 		SystemOut.SYS_OUT.println("4. Translating, reusing Graphics:");
-		image = new BufferedImage(MapComponent.getTileSize() * map.cols(),
-				MapComponent.getTileSize() * map.rows(),
-				BufferedImage.TYPE_INT_RGB);
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			pen = image.createGraphics();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperOne.drawTile(pen, map.getTile(i, j),
-							i * MapComponent.getTileSize(),
-							j * MapComponent.getTileSize(),
-							MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Caching: ");
-		printStats(end - start, reps);
-		start = System.nanoTime();
-		for (int rep = 0; rep < reps; rep++) {
-			image.flush();
-			pen = image.createGraphics();
-			for (int i = 0; i < map.rows(); i++) {
-				for (int j = 0; j < map.cols(); j++) {
-					helperTwo.drawTile(pen, map.getTile(i, j),
-							i * MapComponent.getTileSize(),
-							j * MapComponent.getTileSize(),
-							MapComponent.getTileSize(),
-							MapComponent.getTileSize());
-				}
-			}
-		}
-		end = System.nanoTime();
-		SystemOut.SYS_OUT.print("Direct: ");
-		printStats(end - start, reps);
+		SystemOut.SYS_OUT.print(CACHING_NAME);
+		comp.printStats(comp.fourth(helperOne));
+		SystemOut.SYS_OUT.print(DIRECT_NAME);
+		comp.printStats(comp.fourth(helperTwo));
 	}
 	/**
 	 * A helper method to reduce repeated strings.
 	 * @param total the total time
-	 * @param reps how many reps there were
 	 */
-	private static void printStats(final long total, final int reps) {
+	public void printStats(final long total) {
 		SystemOut.SYS_OUT.print('\t');
 		SystemOut.SYS_OUT.print(total);
 		SystemOut.SYS_OUT.print(", average of\t");
