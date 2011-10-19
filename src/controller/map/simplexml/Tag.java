07ab2b61195c759e76dae6cb5317750857e0370d
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-19 00:22:17 -0400

    Move Tag into its own file.

---
+++ src/controller/map/simplexml/Tag.java
@@ -0,0 +1,78 @@
+package controller.map.simplexml;
+
+/**
+ * The kinds of tags the Factory knows how to parse. Note that multiple XML
+ * tags can get mapped to a single enumerated tag-type.
+ * 
+ * @author kingjon
+ * 
+ */
+enum Tag {
+	/**
+	 * The main map tag.
+	 */
+	Map,
+	/**
+	 * Row and column tags, and perhaps others we'd rather ignore.
+	 */
+	Skippable,
+	/**
+	 * An individual tile.
+	 */
+	Tile,
+	/**
+	 * A player.
+	 */
+	Player,
+	/**
+	 * A fortress.
+	 */
+	Fortress,
+	/**
+	 * A unit.
+	 */
+	Unit,
+	/**
+	 * A river.
+	 */
+	River,
+	/**
+	 * A lake. Since lakes have no attributes, we can't make them just a
+	 * special case of rivers at this stage without just saying that any
+	 * river tag without a direction is a lake.
+	 */
+	Lake,
+	/**
+	 * An Event.
+	 */
+	Event,
+	/**
+	 * A battlefield.
+	 */
+	Battlefield,
+	/**
+	 * A cave.
+	 */
+	Cave,
+	/**
+	 * A city.
+	 */
+	City,
+	/**
+	 * A fortification. FIXME: Again, we want this to use the Fortress tag
+	 * instead, eventually.
+	 */
+	Fortification,
+	/**
+	 * A stone deposit.
+	 */
+	Stone,
+	/**
+	 * A non-stone mineral deposit.
+	 */
+	Mineral,
+	/**
+	 * A town.
+	 */
+	Town;
+}
