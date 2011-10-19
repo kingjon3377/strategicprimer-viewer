894044da46ce1c5a18c155eb272c2434381ec3bd
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-19 05:56:08 -0400

    Move the main() method from the ExplorationRunner into its own driver class.

---
+++ src/controller/map/drivers/TablesCheckDriver.java
@@ -0,0 +1,31 @@
+package controller.map.drivers;
+
+import model.exploration.ExplorationRunner;
+import controller.exploration.TableLoader;
+
+/**
+ * A driver to check that a directory of encounter tables is entirely formatted correctly.
+ * @author Jonathan Lovelace
+ *
+ */
+public final class TablesCheckDriver {
+	/**
+	 * A utility driver method that loads all files in tables/ under the current
+	 * directory, then checks to see whether any references a nonexistent table.
+	 * 
+	 * @param args
+	 *            ignore
+	 */
+	public static void main(final String[] args) {
+		final ExplorationRunner runner = new ExplorationRunner();
+		new TableLoader().loadAllTables("tables", runner);
+		// ESCA-JAVA0266:
+		runner.verboseRecursiveCheck(System.out);
+	}
+	/**
+	 * Do  not instantiate.
+	 */
+	private TablesCheckDriver() {
+		// Do nothing.
+	}
+}
--- src/model/exploration/ExplorationRunner.java
+++ src/model/exploration/ExplorationRunner.java
@@ -224,20 +224,6 @@ public class ExplorationRunner { // NOPMD
 	}
 
 	/**
-	 * A utility driver method that loads all files in tables/ under the current
-	 * directory, then checks to see whether any references a nonexistent table.
-	 * 
-	 * @param args
-	 *            ignore
-	 */
-	public static void main(final String[] args) {
-		final ExplorationRunner runner = new ExplorationRunner();
-		new TableLoader().loadAllTables("tables", runner);
-		// ESCA-JAVA0266:
-		runner.verboseRecursiveCheck(System.out);
-	}
-
-	/**
 	 * @return a String representation of the object
 	 */
 	@Override
