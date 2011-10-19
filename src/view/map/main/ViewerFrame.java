4a11a9986798caf8b43f2afa1aabda560a3892c0
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-19 03:53:53 -0400

    Move the createMenuItem method to its own class.

--- src/view/map/main/ViewerFrame.java
+++ src/view/map/main/ViewerFrame.java
@@ -12,13 +12,13 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.KeyStroke;
 
 import model.viewer.MapModel;
 import view.map.details.DetailPanel;
 import view.util.DriverQuit;
+import view.util.MenuItemCreator;
 
 /**
  * The main driver class for the viewer app.
@@ -82,39 +82,40 @@ public final class ViewerFrame extends JFrame implements ActionListener {
 	 * Set up the menu.
 	 */
 	private void createMenu() {
+		final MenuItemCreator creator = new MenuItemCreator();
 		final JMenuBar mbar = new JMenuBar();
 		final JMenu mapMenu = new JMenu("Map");
 		final ActionListener ioListener = new IOHandler(mapPanel.getModel(), this, chooser);
 		mapMenu.setMnemonic(KeyEvent.VK_M);
-		mapMenu.add(createMenuItem("Load", KeyEvent.VK_L,
+		mapMenu.add(creator.createMenuItem("Load", KeyEvent.VK_L,
 				KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK),
 				"Load a main map from file", ioListener));
-		mapMenu.add(createMenuItem("Save", KeyEvent.VK_S,
+		mapMenu.add(creator.createMenuItem("Save", KeyEvent.VK_S,
 				KeyStroke.getKeyStroke(KeyEvent.VK_S, ActionEvent.CTRL_MASK),
 				"Save the main map to file", ioListener));
 		mapMenu.addSeparator();
-		mapMenu.add(createMenuItem(
+		mapMenu.add(creator.createMenuItem(
 				LOAD_ALT_MAP_CMD,
 				KeyEvent.VK_D,
 				KeyStroke.getKeyStroke(KeyEvent.VK_O, ActionEvent.CTRL_MASK
 						+ ActionEvent.ALT_MASK),
 				"Load a secondary map from file", ioListener));
-		mapMenu.add(createMenuItem(SAVE_ALT_MAP_CMD,
+		mapMenu.add(creator.createMenuItem(SAVE_ALT_MAP_CMD,
 				KeyEvent.VK_V, KeyStroke.getKeyStroke(KeyEvent.VK_S,
 						ActionEvent.CTRL_MASK + ActionEvent.ALT_MASK),
 				"Save the secondary map to file", ioListener));
 		mapMenu.addSeparator();
-		mapMenu.add(createMenuItem("Switch maps", KeyEvent.VK_W,
+		mapMenu.add(creator.createMenuItem("Switch maps", KeyEvent.VK_W,
 				KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK),
 				"Make the secondary map the main map and vice versa", this));
 		mbar.add(mapMenu);
-		mbar.add(createMenuItem("Restrict view",
+		mbar.add(creator.createMenuItem("Restrict view",
 				KeyEvent.VK_R,
 				KeyStroke.getKeyStroke(KeyEvent.VK_R, ActionEvent.CTRL_MASK),
 				"Show only a subset of the map", this));
 		mbar.add(new Filler(new Dimension(0, 0), new Dimension(0, 0),
 				new Dimension(Integer.MAX_VALUE, 0)));
-		mbar.add(createMenuItem("Quit", KeyEvent.VK_Q,
+		mbar.add(creator.createMenuItem("Quit", KeyEvent.VK_Q,
 				KeyStroke.getKeyStroke(KeyEvent.VK_Q, ActionEvent.CTRL_MASK),
 				"Quit the viewer", new ActionListener() {
 			@Override
@@ -125,24 +126,6 @@ public final class ViewerFrame extends JFrame implements ActionListener {
 			}
 		}));
 		setJMenuBar(mbar);
-	}
-	/**
-	 * Create a menu item.
-	 * @param item the text of the item
-	 * @param mnemonic the mnemonic key
-	 * @param accel the keyboard accelerator
-	 * @param desc the accessibile description.
-	 * @param list the listener to hande when the item is selected.
-	 * @return the configured menu item.
-	 */
-	private static JMenuItem createMenuItem(final String item,
-			final int mnemonic, final KeyStroke accel, final String desc,
-			final ActionListener list) {
-		final JMenuItem mitem = new JMenuItem(item, mnemonic);
-		mitem.setAccelerator(accel);
-		mitem.getAccessibleContext().setAccessibleDescription(desc);
-		mitem.addActionListener(list);
-		return mitem;
 	}
 	/**
 	 * Handle button presses and the like.
---
+++ src/view/util/MenuItemCreator.java
@@ -0,0 +1,32 @@
+package view.util;
+
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenuItem;
+import javax.swing.KeyStroke;
+
+/**
+ * A utility class to create menu items in a more functional style.
+ * @author Jonathan Lovelace
+ *
+ */
+public class MenuItemCreator {
+	/**
+	 * Create a menu item.
+	 * @param item the text of the item
+	 * @param mnemonic the mnemonic key
+	 * @param accel the keyboard accelerator
+	 * @param desc the accessibile description.
+	 * @param list the listener to hande when the item is selected.
+	 * @return the configured menu item.
+	 */
+	public JMenuItem createMenuItem(final String item,
+			final int mnemonic, final KeyStroke accel, final String desc,
+			final ActionListener list) {
+		final JMenuItem mitem = new JMenuItem(item, mnemonic);
+		mitem.setAccelerator(accel);
+		mitem.getAccessibleContext().setAccessibleDescription(desc);
+		mitem.addActionListener(list);
+		return mitem;
+	}
+}
