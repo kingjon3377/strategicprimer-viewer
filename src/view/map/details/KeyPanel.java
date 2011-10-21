df1a0f0d528db6109ebffe0aa8ad810c8df18fff
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-21 02:44:05 -0400

    Untested, but: Update the cache when notified about map version changes.

--- src/view/map/details/KeyPanel.java
+++ src/view/map/details/KeyPanel.java
@@ -2,6 +2,8 @@ package view.map.details;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.swing.JPanel;
 
@@ -13,18 +15,36 @@ import model.map.TileType;
  * @author Jonathan Lovelace
  * 
  */
-public class KeyPanel extends JPanel {
+public class KeyPanel extends JPanel implements PropertyChangeListener {
 	/**
 	 * Constructor.
 	 * @param version the map version
 	 */
 	public KeyPanel(final int version) {
 		super(new GridLayout(0, 3));
-		for (final TileType type : TileType.valuesForVersion(version)) {
-			add(new KeyElement(version, type)); // NOPMD
-		}
+		updateForVersion(version);
 		setMinimumSize(new Dimension(getMinimumSize().width, new KeyElement(version, 
 				TileType.NotVisible).getMinimumSize().height * 3));
 		setPreferredSize(getMinimumSize());
+	}
+	/**
+	 * Update the key for a new map version.
+	 * @param version the map version
+	 */
+	private void updateForVersion(final int version) {
+		removeAll();
+		for (final TileType type : TileType.valuesForVersion(version)) {
+			add(new KeyElement(version, type)); // NOPMD
+		}
+	}
+	/**
+	 * Listen for property changes---specifically, the map version.
+	 * @param evt the event to handle
+	 */
+	@Override
+	public void propertyChange(final PropertyChangeEvent evt) {
+		if ("version".equals(evt.getPropertyName())) {
+			updateForVersion((Integer) evt.getNewValue());
+		}
 	}
 }
