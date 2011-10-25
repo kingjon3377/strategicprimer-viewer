a7e9ab132347a3c625135101d9b528e78a805bf4
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-25 14:22:53 -0400

    Fix a NullPointerException firing the version-change event when there is no previous map.

--- src/model/viewer/MapModel.java
+++ src/model/viewer/MapModel.java
@@ -75,7 +75,7 @@ public final class MapModel implements PropertyChangeSource {
 	 *            the new map
 	 */
 	public void setMainMap(final SPMap newMap) {
-		pcs.firePropertyChange("version", map.getVersion(), newMap.getVersion());
+		pcs.firePropertyChange("version", (map == null ? 0 : map.getVersion()), newMap.getVersion());
 		map = newMap;
 		setSecondaryMap(new SPMap(map.getVersion(), map.rows(), map.cols()));
 		setSelection(-1, -1);
