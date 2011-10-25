5899499e37acf84912458ce0afac692191a5e01b
Jonathan Lovelace <kingjon3377@gmail.com>
2011-10-25 12:45:30 -0400

    An interface for a new XML-output paradigm.

---
+++ src/model/map/XMLWritable.java
@@ -0,0 +1,19 @@
+package model.map;
+
+/**
+ * An interface for model objects that can be written to XML. I know this is
+ * model-controller mixing, but I *really* don't want to have to keep adding new
+ * values to enums and new methods to the XMLWriter every time I want to extend
+ * the model. So instead we let every model object take care of writing itself
+ * to XML.
+ * 
+ * @author Jonathan Lovelace
+ * 
+ */
+public interface XMLWritable {
+	/**
+	 * Write the object to XML.
+	 * @return the XML representation of the object.
+	 */
+	String toXML();
+}
