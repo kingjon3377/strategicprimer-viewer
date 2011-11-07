7d8036fd400e6f5360f4b6886789085bf150f5e7
Jonathan Lovelace <kingjon3377@gmail.com>
2011-11-07 16:15:40 -0500

    Add a note about Meadow's use when we add vision.

--- src/model/map/fixtures/Meadow.java
+++ src/model/map/fixtures/Meadow.java
@@ -4,7 +4,7 @@ import model.map.HasImage;
 import model.map.TileFixture;
 
 /**
- * A field or meadow.
+ * A field or meadow. If in forest, should increase a unit's vision slightly when the unit is on it.
  * @author Jonathan Lovelace
  *
  */
