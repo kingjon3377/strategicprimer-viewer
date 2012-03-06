706ace5e884d846adb5a02719252f263d9856b9a
Jonathan Lovelace <kingjon3377@gmail.com>
2012-03-06 17:18:03 -0500

    Eclipse reformatting.

--- src/model/map/fixtures/TestFixtureSerialization.java
+++ src/model/map/fixtures/TestFixtureSerialization.java
@@ -14,8 +14,10 @@ import org.junit.Test;
 
 import controller.map.SPFormatException;
 import controller.map.simplexml.SimpleXMLReader;
+
 /**
  * A class to test serialization of TileFixtures.
+ * 
  * @author Jonathan Lovelace
  */
 public final class TestFixtureSerialization {
@@ -25,6 +27,7 @@ public final class TestFixtureSerialization {
 	public TestFixtureSerialization() {
 		setUp();
 	}
+
 	/**
 	 * Set-up method.
 	 */
@@ -32,17 +35,23 @@ public final class TestFixtureSerialization {
 	public void setUp() {
 		reader = new SimpleXMLReader();
 	}
+
 	/**
 	 * The XML reader we'll use to test.
 	 */
 	private SimpleXMLReader reader;
+
 	/**
 	 * Test the serialization of Animal.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testAnimalSerialization() throws XMLStreamException, SPFormatException {
+	public void testAnimalSerialization() throws XMLStreamException,
+			SPFormatException {
 		final Animal one = new Animal("animalOne", false, false);
 		assertEquals("First test of Animal serialization, reflection", one,
 				helpSerialization(one, Animal.class, true));
@@ -51,26 +60,31 @@ public final class TestFixtureSerialization {
 		final Animal two = new Animal("animalTwo", false, true);
 		assertEquals("Second test of Animal serialization, reflection", two,
 				helpSerialization(two, Animal.class, true));
-		assertEquals("Second test of Animal serialization, non-reflection", two,
-				helpSerialization(two, Animal.class, false));
+		assertEquals("Second test of Animal serialization, non-reflection",
+				two, helpSerialization(two, Animal.class, false));
 		final Animal three = new Animal("animalThree", true, false);
 		assertEquals("Third test of Animal serialization, reflection", three,
 				helpSerialization(three, Animal.class, true));
-		assertEquals("Third test of Animal serialization, non-reflection", three,
-				helpSerialization(three, Animal.class, false));
+		assertEquals("Third test of Animal serialization, non-reflection",
+				three, helpSerialization(three, Animal.class, false));
 		final Animal four = new Animal("animalFour", true, true);
 		assertEquals("Fourth test of Animal serialization, reflection", four,
 				helpSerialization(four, Animal.class, true));
-		assertEquals("Fourth test of Animal serialization, non-reflection", four,
-				helpSerialization(four, Animal.class, false));
+		assertEquals("Fourth test of Animal serialization, non-reflection",
+				four, helpSerialization(four, Animal.class, false));
 	}
+
 	/**
 	 * Test the serialization of CacheFixture.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testCacheSerialization() throws XMLStreamException, SPFormatException {
+	public void testCacheSerialization() throws XMLStreamException,
+			SPFormatException {
 		final CacheFixture one = new CacheFixture("kindOne", "contentsOne");
 		assertEquals("First test of Cache serialization, reflection", one,
 				helpSerialization(one, CacheFixture.class, true));
@@ -82,31 +96,41 @@ public final class TestFixtureSerialization {
 		assertEquals("Second test of Cache serialization, non-reflection", two,
 				helpSerialization(two, CacheFixture.class, false));
 	}
+
 	/**
 	 * Test the serialization of Centaurs.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testCentaurSerialization() throws XMLStreamException, SPFormatException {
+	public void testCentaurSerialization() throws XMLStreamException,
+			SPFormatException {
 		final Centaur one = new Centaur("firstCentaur");
 		assertEquals("First test of Centaur serialization, reflection", one,
 				helpSerialization(one, Centaur.class, true));
-		assertEquals("First test of Centaur serialization, non-reflection", one,
-				helpSerialization(one, Centaur.class, false));
+		assertEquals("First test of Centaur serialization, non-reflection",
+				one, helpSerialization(one, Centaur.class, false));
 		final Centaur two = new Centaur("secondCentaur");
 		assertEquals("Second test of Centaur serialization, reflection", two,
 				helpSerialization(two, Centaur.class, true));
-		assertEquals("Second test of Centaur serialization, non-reflection", two,
-				helpSerialization(two, Centaur.class, false));
+		assertEquals("Second test of Centaur serialization, non-reflection",
+				two, helpSerialization(two, Centaur.class, false));
 	}
+
 	/**
 	 * Test the serialization of Dragons.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testDragonSerialization() throws XMLStreamException, SPFormatException {
+	public void testDragonSerialization() throws XMLStreamException,
+			SPFormatException {
 		final Dragon one = new Dragon("");
 		assertEquals("First test of Dragon serialization, reflection", one,
 				helpSerialization(one, Dragon.class, true));
@@ -115,16 +139,21 @@ public final class TestFixtureSerialization {
 		final Dragon two = new Dragon("secondDragon");
 		assertEquals("Second test of Dragon serialization, reflection", two,
 				helpSerialization(two, Dragon.class, true));
-		assertEquals("Second test of Dragon serialization, non-reflection", two,
-				helpSerialization(two, Dragon.class, false));
+		assertEquals("Second test of Dragon serialization, non-reflection",
+				two, helpSerialization(two, Dragon.class, false));
 	}
+
 	/**
 	 * Test the serialization of Fairies.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testFairySerialization() throws XMLStreamException, SPFormatException {
+	public void testFairySerialization() throws XMLStreamException,
+			SPFormatException {
 		final Fairy one = new Fairy("oneFairy");
 		assertEquals("First test of Fairy serialization, reflection", one,
 				helpSerialization(one, Fairy.class, true));
@@ -136,13 +165,18 @@ public final class TestFixtureSerialization {
 		assertEquals("Second test of Fairy serialization, non-reflection", two,
 				helpSerialization(two, Fairy.class, false));
 	}
+
 	/**
 	 * Test the serialization of Forests.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testForestSerialization() throws XMLStreamException, SPFormatException {
+	public void testForestSerialization() throws XMLStreamException,
+			SPFormatException {
 		final Forest one = new Forest("one", false);
 		assertEquals("First test of Forest serialization, reflection", one,
 				helpSerialization(one, Forest.class, true));
@@ -151,8 +185,8 @@ public final class TestFixtureSerialization {
 		final Forest two = new Forest("two", true);
 		assertEquals("Second test of Forest serialization, reflection", two,
 				helpSerialization(two, Forest.class, true));
-		assertEquals("Second test of Forest serialization, non-reflection", two,
-				helpSerialization(two, Forest.class, false));
+		assertEquals("Second test of Forest serialization, non-reflection",
+				two, helpSerialization(two, Forest.class, false));
 	}
 
 	/**
@@ -223,31 +257,43 @@ public final class TestFixtureSerialization {
 
 	/**
 	 * Test the serialization of simple (no-parameter) fixtures.
-	 * @throws SPFormatException on XML format error
-	 * @throws XMLStreamException on XML reader error
+	 * 
+	 * @throws SPFormatException
+	 *             on XML format error
+	 * @throws XMLStreamException
+	 *             on XML reader error
 	 */
 	@Test
-	public void testSimpleSerialization() throws XMLStreamException, SPFormatException {
+	public void testSimpleSerialization() throws XMLStreamException,
+			SPFormatException {
 		final Djinn djinn = new Djinn();
 		assertEquals("Test of Djinn serialization, reflection", djinn,
 				helpSerialization(djinn, Djinn.class, true));
 		assertEquals("Test of Djinn serialization, non-reflection", djinn,
 				helpSerialization(djinn, Djinn.class, false));
 		final Griffin griffin = new Griffin();
-		assertEquals("Test of Griffin serialization, reflection", griffin, 
+		assertEquals("Test of Griffin serialization, reflection", griffin,
 				helpSerialization(griffin, Griffin.class, true));
-		assertEquals("Test of Griffin serialization, non-reflection", griffin, 
+		assertEquals("Test of Griffin serialization, non-reflection", griffin,
 				helpSerialization(griffin, Griffin.class, false));
 	}
+
 	/**
 	 * A helper method to simplify test boiler plate code.
-	 * @param <T> The type of the object
-	 * @param orig the object to serialize
-	 * @param type the type of the object
-	 * @param reflection whether to use reflection
-	 * @return the result of deserializing the serialized form 
-	 * @throws SPFormatException on SP XML problem
-	 * @throws XMLStreamException on XML reading problem
+	 * 
+	 * @param <T>
+	 *            The type of the object
+	 * @param orig
+	 *            the object to serialize
+	 * @param type
+	 *            the type of the object
+	 * @param reflection
+	 *            whether to use reflection
+	 * @return the result of deserializing the serialized form
+	 * @throws SPFormatException
+	 *             on SP XML problem
+	 * @throws XMLStreamException
+	 *             on XML reading problem
 	 */
 	private <T extends XMLWritable> T helpSerialization(final T orig,
 			final Class<T> type, final boolean reflection)
