bd26ef60609a1eadf18b5f3ec2784b1abae2d4f3
Jonathan Lovelace <kingjon3377@gmail.com>
2012-04-06 18:45:16 -0400

    Eclipse reformatting.

--- src/controller/map/readerng/TileReader.java
+++ src/controller/map/readerng/TileReader.java
@@ -13,10 +13,12 @@ import model.map.TileType;
 import model.map.fixtures.RiverFixture;
 import model.map.fixtures.TextFixture;
 import controller.map.SPFormatException;
+
 /**
  * A reader for Tiles.
+ * 
  * @author Jonathan Lovelace
- *
+ * 
  */
 public class TileReader implements INodeReader<Tile> {
 	/**
@@ -26,22 +28,29 @@ public class TileReader implements INodeReader<Tile> {
 	public Class<Tile> represents() {
 		return Tile.class;
 	}
+
 	/**
-	 * @param element the element to start with
-	 * @param stream the stream to get more elements from
+	 * @param element
+	 *            the element to start with
+	 * @param stream
+	 *            the stream to get more elements from
 	 * @return the tile we're at in the stream
-	 * @throws SPFormatException on map format error
-	 *
-	 * @see controller.map.readerng.INodeReader#parse(javax.xml.stream.events.StartElement, java.lang.Iterable)
+	 * @throws SPFormatException
+	 *             on map format error
+	 * 
+	 * @see controller.map.readerng.INodeReader#parse(javax.xml.stream.events.StartElement,
+	 *      java.lang.Iterable)
 	 */
 	@Override
-	public Tile parse(final StartElement element, final Iterable<XMLEvent> stream)
-			throws SPFormatException {
+	public Tile parse(final StartElement element,
+			final Iterable<XMLEvent> stream) throws SPFormatException {
 		final Tile tile = new Tile(parseInt(getAttribute(element, "row")),
 				parseInt(getAttribute(element, "column")),
 				TileType.getTileType(getAttribute(element, "type")));
 		for (final XMLEvent event : stream) {
-			if (event.isStartElement() && FixtureReader.supports(event.asStartElement().getName().getLocalPart())) {
+			if (event.isStartElement()
+					&& FixtureReader.supports(event.asStartElement().getName()
+							.getLocalPart())) {
 				tile.addFixture(ReaderFactory.createReader(TileFixture.class)
 						.parse(event.asStartElement(), stream));
 			} else if (event.isCharacters()) {
