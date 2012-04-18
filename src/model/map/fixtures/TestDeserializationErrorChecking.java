856bf06062d83aa9815508f6f5c205d121626675
Jonathan Lovelace <kingjon3377@gmail.com>
2012-04-18 01:01:54 -0400

    Move error-checking tests in with other tests of the same classes.

--- src/model/map/fixtures/TestDeserializationErrorChecking.java
+++ src/model/map/fixtures/TestDeserializationErrorChecking.java
@@ -94,23 +94,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 	}
 
 	/**
-	 * Test that a Troll shouldn't have children.
-	 * 
-	 * @throws SPFormatException
-	 *             always
-	 * @throws XMLStreamException
-	 *             never
-	 */
-	@Test
-	public void testTrollChildren() throws XMLStreamException,
-			SPFormatException {
-		assertUnwantedChild(reader, "<troll><troll /></troll>", Troll.class,
-				false, false);
-		assertUnwantedChild(reader, "<troll><troll /></troll>", Troll.class,
-				true, false);
-	}
-
-	/**
 	 * Test that a town must have a DC, a size, and a status.
 	 * 
 	 * @throws SPFormatException
@@ -153,38 +136,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 		assertMissingProperty(reader, "<cave />", CaveEvent.class, "dc", true,
 				false);
 	}
-
-	/**
-	 * Test that a Centaur can't have any children and must have a kind.
-	 * 
-	 * @throws SPFormatException
-	 *             always
-	 * @throws XMLStreamException
-	 *             never
-	 */
-	@Test
-	public void testCentaurErrors() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<centaur><troll /></centaur>",
-				Centaur.class, false, false);
-		assertUnwantedChild(reader, "<centaur><troll /></centaur>",
-				Centaur.class, true, false);
-		assertMissingProperty(reader, "<centaur />", Centaur.class, KIND_PROPERTY,
-				false, false);
-		assertMissingProperty(reader, "<centaur />", Centaur.class, KIND_PROPERTY,
-				true, false);
-	}
-	/**
-	 * Test that a Djinn can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testDjinnChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<djinn><troll /></djinn>", Djinn.class,
-				false, false);
-		assertUnwantedChild(reader, "<djinn><troll /></djinn>", Djinn.class,
-				true, false);
-	}
 	
 	/**
 	 * Test that a tile must have a row, a column, and a kind, and objects to
@@ -217,30 +168,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 				reader,
 				"<tile row=\"0\" column=\"0\" kind=\"plains\"><tile row=\"1\" column=\"1\" kind=\"plains\" /></tile>",
 				Tile.class, true, false);
-	}
-	/**
-	 * Test that a Minotaur can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testMinotaurChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<minotaur><troll /></minotaur>",
-				Minotaur.class, false, false);
-		assertUnwantedChild(reader, "<minotaur><troll /></minotaur>",
-				Minotaur.class, true, false);
-	}
-	/**
-	 * Test that a Mountain can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testMountainChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<mountain><troll /></mountain>",
-				Mountain.class, false, false);
-		assertUnwantedChild(reader, "<mountain><troll /></mountain>",
-				Mountain.class, true, false);
 	}
 	/**
 	 * Test that a Mine can't have any children and must have a kind.
@@ -283,22 +210,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 				Meadow.class, "cultivated", true, false);
 	}
 	/**
-	 * Test that an Animal can't have any children and must have a kind.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testAnimalErrors() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<animal><troll /></animal>",
-				Animal.class, false, false);
-		assertUnwantedChild(reader, "<animal><troll /></animal>",
-				Animal.class, true, false);
-		assertMissingProperty(reader, "<animal />",
-				Animal.class, KIND_PROPERTY, false, false);
-		assertMissingProperty(reader, "<animal />",
-				Animal.class, KIND_PROPERTY, true, false);
-	}
-	/**
 	 * Test that a Battlefield can't have any children and must have a DC.
 	 * @throws SPFormatException always
 	 * @throws XMLStreamException never
@@ -315,30 +226,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 				BattlefieldEvent.class, "dc", true, false);
 	}
 	
-	/**
-	 * Test that a Cache can't have any children and must have 'kind' and
-	 * 'contents' properties.
-	 * 
-	 * @throws SPFormatException
-	 *             always
-	 * @throws XMLStreamException
-	 *             never
-	 */
-	@Test
-	public void testCacheChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<cache><troll /></cache>",
-				CacheFixture.class, false, false);
-		assertUnwantedChild(reader, "<cache><troll /></cache>",
-				CacheFixture.class, true, false);
-		assertMissingProperty(reader, "<cache contents=\"contents\" />",
-				CacheFixture.class, KIND_PROPERTY, false, false);
-		assertMissingProperty(reader, "<cache contents=\"contents\" />",
-				CacheFixture.class, KIND_PROPERTY, true, false);
-		assertMissingProperty(reader, "<cache kind=\"kind\" />",
-				CacheFixture.class, "contents", false, false);
-		assertMissingProperty(reader, "<cache kind=\"kind\" />",
-				CacheFixture.class, "contents", true, false);
-	}
 	/**
 	 * Test that a text node shouldn't have child nodes.
 	 * @throws SPFormatException always
@@ -374,34 +261,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 				StoneEvent.class, KIND_PROPERTY, true, false);
 	}
 	/**
-	 * Test that a Sphinx doesn't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testSphinxChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, 
-				"<sphinx><troll /></sphinx>",
-				Sphinx.class, false, false);
-		assertUnwantedChild(reader, 
-				"<sphinx><troll /></sphinx>",
-				Sphinx.class, true, false);
-	}
-	/**
-	 * Test that a Simurgh doesn't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testSimurghChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, 
-				"<simurgh><troll /></simurgh>",
-				Simurgh.class, false, false);
-		assertUnwantedChild(reader, 
-				"<simurgh><troll /></simurgh>",
-				Simurgh.class, true, false);
-	}
-	/**
 	 * Test that a Shrub can't have any children and must have a kind.
 	 * @throws SPFormatException always
 	 * @throws XMLStreamException never
@@ -414,18 +273,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 				Shrub.class, true, false);
 		assertMissingProperty(reader, "<shrub />", Shrub.class, KIND_PROPERTY, false, false);
 		assertMissingProperty(reader, "<shrub />", Shrub.class, KIND_PROPERTY, true, false);
-	}
-	/**
-	 * Test that a Sandbar can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testSandbarChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<sandbar><troll /></sandbar>",
-				Sandbar.class, false, false);
-		assertUnwantedChild(reader, "<sandbar><troll /></sandbar>",
-				Sandbar.class, true, false);
 	}
 	/**
 	 * Test that a River can't have any children and must have a direction.
@@ -462,42 +309,6 @@ public final class TestDeserializationErrorChecking extends // NOPMD
 				Player.class, "code_name", false, false);
 		assertMissingProperty(reader, "<player number=\"1\" />",
 				Player.class, "code_name", true, false);
-	}
-	/**
-	 * Test that a Phoenix can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testPhoenixChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<phoenix><troll /></phoenix>",
-				Phoenix.class, false, false);
-		assertUnwantedChild(reader, "<phoenix><troll /></phoenix>",
-				Phoenix.class, true, false);
-	}
-	/**
-	 * Test that an Ogre can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testOgreChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<ogre><troll /></ogre>",
-				Ogre.class, false, false);
-		assertUnwantedChild(reader, "<ogre><troll /></ogre>",
-				Ogre.class, true, false);
-	}
-	/**
-	 * Test that an Oasis can't have any children.
-	 * @throws SPFormatException always
-	 * @throws XMLStreamException never
-	 */
-	@Test
-	public void testOasisChild() throws XMLStreamException, SPFormatException {
-		assertUnwantedChild(reader, "<oasis><troll /></oasis>",
-				Oasis.class, false, false);
-		assertUnwantedChild(reader, "<oasis><troll /></oasis>",
-				Oasis.class, true, false);
 	}
 	
 	/**
--- src/model/map/fixtures/TestFixtureSerialization.java
+++ src/model/map/fixtures/TestFixtureSerialization.java
@@ -21,6 +21,10 @@ import controller.map.simplexml.SimpleXMLReader;
 public final class TestFixtureSerialization extends
 		BaseTestFixtureSerialization {
 	/**
+	 * Extracted constant.
+	 */
+	private static final String KIND_PROPERTY = "kind";
+	/**
 	 * Constructor.
 	 */
 	public TestFixtureSerialization() {
@@ -42,7 +46,7 @@ public final class TestFixtureSerialization extends
 	private SimpleXMLReader reader;
 
 	/**
-	 * Test the serialization of Animal.
+	 * Test the serialization of Animal, including catching format errors.
 	 * 
 	 * @throws SPFormatException
 	 *             on XML format error
@@ -72,10 +76,18 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, four, Animal.class, true));
 		assertEquals("Fourth test of Animal serialization, non-reflection",
 				four, helpSerialization(reader, four, Animal.class, false));
+		assertUnwantedChild(reader, "<animal><troll /></animal>",
+				Animal.class, false, false);
+		assertUnwantedChild(reader, "<animal><troll /></animal>",
+				Animal.class, true, false);
+		assertMissingProperty(reader, "<animal />",
+				Animal.class, KIND_PROPERTY, false, false);
+		assertMissingProperty(reader, "<animal />",
+				Animal.class, KIND_PROPERTY, true, false);
 	}
 
 	/**
-	 * Test the serialization of CacheFixture.
+	 * Test the serialization of CacheFixture, including catching format errors.
 	 * 
 	 * @throws SPFormatException
 	 *             on XML format error
@@ -95,10 +107,22 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, two, CacheFixture.class, true));
 		assertEquals("Second test of Cache serialization, non-reflection", two,
 				helpSerialization(reader, two, CacheFixture.class, false));
+		assertUnwantedChild(reader, "<cache><troll /></cache>",
+				CacheFixture.class, false, false);
+		assertUnwantedChild(reader, "<cache><troll /></cache>",
+				CacheFixture.class, true, false);
+		assertMissingProperty(reader, "<cache contents=\"contents\" />",
+				CacheFixture.class, KIND_PROPERTY, false, false);
+		assertMissingProperty(reader, "<cache contents=\"contents\" />",
+				CacheFixture.class, KIND_PROPERTY, true, false);
+		assertMissingProperty(reader, "<cache kind=\"kind\" />",
+				CacheFixture.class, "contents", false, false);
+		assertMissingProperty(reader, "<cache kind=\"kind\" />",
+				CacheFixture.class, "contents", true, false);
 	}
 
 	/**
-	 * Test the serialization of Centaurs.
+	 * Test the serialization of Centaurs, including catching format errors.
 	 * 
 	 * @throws SPFormatException
 	 *             on XML format error
@@ -118,6 +142,14 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, two, Centaur.class, true));
 		assertEquals("Second test of Centaur serialization, non-reflection",
 				two, helpSerialization(reader, two, Centaur.class, false));
+		assertUnwantedChild(reader, "<centaur><troll /></centaur>",
+				Centaur.class, false, false);
+		assertUnwantedChild(reader, "<centaur><troll /></centaur>",
+				Centaur.class, true, false);
+		assertMissingProperty(reader, "<centaur />", Centaur.class, KIND_PROPERTY,
+				false, false);
+		assertMissingProperty(reader, "<centaur />", Centaur.class, KIND_PROPERTY,
+				true, false);
 	}
 
 	/**
@@ -141,6 +173,7 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, two, Dragon.class, true));
 		assertEquals("Second test of Dragon serialization, non-reflection",
 				two, helpSerialization(reader, two, Dragon.class, false));
+		// TODO: errors.
 	}
 
 	/**
@@ -164,6 +197,7 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, two, Fairy.class, true));
 		assertEquals("Second test of Fairy serialization, non-reflection", two,
 				helpSerialization(reader, two, Fairy.class, false));
+		// TODO: errors
 	}
 
 	/**
@@ -187,6 +221,7 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, two, Forest.class, true));
 		assertEquals("Second test of Forest serialization, non-reflection",
 				two, helpSerialization(reader, two, Forest.class, false));
+		// TODO: errors
 	}
 
 	/**
@@ -230,6 +265,7 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, five, Fortress.class, true));
 		assertEquals("Fifth test of Fortress serialization, non-reflection",
 				five, helpSerialization(reader, five, Fortress.class, false));
+		// TODO: errors
 	}
 
 	/**
@@ -253,6 +289,7 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, two, Giant.class, true));
 		assertEquals("Second test of Giant serialization, non-reflection", two,
 				helpSerialization(reader, two, Giant.class, false));
+		// TODO: errors
 	}
 
 	/**
@@ -281,10 +318,11 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, three, Ground.class, true));
 		assertEquals("Third test of Ground serialization, non-reflection",
 				three, helpSerialization(reader, three, Ground.class, false));
+		// TODO: errors
 	}
 
 	/**
-	 * Test the serialization of simple (no-parameter) fixtures.
+	 * Test the serialization of simple (no-parameter) fixtures, including format errors.
 	 * 
 	 * @throws SPFormatException
 	 *             on XML format error
@@ -299,63 +337,109 @@ public final class TestFixtureSerialization extends
 				helpSerialization(reader, djinn, Djinn.class, true));
 		assertEquals("Test of Djinn serialization, non-reflection", djinn,
 				helpSerialization(reader, djinn, Djinn.class, false));
+		assertUnwantedChild(reader, "<djinn><troll /></djinn>", Djinn.class,
+				false, false);
+		assertUnwantedChild(reader, "<djinn><troll /></djinn>", Djinn.class,
+				true, false);
 		final Griffin griffin = new Griffin();
 		assertEquals("Test of Griffin serialization, reflection", griffin,
 				helpSerialization(reader, griffin, Griffin.class, true));
 		assertEquals("Test of Griffin serialization, non-reflection", griffin,
 				helpSerialization(reader, griffin, Griffin.class, false));
+		// TODO: errors
 		final Hill hill = new Hill();
 		assertEquals("Test of Hill serialization, reflection", hill,
 				helpSerialization(reader, hill, Hill.class, true));
 		assertEquals("Test of Hill serialization, non-reflection", hill,
 				helpSerialization(reader, hill, Hill.class, false));
+		// TODO: errors
 		final Minotaur minotaur = new Minotaur();
 		assertEquals("Test of Minotaur serialization, reflection", minotaur,
 				helpSerialization(reader, minotaur, Minotaur.class, true));
 		assertEquals("Test of Minotaur serialization, non-reflection",
 				minotaur,
 				helpSerialization(reader, minotaur, Minotaur.class, false));
+		assertUnwantedChild(reader, "<minotaur><troll /></minotaur>",
+				Minotaur.class, false, false);
+		assertUnwantedChild(reader, "<minotaur><troll /></minotaur>",
+				Minotaur.class, true, false);
 		final Mountain mountain = new Mountain();
 		assertEquals("Test of Mountain serialization, reflection", mountain,
 				helpSerialization(reader, mountain, Mountain.class, true));
 		assertEquals("Test of Mountain serialization, non-reflection",
 				mountain,
 				helpSerialization(reader, mountain, Mountain.class, false));
+		assertUnwantedChild(reader, "<mountain><troll /></mountain>",
+				Mountain.class, false, false);
+		assertUnwantedChild(reader, "<mountain><troll /></mountain>",
+				Mountain.class, true, false);
 		final Oasis oasis = new Oasis();
 		assertEquals("Test of Oasis serialization, reflection", oasis,
 				helpSerialization(reader, oasis, Oasis.class, true));
 		assertEquals("Test of Oasis serialization, non-reflection", oasis,
 				helpSerialization(reader, oasis, Oasis.class, false));
+		assertUnwantedChild(reader, "<oasis><troll /></oasis>",
+				Oasis.class, false, false);
+		assertUnwantedChild(reader, "<oasis><troll /></oasis>",
+				Oasis.class, true, false);
 		final Ogre ogre = new Ogre();
 		assertEquals("Test of Ogre serialization, reflection", ogre,
 				helpSerialization(reader, ogre, Ogre.class, true));
 		assertEquals("Test of Ogre serialization, non-reflection", ogre,
 				helpSerialization(reader, ogre, Ogre.class, false));
+		assertUnwantedChild(reader, "<ogre><troll /></ogre>",
+				Ogre.class, false, false);
+		assertUnwantedChild(reader, "<ogre><troll /></ogre>",
+				Ogre.class, true, false);
 		final Phoenix phoenix = new Phoenix();
 		assertEquals("Test of Phoenix serialization, reflection", phoenix,
 				helpSerialization(reader, phoenix, Phoenix.class, true));
 		assertEquals("Test of Phoenix serialization, non-reflection", phoenix,
 				helpSerialization(reader, phoenix, Phoenix.class, false));
+		assertUnwantedChild(reader, "<phoenix><troll /></phoenix>",
+				Phoenix.class, false, false);
+		assertUnwantedChild(reader, "<phoenix><troll /></phoenix>",
+				Phoenix.class, true, false);
 		final Sandbar sandbar = new Sandbar();
 		assertEquals("Test of Sandbar serialization, reflection", sandbar,
 				helpSerialization(reader, sandbar, Sandbar.class, true));
 		assertEquals("Test of Sandbar serialization, non-reflection", sandbar,
 				helpSerialization(reader, sandbar, Sandbar.class, false));
+		assertUnwantedChild(reader, "<sandbar><troll /></sandbar>",
+				Sandbar.class, false, false);
+		assertUnwantedChild(reader, "<sandbar><troll /></sandbar>",
+				Sandbar.class, true, false);
 		final Simurgh simurgh = new Simurgh();
 		assertEquals("Test of Simurgh serialization, reflection", simurgh,
 				helpSerialization(reader, simurgh, Simurgh.class, true));
 		assertEquals("Test of Simurgh serialization, non-reflection", simurgh,
 				helpSerialization(reader, simurgh, Simurgh.class, false));
+		assertUnwantedChild(reader, 
+				"<simurgh><troll /></simurgh>",
+				Simurgh.class, false, false);
+		assertUnwantedChild(reader, 
+				"<simurgh><troll /></simurgh>",
+				Simurgh.class, true, false);
 		final Sphinx sphinx = new Sphinx();
 		assertEquals("Test of Sphinx serialization, reflection", sphinx,
 				helpSerialization(reader, sphinx, Sphinx.class, true));
 		assertEquals("Test of Sphinx serialization, non-reflection", sphinx,
 				helpSerialization(reader, sphinx, Sphinx.class, false));
+		assertUnwantedChild(reader, 
+				"<sphinx><troll /></sphinx>",
+				Sphinx.class, false, false);
+		assertUnwantedChild(reader, 
+				"<sphinx><troll /></sphinx>",
+				Sphinx.class, true, false);
 		final Troll troll = new Troll();
 		assertEquals("Test of Troll serialization, reflection", troll,
 				helpSerialization(reader, troll, Troll.class, true));
 		assertEquals("Test of Troll serialization, non-reflection", troll,
 				helpSerialization(reader, troll, Troll.class, false));
+		assertUnwantedChild(reader, "<troll><troll /></troll>", Troll.class,
+				false, false);
+		assertUnwantedChild(reader, "<troll><troll /></troll>", Troll.class,
+				true, false);
 	}
 
 }
