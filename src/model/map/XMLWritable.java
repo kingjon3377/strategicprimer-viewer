87d2d78b7062bda675c601f7d3d7c01bb237cda9
Jonathan Lovelace <kingjon3377@gmail.com>
2012-05-25 10:00:19 -0400

    Remove dependency on a FindBugs jar that had crept in.

--- src/model/map/XMLWritable.java
+++ src/model/map/XMLWritable.java
@@ -1,6 +1,5 @@
 package model.map;
 
-import javax.annotation.CheckForNull;
 
 /**
  * An interface for model objects that can be written to XML. I know this is
@@ -22,7 +21,6 @@ public interface XMLWritable {
 	/**
 	 * @return The name of the file this is to be written to.
 	 */
-	@CheckForNull
 	String getFile();
 	/**
 	 * @param file the name of the file this should be written to.
