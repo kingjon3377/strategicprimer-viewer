fd05968faf7cc4622a6647c9a7d1bd512543ac4a
Jonathan Lovelace <kingjon3377@gmail.com>
2012-07-11 13:40:06 -0400

    Instead of multiplying special cases for SimpleChit, use it for all HasImage fixtures that don't have their own chit class.

--- src/view/map/details/ChitPanel.java
+++ src/view/map/details/ChitPanel.java
@@ -2,30 +2,13 @@ package view.map.details;
 
 import javax.swing.JPanel;
 
+import model.map.HasImage;
 import model.map.TileFixture;
-import model.map.events.BattlefieldEvent;
 import model.map.events.IEvent;
-import model.map.events.MineralEvent;
-import model.map.events.StoneEvent;
-import model.map.fixtures.Animal;
-import model.map.fixtures.CacheFixture;
-import model.map.fixtures.Dragon;
-import model.map.fixtures.Forest;
 import model.map.fixtures.Fortress;
 import model.map.fixtures.Ground;
-import model.map.fixtures.Grove;
-import model.map.fixtures.Hill;
-import model.map.fixtures.Meadow;
-import model.map.fixtures.Mine;
-import model.map.fixtures.Mountain;
-import model.map.fixtures.Oasis;
 import model.map.fixtures.RiverFixture;
-import model.map.fixtures.Sandbar;
-import model.map.fixtures.Shrub;
-import model.map.fixtures.TextFixture;
 import model.map.fixtures.Unit;
-import model.map.fixtures.Village;
-import util.EqualsAny;
 import view.map.main.SelectionListener;
 
 /**
@@ -67,24 +50,19 @@ public class ChitPanel extends JPanel {
 	 * @param fix
 	 *            the Fixture to add a chit for
 	 */
-	@SuppressWarnings("unchecked")
 	public void add(final TileFixture fix) {
 		if (fix instanceof Fortress) {
 			add(new FortChit((Fortress) fix, listener));
 		} else if (fix instanceof Unit) {
 			add(new UnitChit((Unit) fix, listener));
-		} else if (EqualsAny.equalsAny(fix.getClass(), Mine.class, Grove.class,
-				Oasis.class, Shrub.class, MineralEvent.class, StoneEvent.class,
-				Mountain.class, Forest.class, Animal.class, Meadow.class,
-				Hill.class, Village.class, CacheFixture.class, Sandbar.class,
-				TextFixture.class, BattlefieldEvent.class, Dragon.class)) {
-			add(new SimpleChit(fix, listener));
-		} else if (fix instanceof IEvent) {
-			add(new EventChit((IEvent) fix, listener));
 		} else if (fix instanceof RiverFixture) {
 			add(new RiverChit((RiverFixture) fix, listener));
 		} else if (fix instanceof Ground) {
 			add(new GroundChit((Ground) fix, listener));
+		} else if (fix instanceof HasImage) {
+			add(new SimpleChit(fix, listener));
+		} else if (fix instanceof IEvent) {
+			add(new EventChit((IEvent) fix, listener));
 		} else {
 			throw new IllegalStateException("We're missing a case here: " + fix.getClass().getSimpleName());
 		}
