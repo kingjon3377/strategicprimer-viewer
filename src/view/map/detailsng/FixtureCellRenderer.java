94da3dc26f2cf29875a513ec9ebd6d64d57650fb
Jonathan Lovelace <kingjon3377@gmail.com>
2012-08-23 21:42:17 -0400

    Remove the tree and table implementations, since we'll stick with the list-based one.

--- src/view/map/detailsng/FixtureCellRenderer.java
+++ src/view/map/detailsng/FixtureCellRenderer.java
@@ -13,30 +13,22 @@ import javax.swing.ImageIcon;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JTable;
-import javax.swing.JTree;
 import javax.swing.ListCellRenderer;
-import javax.swing.table.DefaultTableCellRenderer;
-import javax.swing.table.TableCellRenderer;
 import javax.swing.text.View;
-import javax.swing.tree.DefaultTreeCellRenderer;
-import javax.swing.tree.TreeCellRenderer;
 
 import model.map.HasImage;
 import model.map.TileFixture;
 import model.map.fixtures.RiverFixture;
-import model.viewer.FixtureTreeNode;
-import model.viewer.TileTreeNode;
 import util.ImageLoader;
 import view.map.details.Chit;
 
 /**
- * A tree-cell-renderer for FixtureTrees.
+ * A cell renderer for tile-details GUIs.
  *
  * @author Jonathan Lovelace
  *
  */
-public class FixtureCellRenderer implements ListCellRenderer<TileFixture>, TreeCellRenderer, TableCellRenderer {
+public class FixtureCellRenderer implements ListCellRenderer<TileFixture> {
 	/**
 	 * Default list renderer, for cases we don't know how to handle.
 	 */
@@ -68,83 +60,7 @@ public class FixtureCellRenderer implements ListCellRenderer<TileFixture>, TreeC
 		setComponentPreferredSize((JComponent) component, list.getWidth());
 		return component;
 	}
-	/**
-	 * Default tree renderer.
-	 */
-	private static final DefaultTreeCellRenderer TREE_DEFAULT = new DefaultTreeCellRenderer();
-	/**
-	 * Render a cell.
-	 * @param tree The tree we're rendering in
-	 * @param value The value in the tree we're rendering now
-	 * @param sel Whether or not it's selected
-	 * @param expanded Whether or not it's expanded
-	 * @param leaf Whether or not it's a leaf
-	 * @param row The current row
-	 * @param focused Whether or not it has the focus.
-	 * @return a component to render the node.
-	 */
-	// ESCA-JAVA0138:
-	@Override
-	public Component getTreeCellRendererComponent(final JTree tree, final Object value,
-			final boolean sel, final boolean expanded, final boolean leaf, final int row,
-			final boolean focused) {
-		final Component component = TREE_DEFAULT.getTreeCellRendererComponent(tree,
-				value, sel, expanded, leaf, row, focused);
-		if (component instanceof JLabel) {
-			if (value instanceof TileTreeNode) {
-				((JLabel) component).setText("<html><p>On this tile:</p></html>");
-			} else if (value instanceof FixtureTreeNode) {
-				final TileFixture fixture = ((FixtureTreeNode) value).getFixture();
-				((JLabel) component).setText("<html><p>" + fixture.toString() + "</p></html>");
-				if (fixture instanceof HasImage) {
-					((JLabel) component).setIcon(getIcon((HasImage) fixture));
-				} else {
-					((JLabel) component).setIcon(defaultFixtIcon);
-				}
-			}
-			setComponentPreferredSize((JComponent) component, tree.getWidth());
-		} else {
-			LOGGER.warning("Component wasn't a JLabel, so skipping custom rendering");
-		}
-		return component;
-	}
-	/**
-	 * Default table cell renderer.
-	 */
-	private static final DefaultTableCellRenderer TABLE_DEFAULT = new DefaultTableCellRenderer();
-	/**
-	 * Render a table cell.
-	 * @param table the table
-	 * @param value the value being rendered
-	 * @param isSelected whether it's selected
-	 * @param hasFocus whether it has focus
-	 * @param row its row
-	 * @param column its column
-	 * @return a component to render it
-	 */
-	@Override
-	public Component getTableCellRendererComponent(final JTable table, final Object value,
-			final boolean isSelected, final boolean hasFocus, final int row, final int column) {
-		final Component component = TABLE_DEFAULT.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-		if (component instanceof JLabel) {
-			if (value instanceof TileFixture) {
-				final TileFixture fixture = (TileFixture) value;
-				((JLabel) component).setText("<html><p>" + fixture.toString() + "</p></html>");
-				if (fixture instanceof HasImage) {
-					((JLabel) component).setIcon(getIcon((HasImage) fixture));
-				} else {
-					((JLabel) component).setIcon(defaultFixtIcon);
-				}
-			} else {
-				LOGGER.warning("Non-TileFixture item");
-			}
-		} else {
-			LOGGER.warning("Component wasn't a JLabel, so skipping custom rendering");
-		}
-		setComponentPreferredSize((JComponent) component, table.getWidth());
-		table.setRowHeight(row, component.getPreferredSize().height);
-		return component;
-	}
+
 	/**
 	 * @param obj a HasImage object
 	 * @return an icon representing it
