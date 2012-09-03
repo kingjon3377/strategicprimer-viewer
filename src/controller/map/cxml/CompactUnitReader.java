eadce1736963fd7a2e596ac5989f3513a5af5885
Jonathan Lovelace <kingjon3377@gmail.com>
2012-09-03 16:20:55 -0400

    Implement reading of Units.

--- src/controller/map/cxml/CompactUnitReader.java
+++ src/controller/map/cxml/CompactUnitReader.java
@@ -1,5 +1,7 @@
 package controller.map.cxml;
 
+import static controller.map.readerng.XMLHelper.getAttributeWithDeprecatedForm;
+
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
 
@@ -7,6 +9,8 @@ import model.map.PlayerCollection;
 import model.map.fixtures.mobile.Unit;
 import util.IteratorWrapper;
 import util.Warning;
+import controller.map.MissingParameterException;
+import controller.map.SPFormatException;
 import controller.map.misc.IDFactory;
 
 /**
@@ -34,13 +38,54 @@ public final class CompactUnitReader extends CompactReaderSuperclass implements 
 	 * @param warner the Warning instance to use for warnings
 	 * @param idFactory the ID factory to use to generate IDs
 	 * @return the parsed tile
+	 * @throws SPFormatException on SP format problem
 	 */
 	@Override
 	public <U extends Unit> U read(final StartElement element,
 			final IteratorWrapper<XMLEvent> stream, final PlayerCollection players,
-			final Warning warner, final IDFactory idFactory) {
-		// TODO Auto-generated method stub
-		return null;
+			final Warning warner, final IDFactory idFactory) throws SPFormatException {
+		requireTag(element, "unit");
+		requireNonEmptyParameter(element, "name", false, warner);
+		requireNonEmptyParameter(element, "owner", false, warner);
+		spinUntilEnd(element.getName(), stream);
+		return (U) new Unit(
+				players.getPlayer(Integer.parseInt(ensureNumeric(getParameter(
+						element, "owner", "-1")))), parseKind(element, warner),
+				getParameter(element, "name", ""), getOrGenerateID(element,
+						warner, idFactory), getFile(stream));
+	}
+	/**
+	 * Parse the kind of unit, from the "kind" or "type" parameter---default the
+	 * empty string.
+	 *
+	 * @param element the current element
+	 * @param warner the Warning instance to use
+	 * @return the kind of unit
+	 * @throws SPFormatException on SP format error.
+	 */
+	private static String parseKind(final StartElement element,
+			final Warning warner) throws SPFormatException {
+		String retval = "";
+		try {
+			retval = getParameterWithDeprecatedForm(element, // NOPMD
+					"kind", "type", warner);
+		} catch (final MissingParameterException except) {
+			warner.warn(except);
+			return ""; // NOPMD
+		}
+		if (retval.isEmpty()) {
+			warner.warn(new MissingParameterException(element.getName()
+					.getLocalPart(), "kind", element.getLocation()
+					.getLineNumber()));
+		}
+		return retval;
+	}
+	/**
+	 * @param string a string which should be numeric or empty
+	 * @return it, or "-1" if it's empty.
+	 */
+	private static String ensureNumeric(final String string) {
+		return string.isEmpty() ? "-1" : string;
 	}
 	/**
 	 * @param tag a tag
