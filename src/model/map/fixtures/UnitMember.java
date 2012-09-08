5ea08f311c88be48b09c7e082c21e7b49a802adb
Jonathan Lovelace <kingjon3377@gmail.com>
2012-09-07 20:59:13 -0400

    All UnitMembers must be XMLWritable.

--- src/model/map/fixtures/UnitMember.java
+++ src/model/map/fixtures/UnitMember.java
@@ -1,10 +1,12 @@
 package model.map.fixtures;
 
+import model.map.XMLWritable;
+
 /**
  * A (marker) interface for things that can be part of a unit.
  * @author Jonathan Lovelace
  *
  */
-public interface UnitMember {
+public interface UnitMember extends XMLWritable {
 	// Just a marker interface for now. TODO: members?
 }
