d4238743838f52ed4a9aacc1743ba99c84cab545
Jonathan Lovelace <kingjon3377@gmail.com>
2012-10-19 00:36:15 -0400

    Change exploration CLI from encounter-table-based to fixture-based encounters and information.

--- src/controller/map/drivers/ExplorationCLI.java
+++ src/controller/map/drivers/ExplorationCLI.java
@@ -4,29 +4,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.xml.stream.XMLStreamException;
 
-import model.exploration.ExplorationRunner;
-import model.exploration.MissingTableException;
 import model.map.IMap;
 import model.map.PointFactory;
+import model.map.TerrainFixture;
 import model.map.Tile;
+import model.map.TileFixture;
+import model.map.fixtures.Ground;
+import model.map.fixtures.mobile.Animal;
+import model.map.fixtures.resources.Grove;
+import model.map.fixtures.resources.Meadow;
+import model.map.fixtures.terrain.Forest;
 import util.Warning;
 import view.util.DriverQuit;
 import view.util.SystemOut;
-import controller.exploration.TableLoader;
 import controller.map.SPFormatException;
 import controller.map.misc.MapReaderAdapter;
 
 /**
  * A driver for running exploration results, etc., using the new model.
- * 
+ *
  * @author Jonathan Lovelace
  */
 public final class ExplorationCLI {
@@ -36,19 +40,10 @@ public final class ExplorationCLI {
 	private static final Logger LOGGER = Logger.getLogger(ExplorationCLI.class
 			.getName());
 	/**
-	 * The helper that actually runs the exploration.
-	 */
-	private final ExplorationRunner runner = new ExplorationRunner();
-	/**
-	 * Random number generator.
-	 */
-	private final Random random = new Random(System.currentTimeMillis());
-
-	/**
 	 * Constructor.
 	 */
 	private ExplorationCLI() {
-		new TableLoader().loadAllTables("tables", runner);
+		// Not for general use.
 	}
 
 	/**
@@ -83,7 +78,7 @@ public final class ExplorationCLI {
 	 * @param reader the stream to read further input from
 	 * @param ostream the stream to write to
 	 * @param input the command
-	 * 
+	 *
 	 * @throws IOException on I/O error
 	 */
 	public void handleCommand(final IMap map, final BufferedReader reader,
@@ -93,30 +88,80 @@ public final class ExplorationCLI {
 			explore(map, reader, ostream);
 			break;
 		case 'f':
-			fortressInfo(map, reader, ostream);
-			break;
-		case 'k':
-			runner.verboseRecursiveCheck(ostream);
+			fortressInfo(selectTile(map, reader, ostream), ostream);
 			break;
 		case 'h':
-		case 'g':
 		case 'i':
-			repeatedlyConsultTable(ORDER_MAP.get(Character.valueOf(input)),
-					selectTile(map, reader, ostream), HUNTER_HOURS
-							* HOURLY_ENCOUNTERS, ostream);
+			hunt(populateList(selectTile(map, reader, ostream)), ostream,
+					HUNTER_HOURS * HOURLY_ENCOUNTERS);
+			break;
+		case 'g':
+			gather(populateList(selectTile(map, reader, ostream)), ostream,
+					HUNTER_HOURS * HOURLY_ENCOUNTERS);
 			break;
 		default:
 			ostream.println("Unknown command.");
 			break;
 		}
 	}
-
+	/**
+	 * Adapter between Tile and List<TileFixture>.
+	 * @param tile a tile
+	 * @return a list of the fixtures on it.
+	 */
+	private static List<TileFixture> populateList(final Tile tile) {
+		final List<TileFixture> retval = new ArrayList<TileFixture>();
+		for (TileFixture fix : tile) {
+			retval.add(fix);
+		}
+		return retval;
+	}
+	/**
+	 * Run hunting, fishing, or trapping.
+	 * @param fixtures a list of the fixtures on the tile
+	 * @param ostream the stream to write to
+	 * @param encounters how many encounters to show
+	 */
+	private static void hunt(final List<TileFixture> fixtures,
+			final PrintStream ostream, final int encounters) {
+		for (int i = 0; i < encounters; i++) {
+			Collections.shuffle(fixtures);
+			final TileFixture fix = fixtures.get(0);
+			if (fix instanceof Animal) {
+				ostream.println(fix);
+			} else {
+				ostream.print("nothing ... (");
+				ostream.print(fix.getClass().getName());
+				ostream.println(')');
+			}
+		}
+	}
+	/**
+	 * Run food-gathering.
+	 * @param fixtures a list of the fixtures on the tile
+	 * @param ostream the stream to write to
+	 * @param encounters how many encounters to show
+	 */
+	private static void gather(final List<TileFixture> fixtures,
+			final PrintStream ostream, final int encounters) {
+		for (int i = 0; i < encounters; i++) {
+			Collections.shuffle(fixtures);
+			final TileFixture fix = fixtures.get(0);
+			if (fix instanceof Grove || fix instanceof Meadow) {
+				ostream.println(fix);
+			} else {
+				ostream.print("nothing ... (");
+				ostream.print(fix.getClass().getName());
+				ostream.println(')');
+			}
+		}
+	}
 	/**
 	 * We keep looping if the input is not null, is not an empty string, and is
 	 * not "quit" (only the first character is checked).
-	 * 
+	 *
 	 * @param input a line of input
-	 * 
+	 *
 	 * @return whether that input says we should keep going.
 	 */
 	private static boolean canKeepGoing(final String input) {
@@ -125,45 +170,71 @@ public final class ExplorationCLI {
 
 	/**
 	 * Explore a user-specified tile.
-	 * 
+	 *
 	 * @param map the map
 	 * @param reader source of user input
 	 * @param ostream the stream to write the results to
-	 * 
+	 *
 	 * @throws IOException on I/O error
 	 */
-	private void explore(final IMap map, final BufferedReader reader,
+	private static void explore(final IMap map, final BufferedReader reader,
 			final PrintStream ostream) throws IOException {
 		final Tile tile = selectTile(map, reader, ostream);
 		ostream.print("Tile is ");
 		ostream.println(tile.getTerrain());
-		try {
-			final int reps = random.nextInt(6) + 1;
-			for (int i = 0; i < reps; i++) {
-				ostream.println(runner.recursiveConsultTable("main", tile));
+		ostream.print("Fixtures influencing terrain: ");
+		boolean any = false;
+		final List<TileFixture> fixtures = new ArrayList<TileFixture>();
+		for (final TileFixture fix : tile) {
+			if (fix instanceof TerrainFixture) {
+				if (any) {
+					ostream.print("; ");
+				} else {
+					any = true;
+				}
+				ostream.print(fix);
 			}
-		} catch (final MissingTableException e) {
-			LOGGER.log(Level.SEVERE, "Missing table", e);
+			fixtures.add(fix);
+		}
+		ostream.println();
+		ostream.print("Here are four random fixtures from the tile:");
+		Collections.shuffle(fixtures);
+		for (int i = 0; i < 4; i++) {
+			ostream.println(fixtures.get(i));
 		}
 	}
 
 	/**
 	 * Give the data the player automatically knows about a user-specified tile
 	 * if he has a fortress on it.
-	 * 
-	 * @param map the map
-	 * @param reader source of user input
+	 *
+	 * @param tile the selected tile
 	 * @param ostream the stream to print results to
-	 * 
-	 * @throws IOException on I/O error
 	 */
-	private void fortressInfo(final IMap map, final BufferedReader reader,
-			final PrintStream ostream) throws IOException {
-		try {
-			ostream.print(runner
-					.defaultResults(selectTile(map, reader, ostream)));
-		} catch (final MissingTableException e) {
-			LOGGER.log(Level.SEVERE, "Missing table", e);
+	private static void fortressInfo(final Tile tile, final PrintStream ostream) {
+		ostream.print("Terrain is ");
+		ostream.println(tile.getTerrain());
+		final List<TileFixture> fixtures = populateList(tile);
+		final List<Ground> ground = new ArrayList<Ground>();
+		final List<Forest> forests = new ArrayList<Forest>();
+		for (TileFixture fix : fixtures) {
+			if (fix instanceof Ground) {
+				ground.add((Ground) fix);
+			} else if (fix instanceof Forest) {
+				forests.add((Forest) fix);
+			}
+		}
+		if (!ground.isEmpty()) {
+			ostream.println("Kind(s) of ground (rock) on the tile:");
+			for (Ground fix : ground) {
+				ostream.println(fix);
+			}
+		}
+		if (!forests.isEmpty()) {
+			ostream.println("Kind(s) of forests on the tile:");
+			for (Forest fix : forests) {
+				ostream.println(fix);
+			}
 		}
 	}
 
@@ -208,48 +279,6 @@ public final class ExplorationCLI {
 	private static final int HOURLY_ENCOUNTERS = 4;
 
 	/**
-	 * A map from commands to tables. Hunters, fishermen, and Food Gatherers are
-	 * handled the same way, but with different tables.
-	 */
-	private static final Map<Character, String> ORDER_MAP = setUpOrders();
-
-	/**
-	 * Set up the map from commands to tables.
-	 * 
-	 * @return the map
-	 */
-	@SuppressWarnings("boxing")
-	private static Map<Character, String> setUpOrders() {
-		final Map<Character, String> map = new HashMap<Character, String>();
-		map.put('h', "hunter");
-		map.put('H', "hunter");
-		map.put('g', "gatherer");
-		map.put('G', "gatherer");
-		map.put('i', "fisher");
-		map.put('I', "fisher");
-		return map;
-	}
-
-	/**
-	 * Repeatedly consult a table.
-	 * 
-	 * @param table the table to consult
-	 * @param tile the tile to refer to
-	 * @param reps how many times to consult it
-	 * @param ostream the stream to print the results to
-	 */
-	private void repeatedlyConsultTable(final String table, final Tile tile,
-			final int reps, final PrintStream ostream) {
-		for (int i = 0; i < reps; i++) {
-			try {
-				ostream.println(runner.recursiveConsultTable(table, tile));
-			} catch (final MissingTableException e) {
-				LOGGER.log(Level.SEVERE, "Missing table", e);
-			}
-		}
-	}
-
-	/**
 	 * @param args command-line arguments
 	 */
 	public static void main(final String[] args) {
@@ -273,7 +302,7 @@ public final class ExplorationCLI {
 	}
 
 	/**
-	 * 
+	 *
 	 * @return a String representation of the object.
 	 */
 	@Override
