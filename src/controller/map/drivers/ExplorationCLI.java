9c5d4d4b3b4c940b68329504f458d46d365143e1
Jonathan Lovelace <kingjon3377@gmail.com>
2012-12-04 17:31:34 -0500

    A tentatively complete first implementation of the exploration CLI.

--- src/controller/map/drivers/ExplorationCLI.java
+++ src/controller/map/drivers/ExplorationCLI.java
@@ -12,9 +12,17 @@ import javax.xml.stream.XMLStreamException;
 import model.map.IMap;
 import model.map.Player;
 import model.map.Point;
+import model.map.PointFactory;
 import model.map.Tile;
 import model.map.TileFixture;
+import model.map.fixtures.RiverFixture;
+import model.map.fixtures.mobile.SimpleMovement;
+import model.map.fixtures.mobile.SimpleMovement.TraversalImpossibleException;
 import model.map.fixtures.mobile.Unit;
+import model.map.fixtures.terrain.Forest;
+import model.map.fixtures.terrain.Hill;
+import model.map.fixtures.terrain.Mountain;
+import model.map.fixtures.towns.Fortress;
 import util.IsNumeric;
 import util.Warning;
 import view.util.SystemOut;
@@ -91,6 +99,96 @@ public class ExplorationCLI {
 		}
 		return retval;
 	}
+
+	/**
+	 * Find a fixture's location in the master map.
+	 *
+	 * @param fix the fixture to find.
+	 * @return the first location found (search order is not defined) containing a
+	 *         fixture "equal to" the specified one. (Using it on mountains,
+	 *         e.g., will *not* do what you want ...)
+	 */
+	public Point find(final TileFixture fix) {
+		for (Point point : source.getTiles()) {
+			for (TileFixture item : source.getTile(point)) {
+				if (fix.equals(item)) {
+					return point; // NOPMD
+				}
+			}
+		}
+		return PointFactory.point(-1, -1);
+	}
+
+	/**
+	 * Move a unit from the specified tile one tile in the specified direction.
+	 * Moves the unit in all maps where the unit *was* in the specified tile,
+	 * copying terrain information if the tile didn't exist in a subordinate
+	 * map.
+	 *
+	 * @param unit the unit to move
+	 * @param point the starting location
+	 * @param direction the direction to move
+	 * @return the movement cost
+	 * @throws TraversalImpossibleException if movement in that direction is impossible
+	 */
+	public int move(final Unit unit, final Point point,
+			final Direction direction) throws TraversalImpossibleException {
+		final Point dest = getDestination(point, direction);
+		final int retval = SimpleMovement.getMovementCost(source.getTile(dest));
+		source.getTile(point).removeFixture(unit);
+		source.getTile(dest).addFixture(unit);
+		for (IMap map : dests) {
+			final Tile stile = map.getTile(point);
+			boolean hasUnit = false;
+			for (final TileFixture fix : stile) {
+				if (fix.equals(unit)) {
+					hasUnit = true;
+					break;
+				}
+			}
+			if (!hasUnit) {
+				continue;
+			}
+			Tile dtile = map.getTile(dest);
+			if (dtile.isEmpty()) {
+				dtile = new Tile(dest.row, dest.col, source.getTile(dest) // NOPMD
+						.getTerrain());
+				map.getTiles().addTile(dtile);
+			}
+			stile.removeFixture(unit);
+			dtile.addFixture(unit);
+		}
+		return retval;
+	}
+	/**
+	 * @param point a point
+	 * @param direction a direction
+	 * @return the point one tile in that direction.
+	 */
+	private Point getDestination(final Point point, final Direction direction) {
+		switch (direction) {
+		case East:
+			return PointFactory.point(point.row, point.col + 1); // NOPMD
+		case North:
+			return PointFactory.point(point.row - 1, point.col); // NOPMD
+		case Northeast:
+			return PointFactory.point(point.row - 1, point.col + 1); // NOPMD
+		case Northwest:
+			return PointFactory.point(point.row - 1, point.col - 1); // NOPMD
+		case South:
+			return PointFactory.point(point.row + 1, point.col); // NOPMD
+		case Southeast:
+			return PointFactory.point(point.row + 1, point.col + 1); // NOPMD
+		case Southwest:
+			return PointFactory.point(point.row + 1, point.col - 1); // NOPMD
+		case Stop:
+			throw new IllegalArgumentException("Stop means no destination.");
+		case West:
+			return PointFactory.point(point.row, point.col - 1); // NOPMD
+		default:
+			throw new IllegalStateException("Unhandled case");
+		}
+	}
 	/**
 	 * Driver. Takes as its parameters the map files to use.
 	 * @param args the command-line arguments
@@ -118,7 +216,7 @@ public class ExplorationCLI {
 		final List<Player> players = getPlayerChoices(maps);
 		if (players.isEmpty()) {
 			SystemOut.SYS_OUT.println("No players shared by all the maps.");
-			return;
+			return; // NOPMD
 		}
 		SystemOut.SYS_OUT.println("The players shared by all the maps:");
 		for (int i = 0; i < players.size(); i++) {
@@ -126,7 +224,7 @@ public class ExplorationCLI {
 			SystemOut.SYS_OUT.print(": ");
 			SystemOut.SYS_OUT.println(players.get(i).getName());
 		}
-		Player player = players.get(inputNumber());
+		final Player player = players.get(inputNumber());
 		final List<Unit> units = getUnits(master, player);
 		if (units.isEmpty()) {
 			SystemOut.SYS_OUT.println("That player has no units in the master map.");
@@ -138,9 +236,81 @@ public class ExplorationCLI {
 			SystemOut.SYS_OUT.print(": ");
 			SystemOut.SYS_OUT.println(units.get(i).getName());
 		}
-		Unit unit = units.get(inputNumber());
+		final Unit unit = units.get(inputNumber());
 		SystemOut.SYS_OUT.println("Details of that unit:");
 		SystemOut.SYS_OUT.println(unit.verbose());
+		SystemOut.SYS_OUT.print("MP that unit has: ");
+		int movement = inputNumber();
+		int direction = -1;
+		final List<TileFixture> allFixtures = new ArrayList<TileFixture>();
+		// "contstants" is the fixtures that *always* get copied, such as
+		// forests, mountains hills, and rivers.
+		final List<TileFixture> constants = new ArrayList<TileFixture>();
+		while (movement > 0) {
+			SystemOut.SYS_OUT.print("0 = N, 1 = NE, 2 = E, 3 = SE, 4 = S, 5 = SW, ");
+			SystemOut.SYS_OUT.println("6 = W, 7 = NW, 8 = Quit.");
+			SystemOut.SYS_OUT.print("Direction to move: ");
+			direction = inputNumber();
+			if (direction > 7) {
+				break;
+			}
+			final Point point = cli.find(unit);
+			final Point dPoint = cli.getDestination(point, Direction.values()[direction]);
+			// ESCA-JAVA0177:
+			int cost;
+			try {
+				cost = cli.move(unit, point, Direction.values()[direction]);
+			} catch (TraversalImpossibleException except) {
+				movement--;
+				for (IMap map : secondaries) {
+					if (map.getTile(dPoint).isEmpty()) {
+						map.getTiles().addTile(
+								new Tile(dPoint.row, dPoint.col, master// NOPMD
+										.getTile(dPoint).getTerrain()));
+					}
+				}
+				SystemOut.SYS_OUT.println("That direction is impassable; we've made sure all maps show that at a cost of 1 MP");
+				continue;
+			}
+			movement -= cost;
+			allFixtures.clear();
+			// "contstants" is the fixtures that *always* get copied, such as
+			// forests, mountains hills, and rivers. Also fortresses, primarily
+			// so we'll always see when we want to stop. FIXME: Find a better way
+			// of doing that, so players can have a strategy based on secrecy.
+			constants.clear();
+			for (TileFixture fix : master.getTile(dPoint)) {
+				allFixtures.add(fix);
+				if (fix instanceof Mountain || fix instanceof RiverFixture
+						|| fix instanceof Hill || fix instanceof Forest
+						|| fix instanceof Fortress) {
+					constants.add(fix);
+				}
+			}
+			SystemOut.SYS_OUT.print("The explorer comes to a tile with terrain ");
+			SystemOut.SYS_OUT.print(master.getTile(dPoint).getTerrain());
+			SystemOut.SYS_OUT.println(" and the following automatically-noticed fixtures:");
+			for (TileFixture fix : constants) {
+				SystemOut.SYS_OUT.println(fix);
+			}
+			Collections.shuffle(allFixtures);
+			if (!allFixtures.isEmpty()) {
+				SystemOut.SYS_OUT.println("The following fixture was also noticed:");
+				SystemOut.SYS_OUT.println(allFixtures.get(0));
+			}
+			SystemOut.SYS_OUT.println("Updating subordinate maps with new data ...");
+			for (IMap map : secondaries) {
+				for (final TileFixture fix : constants) {
+					map.getTile(dPoint).addFixture(fix);
+				}
+				if (!allFixtures.isEmpty()) {
+					map.getTile(dPoint).addFixture(allFixtures.get(0));
+				}
+			}
+		}
+		for (int i = 1; i < args.length; i++) {
+			reader.write(args[i], maps.get(i));
+		}
 	}
 	/**
 	 * Read input from stdin repeatedly until a nonnegative integer is entered, and return it.
@@ -158,5 +328,46 @@ public class ExplorationCLI {
 			}
 		}
 		return retval;
+	}
+	/**
+	 * An enumeration of directions.
+	 */
+	public enum Direction {
+		/**
+		 * North.
+		 */
+		North,
+		/**
+		 * Northeast.
+		 */
+		Northeast,
+		/**
+		 * East.
+		 */
+		East,
+		/**
+		 * Southeast.
+		 */
+		Southeast,
+		/**
+		 * South.
+		 */
+		South,
+		/**
+		 * Southwest.
+		 */
+		Southwest,
+		/**
+		 * West.
+		 */
+		West,
+		/**
+		 * Northwest.
+		 */
+		Northwest,
+		/**
+		 * Stop (i.e. quit).
+		 */
+		Stop;
 	}
 }
