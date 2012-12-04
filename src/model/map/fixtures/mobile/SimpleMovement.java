c5cd40a71acf312e10be3fe6224dd705a6993113
Jonathan Lovelace <kingjon3377@gmail.com>
2012-12-04 14:51:41 -0500

    A simplistic data class that knows how many MP a normal land unit takes to move over each kind of terrain.

---
+++ src/model/map/fixtures/mobile/SimpleMovement.java
@@ -0,0 +1,93 @@
+package model.map.fixtures.mobile;
+
+import model.map.Tile;
+import model.map.TileFixture;
+import model.map.TileType;
+import model.map.fixtures.terrain.Forest;
+import model.map.fixtures.terrain.Hill;
+import model.map.fixtures.terrain.Mountain;
+import util.EqualsAny;
+
+/**
+ * A class encapsulating knowledge about movement costs associated with various
+ * tile types. FIXME: This ought to be per-unit-type, rather than one
+ * centralized set of figures.
+ *
+ * @author Jonathan Lovelace
+ *
+ */
+public final class SimpleMovement {
+	/**
+	 * Do not instantiate.
+	 */
+	private SimpleMovement() {
+		// Do not instantiate.
+	}
+	/**
+	 * An exception thrown to signal traversal is impossible. FIXME: Ocean isn't
+	 * impassable to everything, of course.
+	 */
+	public static final class TraversalImpossibleException extends Exception {
+		/**
+		 * Constructor.
+		 */
+		public TraversalImpossibleException() {
+			super("Traversal is impossible.");
+		}
+	}
+	/**
+	 * @param tile a tile
+	 * @return the movement cost to traverse it.
+	 * @throws TraversalImpossibleException if the tile is impassable.
+	 */
+	public static int getMovementCost(final Tile tile) throws TraversalImpossibleException {
+		if (isForest(tile) || isHill(tile)
+				|| TileType.Desert.equals(tile.getTerrain())) {
+			return 3; // NOPMD
+		} else if (TileType.Jungle.equals(tile.getTerrain())) {
+			return 6; // NOPMD
+		} else if (EqualsAny.equalsAny(tile.getTerrain(), TileType.Steppe,
+				TileType.Plains, TileType.Tundra)) {
+			return 2;
+		} else if (TileType.Ocean.equals(tile.getTerrain())) {
+			throw new TraversalImpossibleException();
+		} else {
+			throw new IllegalArgumentException("Unknown tile type");
+		}
+	}
+	/**
+	 * @param tile a tile
+	 * @return whether it is or contains a forest
+	 */
+	@SuppressWarnings("deprecation")
+	private static boolean isForest(final Tile tile) {
+		if (EqualsAny.equalsAny(tile.getTerrain(), TileType.BorealForest,
+				TileType.TemperateForest)) {
+			return true; // NOPMD
+		} else {
+			for (TileFixture fix : tile) {
+				if (fix instanceof Forest) {
+					return true; // NOPMD
+				}
+			}
+			return false;
+		}
+	}
+	/**
+	 * @param tile a tile
+	 * @return whether it is mountainous or hilly
+	 */
+	@SuppressWarnings("deprecation")
+	private static boolean isHill(final Tile tile) {
+		if (TileType.Mountain.equals(tile.getTerrain())) {
+			return true; // NOPMD
+		} else {
+			for (TileFixture fix : tile) {
+				if (fix instanceof Mountain || fix instanceof Hill) {
+					return true; // NOPMD
+				}
+			}
+			return false;
+		}
+	}
+}
