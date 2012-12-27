81b44a351eb3d3ae2273cd9ba298f80c28704765
Jonathan Lovelace <kingjon3377@gmail.com>
2012-12-27 10:07:12 -0500

    An InputStream implementation to replace FileLoader.

---
+++ src/util/ResourceInputStream.java
@@ -0,0 +1,92 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+/**
+ * Basically a FileInputStream, but the file could be on disk or in the classpath.
+ * @author Jonathan Lovelace
+ *
+ */
+public class ResourceInputStream extends InputStream {
+	/**
+	 * The stream we wrap.
+	 */
+	private final InputStream wrapped;
+	/**
+	 * Constructor.
+	 * @param filename the name of the file to read
+	 * @throws FileNotFoundException if it's not found on disk or in the classpath
+	 */
+	public ResourceInputStream(final String filename) throws FileNotFoundException {
+		// ESCA-JAVA0177:
+		InputStream temp;
+		try {
+			temp = new FileInputStream(filename);
+		} catch (final FileNotFoundException fe) {
+			temp = ResourceInputStream.class.getClassLoader().getResourceAsStream(
+					filename);
+			if (temp == null) {
+				throw fe;
+			}
+		}
+		wrapped = temp;
+	}
+	/**
+	 * @return the result of reading from the file
+	 * @throws IOException when thrown by wrapped stream
+	 *
+	 * @see java.io.InputStream#read()
+	 */
+	@Override
+	public int read() throws IOException {
+		return wrapped.read();
+	}
+	/**
+	 * @param buf a buffer into which to read
+	 * @return the number of bytes read
+	 * @throws IOException when thrown by wrapped stream
+	 */
+	@Override
+	public int read(final byte[] buf) throws IOException {
+		return wrapped.read(buf);
+	}
+
+	/**
+	 * @param buf the buffer into which to read the data
+	 * @param off the offset in the array
+	 * @param len the maximum number of bytes to read
+	 * @return the total number of bytes read into the buffer, or 1 if EOF
+	 * @throws IOException when thrown by wrapped implementation
+	 * @see InputStream#read(byte[], int, int)
+	 */
+	@Override
+	public int read(final byte[] buf, final int off, final int len) throws IOException {
+		return wrapped.read(buf, off, len);
+	}
+	/**
+	 * @param num how many bytes to skip
+	 * @return the actual number of bytes skipped
+	 * @throws IOException if thrown by wrapped implementation.
+	 */
+	@Override
+	public long skip(final long num) throws IOException {
+		return wrapped.skip(num);
+	}
+	/**
+	 * @return an estimate of the number of bytes that can be read or skipped over in the stream without blocking
+	 * @throws IOException if thrown by wrapped implementation
+	 */
+	@Override
+	public int available() throws IOException {
+		return wrapped.available();
+	}
+	/**
+	 * @throws IOException if thrown by wrapped implementation
+	 */
+	@Override
+	public void close() throws IOException {
+		wrapped.close();
+	}
+}
