d3acc3d2fa71655f3a785e14bcee6b5621b3ca70
Jonathan Lovelace <kingjon3377@gmail.com>
2013-02-01 10:26:27 -0500

    Add an exception to the interface.

--- src/controller/map/drivers/ISPDriver.java
+++ src/controller/map/drivers/ISPDriver.java
@@ -12,6 +12,22 @@ public interface ISPDriver {
 	 * Run the driver. If the driver is a GUIDriver, this should use
 	 * SwingUtilities.invokeLater(); if it's a CLIDriver, that's not necessary.
 	 * @param args any command-line arguments that should be passed to the driver.
+	 * @throws DriverFailedException if it's impossible for the driver to start.
 	 */
-	void startDriver(final String... args);
+	void startDriver(final String... args) throws DriverFailedException;
+
+	/**
+	 * An exception to throw when the driver fails ... such as if the map is
+	 * improperly formatted, etc. This means we don't have to declare a long
+	 * list of possible exceptional circumstances.
+	 */
+	class DriverFailedException extends Exception {
+		/**
+		 * Constructor.
+		 * @param cause the exception we're wrapping. Should *not* be null.
+		 */
+		public DriverFailedException(final Throwable cause) {
+			super("The driver could not start because of an exception:", cause);
+		}
+	}
 }
