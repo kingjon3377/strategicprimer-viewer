d47362f0b22914477b092996891a5682ab60dde3
Jonathan Lovelace <kingjon3377@gmail.com>
2013-04-03 15:48:10 -0400

    Extract most of the rest of the logic---the second panel---into its own class too.

--- src/view/exploration/ExplorationFrame.java
+++ src/view/exploration/ExplorationFrame.java
@@ -3,34 +3,14 @@ package view.exploration;
 import java.awt.CardLayout;
 import java.awt.Container;
 import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
-import java.util.EnumMap;
-import java.util.Iterator;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
-import javax.swing.JTextField;
 
 import model.exploration.ExplorationModel;
-import model.exploration.IExplorationModel.Direction;
-import model.map.IMap;
-import model.map.Point;
-import model.map.Tile;
-import model.map.TileType;
-import util.IsNumeric;
-import util.Pair;
 import util.PropertyChangeSource;
-import util.PropertyChangeSupportSource;
-import view.map.details.FixtureList;
 
 /**
  * The main window for the exploration GUI.
@@ -40,8 +20,7 @@ import view.map.details.FixtureList;
  *
  * @author Jonathan Lovelace
  */
-public class ExplorationFrame extends JFrame implements PropertyChangeSource,
-		PropertyChangeListener {
+public class ExplorationFrame extends JFrame implements PropertyChangeSource {
 	/**
 	 * The exploration model.
 	 */
@@ -72,133 +51,17 @@ public class ExplorationFrame extends JFrame implements PropertyChangeSource,
 				}
 			}
 		});
-		add(esp);
-
-		final JPanel headerPanel = new JPanel();
-		headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.LINE_AXIS));
-		final JButton backButton = new JButton("Select a different explorer");
-		backButton.addActionListener(new ActionListener() {
+		explorationPanel.addPropertyChangeListener(new PropertyChangeListener() {
 			@Override
-			public void actionPerformed(final ActionEvent event) {
-				esp.validate();
-				layout.first(outer);
+			public void propertyChange(final PropertyChangeEvent evt) {
+				if ("switch".equalsIgnoreCase(evt.getPropertyName())) {
+					esp.validate();
+					layout.first(outer);
+				}
 			}
 		});
-		headerPanel.add(backButton);
-		headerPanel.add(locLabel);
-		headerPanel.add(new JLabel("Remaining Movement Points: "));
-		mpField = new JTextField(esp.getMPDocument(), null, 5);
-		headerPanel.add(mpField);
-		explorationPanel.setTopComponent(headerPanel);
-		final JPanel tilePanel = new JPanel(new GridLayout(3, 12, 2, 2));
-		addTileGUI(tilePanel, Direction.Northwest);
-		addTileGUI(tilePanel, Direction.North);
-		addTileGUI(tilePanel, Direction.Northeast);
-		addTileGUI(tilePanel, Direction.West);
-		addTileGUI(tilePanel, Direction.Nowhere);
-		addTileGUI(tilePanel, Direction.East);
-		addTileGUI(tilePanel, Direction.Southwest);
-		addTileGUI(tilePanel, Direction.South);
-		addTileGUI(tilePanel, Direction.Southeast);
-		explorationPanel.setBottomComponent(tilePanel);
+		add(esp);
 		add(explorationPanel);
-		emodel.addPropertyChangeListener(this);
 		pack();
 	}
-
-	/**
-	 * The collection of proxies for main-map tile-fixture-lists.
-	 */
-	private final EnumMap<Direction, PropertyChangeSupportSource> mains = new EnumMap<Direction, PropertyChangeSupportSource>(
-			Direction.class);
-	/**
-	 * The collection of proxies for secondary-map tile-fixture lists.
-	 */
-	private final EnumMap<Direction, PropertyChangeSupportSource> seconds = new EnumMap<Direction, PropertyChangeSupportSource>(
-			Direction.class);
-	/**
-	 * The collection of dual-tile-buttons.
-	 */
-	private final EnumMap<Direction, DualTileButton> buttons = new EnumMap<Direction, DualTileButton>(
-			Direction.class);
-
-	/**
-	 * Set up the GUI representation of a tile---a list of its contents in the
-	 * main map, a visual representation, and a list of its contents in a
-	 * secondary map.
-	 *
-	 * @param panel the panel to add them to
-	 * @param direction which direction from the currently selected tile this
-	 *        GUI represents.
-	 */
-	private void addTileGUI(final JPanel panel, final Direction direction) {
-		final PropertyChangeSupportSource mainPCS = new PropertyChangeSupportSource(
-				this);
-		final FixtureList mainList = new FixtureList(panel, mainPCS);
-		panel.add(new JScrollPane(mainList));
-		final DualTileButton dtb = new DualTileButton();
-		// panel.add(new JScrollPane(dtb));
-		panel.add(dtb);
-		final ExplorationClickListener ecl = new ExplorationClickListener(model, direction, mainList);
-		dtb.addActionListener(ecl);
-		ecl.addPropertyChangeListener(this);
-		mainList.getModel().addListDataListener(new ExplorationListListener(model, mainList));
-		final PropertyChangeSupportSource secPCS = new PropertyChangeSupportSource(
-				this);
-		panel.add(new JScrollPane(new FixtureList(panel, secPCS)));
-		mains.put(direction, mainPCS);
-		buttons.put(direction, dtb);
-		seconds.put(direction, secPCS);
-	}
-
-	/**
-	 * Handle change in selected location.
-	 *
-	 * @param evt the event to handle
-	 */
-	@Override
-	public final void propertyChange(final PropertyChangeEvent evt) {
-		if ("point".equalsIgnoreCase(evt.getPropertyName())) {
-			final Point selPoint = model.getSelectedUnitLocation();
-			for (final Direction dir : Direction.values()) {
-				final Point point = model.getDestination(selPoint, dir);
-				final Tile tileOne = model.getMap().getTile(point);
-				final Iterator<Pair<IMap, String>> subs = model
-						.getSubordinateMaps().iterator();
-				// ESCA-JAVA0177:
-				final Tile tileTwo; // NOPMD
-				if (subs.hasNext()) {
-					tileTwo = subs.next().first().getTile(point);
-				} else {
-					tileTwo = new Tile(TileType.NotVisible); // NOPMD
-				}
-				mains.get(dir).firePropertyChange("tile", null, tileOne);
-				seconds.get(dir).firePropertyChange("tile", null, tileTwo);
-				buttons.get(dir).setTiles(tileOne, tileTwo);
-
-			}
-			locLabel.setText("<html><body>Currently exploring "
-					+ model.getSelectedUnitLocation()
-					+ "; click a tile to explore it. "
-					+ "Selected fixtures in its left-hand list will be 'discovered'.</body></html>");
-		} else if ("cost".equalsIgnoreCase(evt.getPropertyName())
-				&& evt.getNewValue() instanceof Integer) {
-			final int cost = ((Integer) evt.getNewValue()).intValue();
-			if (IsNumeric.isNumeric(mpField.getText().trim())) {
-				int mpoints = Integer.parseInt(mpField.getText().trim());
-				mpoints -= cost;
-				mpField.setText(Integer.toString(mpoints));
-			}
-		}
-	}
-	/**
-	 * The text-field containing the running MP total.
-	 */
-	private final JTextField mpField;
-	/**
-	 * The label showing the current location of the explorer.
-	 */
-	private final JLabel locLabel = new JLabel(
-			"<html><body>Currently exploring (-1, -1); click a tile to explore it. "
-					+ "Selected fixtures in its left-hand list will be 'discovered'.</body></html>");
 }
---
+++ src/view/exploration/ExplorationPanel.java
@@ -0,0 +1,184 @@
+package view.exploration;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.EnumMap;
+import java.util.Iterator;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+import javax.swing.text.Document;
+
+import model.exploration.IExplorationModel;
+import model.exploration.IExplorationModel.Direction;
+import model.map.IMap;
+import model.map.Point;
+import model.map.Tile;
+import model.map.TileType;
+import util.IsNumeric;
+import util.Pair;
+import util.PropertyChangeSupportSource;
+import view.map.details.FixtureList;
+/**
+ * A panel to let the user explore using a unit.
+ * @author Jonathan Lovelace
+ */
+public class ExplorationPanel extends JPanel implements ActionListener, PropertyChangeListener {
+	/**
+	 * The exploration model.
+	 */
+	private final IExplorationModel model;
+	/**
+	 * The text for the 'back' button.
+	 */
+	private static final String BACK_TEXT = "Select a different explorer";
+	/**
+	 * Constructor.
+	 * @param emodel the exploration model.
+	 * @param mpDoc the model underlying the remaining-MP text boxes.
+	 */
+	public ExplorationPanel(final IExplorationModel emodel, final Document mpDoc) {
+		super(new BorderLayout());
+		model = emodel;
+		final JSplitPane impl = new JSplitPane(JSplitPane.VERTICAL_SPLIT);
+		final JPanel headerPanel = new JPanel();
+		headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.LINE_AXIS));
+		final JButton backButton = new JButton(BACK_TEXT);
+		backButton.addActionListener(this);
+		headerPanel.add(backButton);
+		headerPanel.add(locLabel);
+		headerPanel.add(new JLabel("Remaining Movement Points: "));
+		mpField = new JTextField(mpDoc, null, 5);
+		headerPanel.add(mpField);
+		impl.setTopComponent(headerPanel);
+		/**
+		 * TODO: Make the tilePanel and its logic a separate class.
+		 */
+		final JPanel tilePanel = new JPanel(new GridLayout(3, 12, 2, 2));
+		addTileGUI(tilePanel, Direction.Northwest);
+		addTileGUI(tilePanel, Direction.North);
+		addTileGUI(tilePanel, Direction.Northeast);
+		addTileGUI(tilePanel, Direction.West);
+		addTileGUI(tilePanel, Direction.Nowhere);
+		addTileGUI(tilePanel, Direction.East);
+		addTileGUI(tilePanel, Direction.Southwest);
+		addTileGUI(tilePanel, Direction.South);
+		addTileGUI(tilePanel, Direction.Southeast);
+		impl.setBottomComponent(tilePanel);
+		add(impl, BorderLayout.CENTER);
+		emodel.addPropertyChangeListener(this);
+	}
+	/**
+	 * Handle a button press.
+	 * @param evt the event to handle.
+	 */
+	@Override
+	public void actionPerformed(final ActionEvent evt) {
+		if (BACK_TEXT.equalsIgnoreCase(evt.getActionCommand())) {
+			firePropertyChange("switch", false, true);
+		}
+	}
+	/**
+	 * The label showing the current location of the explorer.
+	 */
+	private final JLabel locLabel = new JLabel(
+			"<html><body>Currently exploring (-1, -1); click a tile to explore it. "
+					+ "Selected fixtures in its left-hand list will be 'discovered'.</body></html>");
+	/**
+	 * The text-field containing the running MP total.
+	 */
+	private final JTextField mpField;
+	/**
+	 * The collection of proxies for main-map tile-fixture-lists.
+	 */
+	private final EnumMap<Direction, PropertyChangeSupportSource> mains = new EnumMap<Direction, PropertyChangeSupportSource>(
+			Direction.class);
+	/**
+	 * The collection of proxies for secondary-map tile-fixture lists.
+	 */
+	private final EnumMap<Direction, PropertyChangeSupportSource> seconds = new EnumMap<Direction, PropertyChangeSupportSource>(
+			Direction.class);
+	/**
+	 * The collection of dual-tile-buttons.
+	 */
+	private final EnumMap<Direction, DualTileButton> buttons = new EnumMap<Direction, DualTileButton>(
+			Direction.class);
+
+	/**
+	 * Set up the GUI representation of a tile---a list of its contents in the
+	 * main map, a visual representation, and a list of its contents in a
+	 * secondary map.
+	 *
+	 * @param panel the panel to add them to
+	 * @param direction which direction from the currently selected tile this
+	 *        GUI represents.
+	 */
+	private void addTileGUI(final JPanel panel, final Direction direction) {
+		final PropertyChangeSupportSource mainPCS = new PropertyChangeSupportSource(
+				this);
+		final FixtureList mainList = new FixtureList(panel, mainPCS);
+		panel.add(new JScrollPane(mainList));
+		final DualTileButton dtb = new DualTileButton();
+		// panel.add(new JScrollPane(dtb));
+		panel.add(dtb);
+		final ExplorationClickListener ecl = new ExplorationClickListener(model, direction, mainList);
+		dtb.addActionListener(ecl);
+		ecl.addPropertyChangeListener(this);
+		mainList.getModel().addListDataListener(new ExplorationListListener(model, mainList));
+		final PropertyChangeSupportSource secPCS = new PropertyChangeSupportSource(
+				this);
+		panel.add(new JScrollPane(new FixtureList(panel, secPCS)));
+		mains.put(direction, mainPCS);
+		buttons.put(direction, dtb);
+		seconds.put(direction, secPCS);
+	}
+	/**
+	 * Handle change in selected location.
+	 *
+	 * @param evt the event to handle
+	 */
+	@Override
+	public final void propertyChange(final PropertyChangeEvent evt) {
+		if ("point".equalsIgnoreCase(evt.getPropertyName())) {
+			final Point selPoint = model.getSelectedUnitLocation();
+			for (final Direction dir : Direction.values()) {
+				final Point point = model.getDestination(selPoint, dir);
+				final Tile tileOne = model.getMap().getTile(point);
+				final Iterator<Pair<IMap, String>> subs = model
+						.getSubordinateMaps().iterator();
+				// ESCA-JAVA0177:
+				final Tile tileTwo; // NOPMD
+				if (subs.hasNext()) {
+					tileTwo = subs.next().first().getTile(point);
+				} else {
+					tileTwo = new Tile(TileType.NotVisible); // NOPMD
+				}
+				mains.get(dir).firePropertyChange("tile", null, tileOne);
+				seconds.get(dir).firePropertyChange("tile", null, tileTwo);
+				buttons.get(dir).setTiles(tileOne, tileTwo);
+
+			}
+			locLabel.setText("<html><body>Currently exploring "
+					+ model.getSelectedUnitLocation()
+					+ "; click a tile to explore it. "
+					+ "Selected fixtures in its left-hand list will be 'discovered'.</body></html>");
+		} else if ("cost".equalsIgnoreCase(evt.getPropertyName())
+				&& evt.getNewValue() instanceof Integer) {
+			final int cost = ((Integer) evt.getNewValue()).intValue();
+			if (IsNumeric.isNumeric(mpField.getText().trim())) {
+				int mpoints = Integer.parseInt(mpField.getText().trim());
+				mpoints -= cost;
+				mpField.setText(Integer.toString(mpoints));
+			}
+		}
+	}
+}
