f335b8833c904ce86149ef25829ba3878b4cb984
Jonathan Lovelace <kingjon3377@gmail.com>
2013-08-17 14:45:05 -0400

    Fix warnings by removing unused imports.

--- src/view/worker/AdvancementFrame.java
+++ src/view/worker/AdvancementFrame.java
@@ -1,6 +1,5 @@
 package view.worker;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,7 +11,6 @@ import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTree;
