f7257d5f024e012bdde1aa9acacf14c66342068e
Jonathan Lovelace <kingjon3377@gmail.com>
2013-08-25 00:34:05 -0400

    Fix a ClassCastException; the instanceof guarantee didn't last as long a I thought it did, so add parentheses to make sure.

--- src/view/map/main/FindDialog.java
+++ src/view/map/main/FindDialog.java
@@ -156,14 +156,13 @@ public class FindDialog extends JDialog implements ActionListener {
 						|| (fix instanceof HasName && ((HasName) fix).getName()
 								.contains(pattern))
 						|| (fix instanceof HasKind && ((HasKind) fix).getKind()
-								.contains(pattern)) || (fix instanceof HasOwner
-						&& ((HasOwner) fix).getOwner().getName()
-								.contains(pattern)
+								.contains(pattern)) || (fix instanceof HasOwner && (((HasOwner) fix)
+						.getOwner().getName().contains(pattern)
 						|| "me".equalsIgnoreCase(pattern.trim())
 						&& ((HasOwner) fix).getOwner().isCurrent()
 						|| "none".equalsIgnoreCase(pattern.trim())
 						&& ((HasOwner) fix).getOwner().isIndependent() || ((HasOwner) fix)
-						.getOwner().getPlayerId() == idNum))) {
+						.getOwner().getPlayerId() == idNum)))) {
 			return true; // NOPMD
 		} else if (fix instanceof FixtureIterable<?>) {
 			for (IFixture member : (FixtureIterable<?>) fix) {
