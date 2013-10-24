233d201892a074edf822eb934c77a70eff0c7ce9
Jonathan Lovelace <kingjon3377@gmail.com>
2013-10-23 21:40:53 -0400

    Make the SkillList accept level notifications on behalf of its model, and move responsibility for setting up listening to its caller.

--- src/view/worker/SkillList.java
+++ src/view/worker/SkillList.java
@@ -10,6 +10,7 @@ import javax.swing.event.ListSelectionListener;
 
 import model.listeners.CompletionListener;
 import model.listeners.JobSelectionSource;
+import model.listeners.LevelGainListener;
 import model.listeners.LevelGainSource;
 import model.listeners.SkillSelectionListener;
 import model.listeners.SkillSelectionSource;
@@ -26,21 +27,16 @@ import org.eclipse.jdt.annotation.Nullable;
  *
  */
 public class SkillList extends JList<Skill> implements ListSelectionListener,
-		SkillSelectionSource {
+		SkillSelectionSource, LevelGainListener {
 	/**
 	 * Constructor.
 	 *
 	 * @param jsSources what our model should listen to for the currently selected Job
-	 * @param lgSources what our model should listen to for skill leveling notifications
 	 */
-	public SkillList(final JobSelectionSource[] jsSources,
-			final LevelGainSource[] lgSources) {
-		final SkillListModel lmodel = new SkillListModel();
+	public SkillList(final JobSelectionSource[] jsSources) {
+		lmodel = new SkillListModel();
 		for (final JobSelectionSource source : jsSources) {
 			source.addJobSelectionListener(lmodel);
-		}
-		for (final LevelGainSource source : lgSources) {
-			source.addLevelGainListener(lmodel);
 		}
 		setModel(lmodel);
 		lmodel.addCompletionListener(new CompletionListener() {
@@ -73,6 +69,10 @@ public class SkillList extends JList<Skill> implements ListSelectionListener,
 	 * The list of completion listeners listening to us.
 	 */
 	private final List<SkillSelectionListener> ssListeners = new ArrayList<>();
+	/**
+	 * The list model.
+	 */
+	private final SkillListModel lmodel;
 
 	/**
 	 * @param list a listener to add
@@ -88,5 +88,14 @@ public class SkillList extends JList<Skill> implements ListSelectionListener,
 	@Override
 	public final void removeSkillSelectionListener(final SkillSelectionListener list) {
 		ssListeners.remove(list);
+	}
+
+	/**
+	 * We just pass the event on to the list model, which is the object *really*
+	 * concerned about level events.
+	 */
+	@Override
+	public void level() {
+		lmodel.level();
 	}
 }
