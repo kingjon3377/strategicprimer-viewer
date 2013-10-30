1027a4d4f72c48025b0b490bc396fe9934571aee
Jonathan Lovelace <kingjon3377@gmail.com>
2013-10-30 01:20:09 -0400

    Reduce method length by extracting local variables to only do casts once and by creating helper methods for common cases.

--- src/controller/map/misc/HarvestableComparator.java
+++ src/controller/map/misc/HarvestableComparator.java
@@ -2,6 +2,7 @@ package controller.map.misc;
 
 import java.util.Comparator;
 
+import model.map.HasKind;
 import model.map.fixtures.resources.Battlefield;
 import model.map.fixtures.resources.CacheFixture;
 import model.map.fixtures.resources.Cave;
@@ -37,51 +38,46 @@ public class HarvestableComparator implements Comparator<HarvestableFixture> { /
 			} else if (one instanceof CacheFixture
 					&& two instanceof CacheFixture) {
 				// FindBugs objected if we didn't do both instanceofs
-				return repeatedCompare(IntPair.of(((CacheFixture) one)// NOPMD
-						.getKind().hashCode(), ((CacheFixture) two).getKind()
-						.hashCode()), IntPair.of(((CacheFixture) one)
-						.getContents().hashCode(), ((CacheFixture) two)
-						.getContents().hashCode()), idPair(one, two));
+				final CacheFixture oneT = (CacheFixture) one;
+				final CacheFixture twoT = (CacheFixture) two;
+				return repeatedCompare(kindPair(oneT, twoT),
+						hashPair(oneT.getContents(), twoT.getContents()),
+						idPair(one, two));
 			} else if (one instanceof Grove && two instanceof Grove) {
-				return repeatedCompare(IntPair.of(((Grove) one).getKind()// NOPMD
-						.hashCode(), ((Grove) two).getKind().hashCode()),
-						IntPair.of(Boolean.compare(((Grove) one).isOrchard(),
-								((Grove) two).isOrchard()), 0), IntPair.of(
-								Boolean.compare(((Grove) one).isCultivated(),
-										((Grove) two).isCultivated()), 0),
+				final Grove oneT = (Grove) one;
+				final Grove twoT = (Grove) two;
+				return repeatedCompare(kindPair(oneT, twoT),
+						boolPair(oneT.isOrchard(), twoT.isOrchard()),
+						boolPair(oneT.isCultivated(), twoT.isCultivated()),
 						idPair(one, two));
 			} else if (one instanceof Meadow && two instanceof Meadow) {
-				return repeatedCompare(IntPair.of(((Meadow) one).getKind()// NOPMD
-						.hashCode(), ((Meadow) two).getKind().hashCode()),
-						IntPair.of(Boolean.compare(((Meadow) one).isField(),
-								((Meadow) two).isField()), 0), IntPair.of(
-								Boolean.compare(((Meadow) one).isCultivated(),
-										((Meadow) two).isCultivated()), 0),
-						IntPair.of(((Meadow) one).getStatus().ordinal(),
-								((Meadow) two).getStatus().ordinal()),
+				final Meadow oneT = (Meadow) one;
+				final Meadow twoT = (Meadow) two;
+				return repeatedCompare(kindPair(oneT, twoT),
+						boolPair(oneT.isField(), twoT.isField()),
+						boolPair(oneT.isCultivated(), twoT.isCultivated()),
+						enumPair(oneT.getStatus(), twoT.getStatus()),
 						idPair(one, two));
 			} else if (one instanceof Mine && two instanceof Mine) {
-				return repeatedCompare(IntPair.of(((Mine) one).getKind()// NOPMD
-						.hashCode(), ((Mine) two).getKind().hashCode()),
-						IntPair.of(((Mine) one).getStatus().ordinal(),
-								((Mine) two).getStatus().ordinal()),
+				final Mine oneT = (Mine) one;
+				final Mine twoT = (Mine) two;
+				return repeatedCompare(kindPair(oneT, twoT),
+						enumPair(oneT.getStatus(), twoT.getStatus()),
 						idPair(one, two));
 			} else if (one instanceof MineralVein && two instanceof MineralVein) {
-				return repeatedCompare(IntPair.of(((MineralVein) one).getKind()// NOPMD
-						.hashCode(), ((MineralVein) two).getKind().hashCode()),
-						IntPair.of(Boolean.compare(
-								((MineralVein) one).isExposed(),
-								((MineralVein) two).isExposed()), 0),
+				final MineralVein oneT = (MineralVein) one;
+				final MineralVein twoT = (MineralVein) two;
+				return repeatedCompare(kindPair(oneT, twoT),
+						boolPair(oneT.isExposed(), twoT.isExposed()),
 						idPair(one, two));
 			} else if (one instanceof Shrub && two instanceof Shrub) {
-				return repeatedCompare(IntPair.of(((Shrub) one).getKind()// NOPMD
-						.hashCode(), ((Shrub) two).getKind().hashCode()),
+				return repeatedCompare(kindPair((Shrub) one, (Shrub) two),
 						idPair(one, two));
 			} else if (one instanceof StoneDeposit
 					&& two instanceof StoneDeposit) {
-				return repeatedCompare(IntPair.of(((StoneDeposit) one).stone()// NOPMD
-						.ordinal(), ((StoneDeposit) two).stone().ordinal()),
-						idPair(one, two));
+				return repeatedCompare(
+						enumPair(((StoneDeposit) one).stone(),
+								((StoneDeposit) two).stone()), idPair(one, two));
 			} else {
 				throw new IllegalStateException(
 						"Unhandled Harvestable implementation or impossible condition");
@@ -115,6 +111,39 @@ public class HarvestableComparator implements Comparator<HarvestableFixture> { /
 	private static IntPair idPair(final HarvestableFixture one,
 			final HarvestableFixture two) {
 		return IntPair.of(one.getID(), two.getID());
+	}
+	/**
+	 * @param one a boolean value
+	 * @param two another boolean value
+	 * @return an IntPair of the result of the comparison between them and zero.
+	 */
+	private static IntPair boolPair(final boolean one, final boolean two) {
+		return IntPair.of(Boolean.compare(one, two), 0);
+	}
+	/**
+	 * @param one an enum value
+	 * @param two another enum value
+	 * @param <T> their type
+	 * @return an IntPair of their ordinals.
+	 */
+	private static <T extends Enum<T>> IntPair enumPair(final T one, final T two) {
+		return IntPair.of(one.ordinal(), two.ordinal());
+	}
+	/**
+	 * @param one a string value
+	 * @param two another string value
+	 * @return an IntPair of their hash values.
+	 */
+	private static IntPair hashPair(final String one, final String two) {
+		return IntPair.of(one.hashCode(), two.hashCode());
+	}
+	/**
+	 * @param one an object
+	 * @param two another object
+	 * @return an IntPair of the hash values of their "kind" properties
+	 */
+	private static IntPair kindPair(final HasKind one, final HasKind two) {
+		return IntPair.of(one.getKind().hashCode(), two.getKind().hashCode());
 	}
 	/**
 	 * @return a String representation of the object
