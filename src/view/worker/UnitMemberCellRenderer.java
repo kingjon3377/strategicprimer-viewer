bbacce96b8a7e2e3aa783c9e942f072333c39ab1
Jonathan Lovelace <kingjon3377@gmail.com>
2014-01-11 21:40:03 -0500

    Remove unused imports.

--- src/view/worker/UnitMemberCellRenderer.java
+++ src/view/worker/UnitMemberCellRenderer.java
@@ -12,7 +12,6 @@ import java.util.logging.Logger;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JLabel;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
