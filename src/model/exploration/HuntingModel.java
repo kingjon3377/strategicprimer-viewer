af15b8358f47145855d8db4e0bb5b26aa33c7437
Jonathan Lovelace <kingjon3377@gmail.com>
2014-07-09 18:48:20 -0400

    Move SurroundingPointIterable to its own file.

--- src/model/exploration/HuntingModel.java
+++ src/model/exploration/HuntingModel.java
@@ -1,13 +1,11 @@
 package model.exploration;
 
-import static model.map.PointFactory.point;
 import static model.map.TileType.Ocean;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -206,98 +204,6 @@ public class HuntingModel {
 		return retval;
 	}
 	
-	/**
-	 * An iterator over the twenty-five points (including itself) surrounding a
-	 * point.
-	 * 
-	 * @author Jonathan Lovelace
-	 */
-	private static class SurroundingPointIterable implements Iterable<Point> {
-		/**
-		 * Two below the point.
-		 */
-		private static final int TWO_BELOW = -2;
-		/**
-		 * Two above the point. (Since we loop to an exclusive bound, using
-		 * less-than not less-than-or-equals)
-		 */
-		private static final int TWO_ABOVE = 3;
-		/**
-		 * How many times the current point needs to be added to make it come up
-		 * half the time.
-		 * 
-		 * (TODO: is that really what's going on? And should we perhaps just add
-		 * the current tile in one of the other loops instead of giving it its
-		 * own?)
-		 */
-		private static final int HALF_COUNT = 14;
-		/**
-		 * the list of points.
-		 */
-		private final List<Point> points = new ArrayList<>();
-		/**
-		 * @param starting the starting point.
-		 * @param dimensions the dimensions of the map
-		 */
-		protected SurroundingPointIterable(final Point starting,
-				final MapDimensions dimensions) {
-			for (int row = TWO_BELOW; row < TWO_ABOVE; row++) {
-				for (int col = TWO_BELOW; col < TWO_ABOVE; col++) {
-					points.add(point(roundRow(starting.row + row, dimensions),
-							roundCol(starting.col + col, dimensions)));
-				}
-			}
-			for (int row = -1; row < 2; row++) {
-				for (int col = -1; col < 2; col++) {
-					points.add(point(roundRow(starting.row + row, dimensions),
-							roundCol(starting.col + col, dimensions)));
-				}
-			}
-			for (int i = 0; i < HALF_COUNT; i++) {
-				points.add(starting);
-			}
-		}
-		/**
-		 * @return an iterator over the points
-		 */
-		@Override
-		public Iterator<Point> iterator() {
-			return NullCleaner.assertNotNull(points.iterator());
-		}
-		/**
-		 * Round a column number to fit within the map.
-		 * @param col the column number
-		 * @param dims the dimensions of the map
-		 * @return its equivalent that's actually within the map
-		 */
-		protected static int roundCol(final int col, final MapDimensions dims) {
-			if (col < 0) {
-				return dims.cols + col; // NOPMD
-			} else {
-				return col % dims.cols;
-			}
-		}
-		/**
-		 * Round a row number to fit within the map.
-		 * @param row the row number
-		 * @param dims the dimensions of the map
-		 * @return its equivalent that's actually within the map
-		 */
-		protected static int roundRow(final int row, final MapDimensions dims) {
-			if (row < 0) {
-				return dims.rows + row; // NOPMD
-			} else {
-				return row % dims.rows;
-			}
-		}
-		/**
-		 * @return a String representation of the object
-		 */
-		@Override
-		public String toString() {
-			return "SurroundingPointIterable";
-		}
-	}
 	/**
 	 * @return a String representation of the object
 	 */
