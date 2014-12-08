d7eea49dd0341990e246a656f79839a73284431c
Jonathan Lovelace <kingjon3377@gmail.com>
2014-12-08 15:52:43 -0500

    Allow navigation in exploration GUI using arrow keys or numeric keypad.

--- src/view/exploration/ExplorationPanel.java
+++ src/view/exploration/ExplorationPanel.java
@@ -3,6 +3,7 @@ package view.exploration;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.text.NumberFormat;
 import java.text.ParseException;
@@ -14,11 +15,14 @@ import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.swing.AbstractAction;
+import javax.swing.InputMap;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.text.Document;
 
 import model.exploration.IExplorationModel;
@@ -83,7 +87,44 @@ public class ExplorationPanel extends BorderedPanel implements ActionListener,
 	 */
 	private final Map<Direction, DualTileButton> buttons = new EnumMap<>(
 			Direction.class);
-
+	/**
+	 * Key-bindings for dual-tile buttons: arrow keys.
+	 */
+	private static final Map<Direction, KeyStroke> ARROW_KEYS = new EnumMap<>(
+			Direction.class);
+	/**
+	 * Key bindings for dual-tile buttons: numeric keypad.
+	 */
+	private static final Map<Direction, KeyStroke> NUM_KPAD = new EnumMap<>(
+			Direction.class);
+	static {
+		ARROW_KEYS.put(Direction.North,
+				KeyStroke.getKeyStroke(KeyEvent.VK_UP, 0));
+		ARROW_KEYS.put(Direction.South,
+				KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0));
+		ARROW_KEYS.put(Direction.West,
+				KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0));
+		ARROW_KEYS.put(Direction.East,
+				KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0));
+		NUM_KPAD.put(Direction.North,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD8, 0));
+		NUM_KPAD.put(Direction.South,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD2, 0));
+		NUM_KPAD.put(Direction.West,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD4, 0));
+		NUM_KPAD.put(Direction.East,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD6, 0));
+		NUM_KPAD.put(Direction.Northeast,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD9, 0));
+		NUM_KPAD.put(Direction.Northwest,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD7, 0));
+		NUM_KPAD.put(Direction.Southeast,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD3, 0));
+		NUM_KPAD.put(Direction.Southwest,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD1, 0));
+		NUM_KPAD.put(Direction.Nowhere,
+				KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD5, 0));
+	}
 	/**
 	 * The exploration model.
 	 */
@@ -178,6 +219,15 @@ public class ExplorationPanel extends BorderedPanel implements ActionListener,
 		final ExplorationClickListener ecl = new ExplorationClickListener(
 				model, direction, mainList);
 		dtb.addActionListener(ecl);
+		final InputMap dtbIMap = dtb.getInputMap(WHEN_IN_FOCUSED_WINDOW);
+		dtbIMap.put(ARROW_KEYS.get(direction), direction.toString());
+		dtbIMap.put(NUM_KPAD.get(direction), direction.toString());
+		dtb.getActionMap().put(direction.toString(), new AbstractAction() {
+			@Override
+			public void actionPerformed(@Nullable final ActionEvent evt) {
+				ecl.actionPerformed(evt);
+			}
+		});
 		ecl.addSelectionChangeListener(this);
 		ecl.addMovementCostListener(this);
 		mainList.getModel().addListDataListener(
