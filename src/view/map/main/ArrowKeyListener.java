491e63d6aec1d9594422eb739b52a361be718535
Jonathan Lovelace <kingjon3377@gmail.com>
2015-01-25 17:31:23 -0500

    Allow the user to navigate in the viewer using the numeric keypad.

--- src/view/map/main/ArrowKeyListener.java
+++ src/view/map/main/ArrowKeyListener.java
@@ -33,10 +33,22 @@ public class ArrowKeyListener {
 		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, 0), "down");
 		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_RIGHT, 0), "right");
 		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, 0), "left");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD2, 0), "down");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD6, 0), "right");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD8, 0), "up");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD4, 0), "left");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD9, 0), "up-right");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD7, 0), "up-left");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD3, 0), "down-right");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD1, 0), "down-left");
 		actionMap.put("up", new UpListener(selListener, 1));
 		actionMap.put("down", new DownListener(selListener, 1));
 		actionMap.put("left", new LeftListener(selListener, 1));
 		actionMap.put("right", new RightListener(selListener, 1));
+		actionMap.put("up-right", new UpRightListener(selListener, 1));
+		actionMap.put("up-left", new UpLeftListener(selListener, 1));
+		actionMap.put("down-right", new DownRightListener(selListener, 1));
+		actionMap.put("down-left", new DownLeftListener(selListener, 1));
 		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_UP, CTRL_DOWN_MASK),
 				"ctrlUp");
 		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_DOWN, CTRL_DOWN_MASK),
@@ -45,10 +57,22 @@ public class ArrowKeyListener {
 				"ctrlRight");
 		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_LEFT, CTRL_DOWN_MASK),
 				"ctrlLeft");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD2, CTRL_DOWN_MASK), "ctrlDown");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD6, CTRL_DOWN_MASK), "ctrlRight");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD8, CTRL_DOWN_MASK), "ctrlUp");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD4, CTRL_DOWN_MASK), "ctrlLeft");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD9, CTRL_DOWN_MASK), "ctrl-up-right");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD7, CTRL_DOWN_MASK), "ctrl-up-left");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD3, CTRL_DOWN_MASK), "ctrl-down-right");
+		inputMap.put(KeyStroke.getKeyStroke(KeyEvent.VK_NUMPAD1, CTRL_DOWN_MASK), "ctrl-down-left");
 		actionMap.put("ctrlUp", new UpListener(selListener, 5));
 		actionMap.put("ctrlDown", new DownListener(selListener, 5));
 		actionMap.put("ctrlLeft", new LeftListener(selListener, 5));
 		actionMap.put("ctrlRight", new RightListener(selListener, 5));
+		actionMap.put("ctrl-up-right", new UpRightListener(selListener, 5));
+		actionMap.put("ctrl-up-left", new UpLeftListener(selListener, 5));
+		actionMap.put("ctrl-down-right", new DownRightListener(selListener, 5));
+		actionMap.put("ctrl-down-left", new DownLeftListener(selListener, 5));
 	}
 
 	/**
@@ -213,6 +237,122 @@ public class ArrowKeyListener {
 		@Override
 		protected void move() {
 			dsc.right();
+		}
+	}
+	/**
+	 * A listener to move the cursor up and right.
+	 * @uathor Jonathan Lovelace
+	 */
+	private static class UpRightListener extends AbstractDirListener {
+		/**
+		 * The listener that handles the motion.
+		 */
+		private final DirectionSelectionChanger dsc;
+		/**
+		 * @param selListener the listener that handles the motion
+		 * @param countNum how many times to move on each user action
+		 */
+		protected UpRightListener(final DirectionSelectionChanger selListener,
+				final int countNum) {
+			super(countNum);
+			dsc = selListener;
+		}
+
+		/**
+		 * Do the motion.
+		 */
+		@Override
+		protected void move() {
+			// TODO: Perhaps add proper support to DirectionSelectionChanger so we don't have to make two calls here
+			dsc.up();
+			dsc.right();
+		}
+	}
+	/**
+	 * A listener to move the cursor up and left.
+	 * @uathor Jonathan Lovelace
+	 */
+	private static class UpLeftListener extends AbstractDirListener {
+		/**
+		 * The listener that handles the motion.
+		 */
+		private final DirectionSelectionChanger dsc;
+		/**
+		 * @param selListener the listener that handles the motion
+		 * @param countNum how many times to move on each user action
+		 */
+		protected UpLeftListener(final DirectionSelectionChanger selListener,
+				final int countNum) {
+			super(countNum);
+			dsc = selListener;
+		}
+
+		/**
+		 * Do the motion.
+		 */
+		@Override
+		protected void move() {
+			// TODO: Perhaps add proper support to DirectionSelectionChanger so we don't have to make two calls here
+			dsc.up();
+			dsc.left();
+		}
+	}
+	/**
+	 * A listener to move the cursor down and right.
+	 * @uathor Jonathan Lovelace
+	 */
+	private static class DownRightListener extends AbstractDirListener {
+		/**
+		 * The listener that handles the motion.
+		 */
+		private final DirectionSelectionChanger dsc;
+		/**
+		 * @param selListener the listener that handles the motion
+		 * @param countNum how many times to move on each user action
+		 */
+		protected DownRightListener(final DirectionSelectionChanger selListener,
+				final int countNum) {
+			super(countNum);
+			dsc = selListener;
+		}
+
+		/**
+		 * Do the motion.
+		 */
+		@Override
+		protected void move() {
+			// TODO: Perhaps add proper support to DirectionSelectionChanger so we don't have to make two calls here
+			dsc.down();
+			dsc.right();
+		}
+	}
+	/**
+	 * A listener to move the cursor down and left.
+	 * @uathor Jonathan Lovelace
+	 */
+	private static class DownLeftListener extends AbstractDirListener {
+		/**
+		 * The listener that handles the motion.
+		 */
+		private final DirectionSelectionChanger dsc;
+		/**
+		 * @param selListener the listener that handles the motion
+		 * @param countNum how many times to move on each user action
+		 */
+		protected DownLeftListener(final DirectionSelectionChanger selListener,
+				final int countNum) {
+			super(countNum);
+			dsc = selListener;
+		}
+
+		/**
+		 * Do the motion.
+		 */
+		@Override
+		protected void move() {
+			// TODO: Perhaps add proper support to DirectionSelectionChanger so we don't have to make two calls here
+			dsc.down();
+			dsc.left();
 		}
 	}
 }
