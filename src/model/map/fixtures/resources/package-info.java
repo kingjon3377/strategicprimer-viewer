dd1a610e4abfef641376d3b4ab89c83d353546d2
Jonathan Lovelace <kingjon3377@gmail.com>
2015-02-22 14:59:23 -0500

    After porting FieldStatus and HarvestableFixture to Ceylon, remove them.

--- src/model/map/fixtures/resources/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * We want as much null-safety as we can get in Java.
- */
-@NonNullByDefault
-package model.map.fixtures.resources;
-
-import org.eclipse.jdt.annotation.NonNullByDefault; // NOPMD
