2d3bc5ecb6baa8d9de1f56c4e228d1db3365914d
Jonathan Lovelace <kingjon3377@gmail.com>
2015-07-21 23:51:01 -0400

    Extract menu creation in WorkerMenu into helper methods, as in SPMenu.

--- src/view/worker/WorkerMenu.java
+++ src/view/worker/WorkerMenu.java
@@ -13,12 +13,11 @@ import javax.swing.JMenuBar;
 
 import org.eclipse.jdt.annotation.Nullable;
 
-import view.util.DriverQuit;
-import view.util.MenuItemCreator;
-
 import com.bric.window.WindowMenu;
 
 import controller.map.misc.IOHandler;
+import view.util.DriverQuit;
+import view.util.MenuItemCreator;
 
 /**
  * A set of menus for the worker GUI.
@@ -37,6 +36,40 @@ public class WorkerMenu extends JMenuBar {
 	 */
 	public WorkerMenu(final IOHandler handler, final JFrame parent,
 			final PlayerChooserHandler pch) {
+		add(createFileMenu(handler, parent));
+		add(createEditMenu(pch));
+		add(new WindowMenu(parent));
+	}
+	/**
+	 * Create the "edit"menu.
+	 * @param pch the object to notify when the user selects a different player
+	 * @return the "edit" menu
+	 */
+	private static JMenu createEditMenu(final PlayerChooserHandler pch) {
+		final JMenu editMenu = new JMenu("Edit");
+		editMenu.setMnemonic(KeyEvent.VK_E);
+		editMenu.add(MenuItemCreator.createMenuItem(
+				PlayerChooserHandler.MENU_ITEM, KeyEvent.VK_P,
+				MenuItemCreator.createHotkey(KeyEvent.VK_P),
+				"Look at a different player's units and workers", pch));
+		editMenu.add(MenuItemCreator.createMenuItem("Reload tree",
+				KeyEvent.VK_R, MenuItemCreator.createHotkey(KeyEvent.VK_R),
+				"Refresh the view of the workers", new ActionListener() {
+
+			@Override
+			public void actionPerformed(@Nullable final ActionEvent e) {
+				pch.reload();
+			}
+		}));
+		return editMenu;
+	}
+	/**
+	 * Create the file menu.
+	 * @param handler the object to handle I/O related menu items
+	 * @param parent the menu-bar's parent window, which is the window to close on "Close"
+	 * @return the file menu
+	 */
+	private static JMenu createFileMenu(final IOHandler handler, final JFrame parent) {
 		final JMenu fileMenu = new JMenu("File");
 		fileMenu.setMnemonic(KeyEvent.VK_F);
 		fileMenu.add(MenuItemCreator.createMenuItem("Load", KeyEvent.VK_L,
@@ -87,23 +120,6 @@ public class WorkerMenu extends JMenuBar {
 						}
 					}
 				}));
-		add(fileMenu);
-		final JMenu editMenu = new JMenu("Edit");
-		editMenu.setMnemonic(KeyEvent.VK_E);
-		editMenu.add(MenuItemCreator.createMenuItem(
-				PlayerChooserHandler.MENU_ITEM, KeyEvent.VK_P,
-				MenuItemCreator.createHotkey(KeyEvent.VK_P),
-				"Look at a different player's units and workers", pch));
-		editMenu.add(MenuItemCreator.createMenuItem("Reload tree",
-				KeyEvent.VK_R, MenuItemCreator.createHotkey(KeyEvent.VK_R),
-				"Refresh the view of the workers", new ActionListener() {
-
-			@Override
-			public void actionPerformed(@Nullable final ActionEvent e) {
-				pch.reload();
-			}
-		}));
-		add(editMenu);
-		add(new WindowMenu(parent));
+		return fileMenu;
 	}
 }
