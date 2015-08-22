fe000b16b115b341bbfaf8ea9e14c0b38e8a2907
Jonathan Lovelace <kingjon3377@gmail.com>
2015-08-21 20:34:50 -0400

    Travis: Install genisoimage.

--- install-deps.sh
+++ install-deps.sh
@@ -8,3 +8,10 @@ mkdir -p "${HOME}/.ant/lib"
 mv -i jarbundler-2.4.0/jarbundler-2.4.0.jar "${HOME}/.ant/lib/"
 wget http://central.maven.org/maven2/org/eclipse/jdt/org.eclipse.jdt.annotation/1.1.0/org.eclipse.jdt.annotation-1.1.0.jar
 wget http://javagraphics.java.net/jars/WindowMenu.jar
+wget https://github.com/Hunternif/JGitVersion/releases/download/v1.0.13/jgitversion-1.0.13.jar
+mv jgitversion-1.0.13.jar "${HOME}/.ant/lib/"
+wget https://github.com/Hunternif/JGitVersion/archive/v1.0.13.tar.gz
+tar xzf v1.0.13.tar.gz
+mv JGitVersion-1.0.13/lib/jgit/*.jar JGitVersion-1.0.13/lib/gitective/*.jar "${HOME}/.ant/lib"
+sudo apt-get update -qq
+sudo apt-get install genisoimage
