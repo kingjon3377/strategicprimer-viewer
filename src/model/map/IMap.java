kingjon3377/strategicprimer-viewer

commit a31b4422c70b76e5747d2e3cfc1ef3bffa5a3966

Replace accidental calls to Sun internal NotNull annotation with the intended Eclipse NonNull.

src/model/map/IMap.java
@@ -1,6 +1,6 @@
 package model.map;
 
-import com.sun.istack.internal.NotNull;
+import org.eclipse.jdt.annotation.NonNull;
 
 /**
  * An interface for the map and any wrappers around it.
@@ -13,28 +13,35 @@ public interface IMap extends Subsettable<IMap>, Comparable<IMap> {
 	/**
 	 * @return The map's dimensions and version.
 	 */
-	@NotNull
+	@NonNull
 	MapDimensions getDimensions();
+
+	/**
+	 * Add a player to the game.
+	 *
+	 * @param player the player to add
+	 */
+	void addPlayer(@NonNull final Player player);
 
 	/**
 	 * @param point a point
 	 * @return the tile at those coordinates
 	 */
-	@NotNull
-	ITile getTile(@NotNull final Point point);
+	@NonNull
+	Tile getTile(@NonNull final Point point);
 
 	/**
 	 *
 	 * @return the players in the map
 	 */
-	@NotNull
-	IPlayerCollection getPlayers();
+	@NonNull
+	PlayerCollection getPlayers();
 
 	/**
 	 * We need this for subset calculations if nothing else.
 	 *
 	 * @return the collection of tiles.
 	 */
-	@NotNull
-	ITileCollection getTiles();
+	@NonNull
+	TileCollection getTiles();
 }

src/controller/exploration/TableDebugger.java
@@ -11,7 +11,7 @@ import model.exploration.old.ExplorationRunner;
 import model.exploration.old.MissingTableException;
 import util.TypesafeLogger;
 
-import com.sun.istack.internal.NotNull;
+import org.eclipse.jdt.annotation.NonNull;
 
 /**
  * A driver to help debug exploration tables.
@@ -55,7 +55,7 @@ public class TableDebugger {
 	 * @param explRunner an exploration runner loaded with all the tables we
 	 *        want.
 	 */
-	public TableDebugger(@NotNull final ExplorationRunner explRunner) {
+	public TableDebugger(@NonNull final ExplorationRunner explRunner) {
 		runner = explRunner;
 	}
 
@@ -65,7 +65,7 @@ public class TableDebugger {
 	 * @param out the stream to print to.
 	 * @throws MissingTableException if a referenced table isn't there
 	 */
-	public void debugTables(@NotNull final PrintStream out)
+	public void debugTables(@NonNull final PrintStream out)
 			throws MissingTableException {
 		runner.verboseRecursiveCheck(out);
 		final EncounterTable mainTable = runner.getTable("main");
@@ -87,10 +87,10 @@ public class TableDebugger {
 	 *        recursion
 	 * @throws MissingTableException if a table is missing
 	 */
-	private void debugTable(@NotNull final String before,
-			@NotNull final String after, @NotNull final EncounterTable table,
-			@NotNull final String tableName, @NotNull final PrintStream out,
-			@NotNull final Set<EncounterTable> set)
+	private void debugTable(@NonNull final String before,
+			@NonNull final String after, @NonNull final EncounterTable table,
+			@NonNull final String tableName, @NonNull final PrintStream out,
+			@NonNull final Set<EncounterTable> set)
 			throws MissingTableException {
 		if (set.contains(table)) {
 			out.print("table ");

src/model/map/IMutableMap.java
@@ -1,6 +1,6 @@
 package model.map;
 
-import com.sun.istack.internal.NotNull;
+import org.eclipse.jdt.annotation.NonNull;
 
 /**
  * An interface to specify all the mutator methods that were in IMap.
@@ -12,6 +12,6 @@ public interface IMutableMap extends IMap {
 	 *
 	 * @param player the player to add
 	 */
-	void addPlayer(@NotNull final Player player);
+	void addPlayer(@NonNull final Player player);
 
 }

commit 26672c292ef6dc4dda37892c0746a44b74a3610a

Fix build breakage accidentally introduced in a31b442

src/model/map/IMap.java
@@ -28,7 +28,7 @@ public interface IMap extends Subsettable<IMap>, Comparable<IMap> {
 	 * @return the tile at those coordinates
 	 */
 	@NonNull
-	Tile getTile(@NonNull final Point point);
+	ITile getTile(@NonNull final Point point);
 
 	/**
 	 *
@@ -43,5 +43,5 @@ public interface IMap extends Subsettable<IMap>, Comparable<IMap> {
 	 * @return the collection of tiles.
 	 */
 	@NonNull
-	TileCollection getTiles();
+	ITileCollection getTiles();
 }