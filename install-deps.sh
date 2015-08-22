6d4b19c0e3c8f301dc2e1a2248591c790361a3c0
Jonathan Lovelace <kingjon3377@gmail.com>
2015-08-21 20:40:05 -0400

    Travis: Ensure that launch4j.jar is visible to Ant.

--- install-deps.sh
+++ install-deps.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 set -ex
+mkdir -p "${HOME}/.ant/lib"
 wget http://sourceforge.net/projects/launch4j/files/launch4j-3/3.8/launch4j-3.8-linux.tgz/download -O launch4j-3.8-linux.tgz
 tar xzf launch4j-3.8-linux.tgz
+cp launch4j/launch4j.jar "${HOME}/.ant/lib"
 wget https://github.com/tofi86/Jarbundler/releases/download/v2.4.0/jarbundler-2.4.0.tar.gz
 tar xzf jarbundler-2.4.0.tar.gz
-mkdir -p "${HOME}/.ant/lib"
 mv jarbundler-2.4.0/jarbundler-2.4.0.jar "${HOME}/.ant/lib/"
 wget http://central.maven.org/maven2/org/eclipse/jdt/org.eclipse.jdt.annotation/1.1.0/org.eclipse.jdt.annotation-1.1.0.jar
 wget http://javagraphics.java.net/jars/WindowMenu.jar
