744baf793459ac43390c81d0f165b926bc2d9298
Jonathan Lovelace <kingjon3377@gmail.com>
2015-12-23 18:13:43 -0500

    QueryCLI: Fix incorrect toString.

--- src/controller/map/drivers/QueryCLI.java
+++ src/controller/map/drivers/QueryCLI.java
@@ -404,13 +404,12 @@ public final class QueryCLI implements ISPDriver {
 	}
 
 	/**
-	 * FIXME: This isn't correct
 	 * @return a String representation of the object.
 	 */
 	@SuppressWarnings("MethodReturnAlwaysConstant")
 	@Override
 	public String toString() {
-		return "ExplorationCLI";
+		return "QueryCLI";
 	}
 
 	/**
