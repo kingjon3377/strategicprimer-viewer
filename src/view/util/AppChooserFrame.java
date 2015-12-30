aefaa133424aebf56adcda067d3abe2f9b6833b6
Jonathan Lovelace <kingjon3377@gmail.com>
2015-12-30 18:53:24 -0500

    AppChooserFrame: Convert both inner classes to lambdas.

--- src/view/util/AppChooserFrame.java
+++ src/view/util/AppChooserFrame.java
@@ -7,8 +7,6 @@ import controller.map.drivers.ISPDriver;
 import controller.map.drivers.ViewerStart;
 import controller.map.drivers.WorkerStart;
 import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Collections;
 import java.util.List;
@@ -22,7 +20,6 @@ import javax.swing.JScrollPane;
 import javax.swing.SwingUtilities;
 import model.misc.IDriverModel;
 import org.eclipse.jdt.annotation.NonNull;
-import org.eclipse.jdt.annotation.Nullable;
 import util.NullCleaner;
 import util.TypesafeLogger;
 
@@ -51,6 +48,10 @@ import util.TypesafeLogger;
  */
 public final class AppChooserFrame extends JFrame {
 	/**
+	 * Logger.
+	 */
+	private static final Logger LOGGER = TypesafeLogger.getLogger(AppChooserFrame.class);
+	/**
 	 * Create a button for a target.
 	 *
 	 * @param desc   the descriptive string
@@ -60,8 +61,25 @@ public final class AppChooserFrame extends JFrame {
 	 */
 	private JButton button(final String desc, final List<String> params,
 						   final Class<? extends ISPDriver> target) {
-		return new ListenedButton(desc, new AppChoiceListenerArgs(target, params,
-																		 this));
+		return new ListenedButton(desc, evt -> {
+			try {
+				target.getConstructor().newInstance()
+						.startDriver(params.toArray(new String[params.size()]));
+			} catch (final InstantiationException | IllegalAccessException
+					               | NoSuchMethodException | InvocationTargetException
+					               | DriverFailedException except) {
+				final String msg = except.getMessage();
+				final String message = NullCleaner.valueOrDefault(msg,
+						"Exception with null message");
+				LOGGER.log(Level.SEVERE, message, except.getCause());
+				ErrorShower.showErrorDialog(this, message);
+				return;
+			}
+			SwingUtilities.invokeLater(() -> {
+				setVisible(false);
+				dispose();
+			});
+		});
 	}
 
 	/**
@@ -74,8 +92,24 @@ public final class AppChooserFrame extends JFrame {
 	 */
 	private JButton button(final String desc, final IDriverModel model,
 						   final Class<? extends ISPDriver> target) {
-		return new ListenedButton(desc, new AppChoiceListenerDriverModel(target, model,
-																				this));
+		return new ListenedButton(desc, evt -> {
+			try {
+				target.getConstructor().newInstance().startDriver(model);
+			} catch (final InstantiationException | IllegalAccessException
+					               | NoSuchMethodException | InvocationTargetException
+					               | DriverFailedException except) {
+				final String msg = except.getMessage();
+				final String message = NullCleaner.valueOrDefault(msg,
+						"Exception with null message");
+				LOGGER.log(Level.SEVERE, message, except.getCause());
+				ErrorShower.showErrorDialog(this, message);
+				return;
+			}
+			SwingUtilities.invokeLater(() -> {
+				setVisible(false);
+				dispose();
+			});
+		});
 	}
 
 	/**
@@ -128,158 +162,4 @@ public final class AppChooserFrame extends JFrame {
 		pack();
 	}
 
-	/**
-	 * A class to start the selected app with originally-command-line arguments.
-	 *
-	 * @author Jonathan Lovelace
-	 */
-	private static final class AppChoiceListenerArgs implements ActionListener {
-		/**
-		 * Logger for the inner class.
-		 */
-		private static final Logger LOGGER = TypesafeLogger
-													 .getLogger(
-															 AppChoiceListenerArgs
-																	 .class);
-		/**
-		 * The app to start.
-		 */
-		private final Class<? extends ISPDriver> app;
-		/**
-		 * The parameters.
-		 */
-		private final String[] params;
-		/**
-		 * The app-chooser frame. So we can close it properly.
-		 */
-		private final AppChooserFrame outer;
-
-		/**
-		 * Constructor.
-		 *
-		 * @param frame      the app to start if invoked
-		 * @param parameters the parameters to pass to it
-		 * @param acf        the app-chooser frame, so we can close it when something is
-		 *                   selected.
-		 */
-		protected AppChoiceListenerArgs(final Class<? extends ISPDriver> frame,
-										final List<String> parameters,
-										final AppChooserFrame acf) {
-			app = frame;
-			params = parameters.toArray(new String[parameters.size()]);
-			outer = acf;
-		}
-
-		/**
-		 * Handle button press.
-		 *
-		 * @param evt the event to handle
-		 */
-		@Override
-		public void actionPerformed(@Nullable final ActionEvent evt) {
-			try {
-				app.getConstructor().newInstance().startDriver(params);
-			} catch (final InstantiationException | IllegalAccessException
-								   | NoSuchMethodException | InvocationTargetException
-								   | DriverFailedException except) {
-				final String msg = except.getMessage();
-				final String message = NullCleaner.valueOrDefault(msg,
-						"Exception with null message");
-				LOGGER.log(Level.SEVERE, message, except.getCause());
-				ErrorShower.showErrorDialog(outer, message);
-				return;
-			}
-			SwingUtilities.invokeLater(() -> {
-				outer.setVisible(false);
-				outer.dispose();
-			});
-		}
-
-		/**
-		 * @return a String representation of the object
-		 */
-		@SuppressWarnings("MethodReturnAlwaysConstant")
-		@Override
-		public String toString() {
-			return "AppChoiceListenerArgs";
-		}
-	}
-
-	/**
-	 * A class to start the selected app with originally-command-line arguments.
-	 *
-	 * @author Jonathan Lovelace
-	 */
-	private static final class AppChoiceListenerDriverModel
-			implements ActionListener {
-		/**
-		 * Logger for the inner class.
-		 */
-		private static final Logger LOGGER = TypesafeLogger
-													 .getLogger(
-															 AppChoiceListenerArgs
-																	 .class);
-		/**
-		 * The app to start.
-		 */
-		private final Class<? extends ISPDriver> app;
-		/**
-		 * The driver model.
-		 */
-		private final IDriverModel model;
-		/**
-		 * The app-chooser frame. So we can close it properly.
-		 */
-		private final AppChooserFrame outer;
-
-		/**
-		 * Constructor.
-		 *
-		 * @param frame  the app to start if invoked
-		 * @param dmodel the driver model to pass to it
-		 * @param acf    the app-chooser frame, so we can close it when something is
-		 *               selected.
-		 */
-		protected AppChoiceListenerDriverModel(final Class<? extends ISPDriver> frame,
-											   final IDriverModel dmodel,
-											   final AppChooserFrame acf) {
-			app = frame;
-			outer = acf;
-			model = dmodel;
-		}
-
-		/**
-		 * Handle button press.
-		 *
-		 * @param evt the event to handle
-		 */
-		@Override
-		public void actionPerformed(@Nullable final ActionEvent evt) {
-			try {
-				app.getConstructor().newInstance().startDriver(model);
-			} catch (final InstantiationException | IllegalAccessException
-								   | NoSuchMethodException | InvocationTargetException
-								   | DriverFailedException except) {
-				final String msg = except.getMessage();
-				final String message = NullCleaner.valueOrDefault(msg,
-						"Exception with null message");
-				LOGGER.log(Level.SEVERE, message, except.getCause());
-				ErrorShower.showErrorDialog(outer, message);
-				return;
-			}
-			SwingUtilities.invokeLater(() -> {
-				outer.setVisible(false);
-				outer.dispose();
-			});
-		}
-
-		/**
-		 * @return a String representation of the object
-		 */
-		@SuppressWarnings("MethodReturnAlwaysConstant")
-		@Override
-		public String toString() {
-			return "AppChoiceListenerDriverModel";
-		}
-	}
 }
