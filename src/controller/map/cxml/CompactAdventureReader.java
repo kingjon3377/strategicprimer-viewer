c2aa78d64b42299a7c75f42cc2a6276c48e9cc41
Jonathan Lovelace <kingjon3377@gmail.com>
2016-01-04 23:18:43 -0500

    CompactAdventureReader: Mark parameter as final.

--- src/controller/map/cxml/CompactAdventureReader.java
+++ src/controller/map/cxml/CompactAdventureReader.java
@@ -130,7 +130,7 @@ public final class CompactAdventureReader extends
 	 * @param obj an object
 	 * @return whether we can write it
 	 */
-	public boolean canWrite(Object obj) {
+	public boolean canWrite(final Object obj) {
 		return obj instanceof AdventureFixture;
 	}
 }
