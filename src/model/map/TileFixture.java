f743e648e58f29dca10acdb97ae3080094539ba3
Jonathan Lovelace <kingjon3377@gmail.com>
2016-01-21 13:18:31 -0500

    Suppress Comparable-but-doesnt-override-equals warning for TileFixture, since it's an interface providing default compareTo().

--- src/model/map/TileFixture.java
+++ src/model/map/TileFixture.java
@@ -24,6 +24,7 @@ import org.eclipse.jdt.annotation.NonNull;
  *
  * @author Jonathan Lovelace
  */
+@SuppressWarnings("ComparableImplementedButEqualsNotOverridden")
 public interface TileFixture extends IFixture, Comparable<@NonNull TileFixture> {
 	// Marker interface; also, TODO: what members should this have?
 
