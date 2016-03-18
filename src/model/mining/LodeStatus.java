c39ef40374a78ff806150b6b12a9bf8e29dee34a
Jonathan Lovelace <kingjon3377@gmail.com>
2016-03-18 14:41:06 -0400

    LodeStatus: Refactor to appease 'suspicious indentation' warnings.

--- src/model/mining/LodeStatus.java
+++ src/model/mining/LodeStatus.java
@@ -32,41 +32,62 @@ public enum LodeStatus {
 	/**
 	 * There is very little ore: one part ore per 16,384 parts other rock.
 	 */
-	Minimal(16384),
+	Minimal(16384, 0.4, 0.8),
 	/**
 	 * There is quite little ore: one part ore per 4096 parts other rock.
 	 */
-	VeryPoor(4096),
+	VeryPoor(4096, 0.5, 0.8),
 	/**
 	 * There is little ore: one part ore per 1096 parts other rock.
 	 */
-	Poor(1096),
+	Poor(1096, 0.5, 0.8),
 	/**
 	 * There is a fair amount of ore: one part ore per 256 parts other rock.
 	 */
-	Fair(256),
+	Fair(256, 0.5, 0.8),
 	/**
 	 * There is quite a bit of ore: one part ore per 16 parts other rock.
 	 */
-	Good(16),
+	Good(16, 0.5, 0.8),
 	/**
 	 * There is a relatively high ratio of ore: one part ore per 4 parts other rock.
 	 */
-	VeryGood(4),
+	VeryGood(4, 0.5, 0.8),
 	/**
 	 * The mother-lode: one part ore per one part other rock.
 	 */
-	MotherLode(1);
+	MotherLode(1, 0.7, 1.0);
 	/**
 	 * The number of parts of other rock per part of ore.
 	 */
 	private final int ratio;
-
+	/**
+	 * The probability that an adjacent area will be the next state lower.
+	 */
+	private final double lowerProbability;
+	/**
+	 * The probability that an adjacent area will be either the next state lower or the
+	 * same state.
+	 */
+	private final double notHigherProbability;
 	/**
 	 * @param qty the number of parts of other rock per part of ore
 	 */
 	LodeStatus(final int qty) {
+		this(qty, 1.0, 1.0);
+	}
+
+	/**
+	 * @param qty the number of parts of other rock per part of ore
+	 * @param lowerChance the probability that an adjacent area will be the next state
+	 *                       lower
+	 * @param notHigherChance the probability that an adjacent area will be either the
+	 *                           next state lower or the same state
+	 */
+	LodeStatus(final int qty, final double lowerChance, final double notHigherChance) {
 		ratio = qty;
+		lowerProbability = lowerChance;
+		notHigherProbability = notHigherChance;
 	}
 
 	/**
@@ -75,7 +96,59 @@ public enum LodeStatus {
 	public int getRatio() {
 		return ratio;
 	}
+	/**
+	 * @param state a LodeStatus
+	 * @return the next status lower
+	 */
+	private static LodeStatus lower(final LodeStatus state) {
+		switch (state) {
+		case None:
+			return None;
+		case Minimal:
+			return None;
+		case VeryPoor:
+			return Minimal;
+		case Poor:
+			return VeryPoor;
+		case Fair:
+			return Poor;
+		case Good:
+			return Fair;
+		case VeryGood:
+			return Good;
+		case MotherLode:
+			return VeryGood;
+		default:
+			throw new IllegalStateException("Unhandled case in switch");
+		}
+	}
 
+	/**
+	 * @param state a LodeStatus
+	 * @return the next status higher
+	 */
+	private static LodeStatus higher(final LodeStatus state) {
+		switch (state) {
+		case None:
+			return None;
+		case Minimal:
+			return VeryPoor;
+		case VeryPoor:
+			return Poor;
+		case Poor:
+			return Fair;
+		case Fair:
+			return Good;
+		case Good:
+			return VeryGood;
+		case VeryGood:
+			return MotherLode;
+		case MotherLode:
+			return MotherLode;
+		default:
+			throw new IllegalStateException("Unhandled case in switch");
+		}
+	}
 	/**
 	 * @param state the status of one location
 	 * @param rng   a random-number generator
@@ -83,77 +156,14 @@ public enum LodeStatus {
 	 */
 	public static LodeStatus adjacent(final LodeStatus state, final Random rng) {
 		final double rand = rng.nextDouble();
-		switch (state) {
-		case None:
+		if (state == None) {
 			return None;
-		case Minimal:
-			if (rand < 0.4) {
-				return None;
-			} else if (rand < 0.8) {
-				return Minimal;
-			} else {
-				return VeryPoor;
-			}
-		case VeryPoor:
-			if (rand < 0.2) {
-				return None;
-			} else if (rand < 0.5) {
-				return Minimal;
-			} else if (rand < 0.8) {
-				return VeryPoor;
-			} else {
-				return Poor;
-			}
-		case Poor:
-			if (rand < 0.2) {
-				return None;
-			} else if (rand < 0.5) {
-				return VeryPoor;
-			} else if (rand < 0.8) {
-				return Poor;
-			} else {
-				return Fair;
-			}
-		case Fair:
-			if (rand < 0.2) {
-				return None;
-			} else if (rand < 0.5) {
-				return Poor;
-			} else if (rand < 0.8) {
-				return Fair;
-			} else {
-				return Good;
-			}
-		case Good:
-			if (rand < 0.2) {
-				return None;
-			} else if (rand < 0.5) {
-				return Fair;
-			} else if (rand < 0.8) {
-				return Good;
-			} else {
-				return VeryGood;
-			}
-		case VeryGood:
-			if (rand < 0.2) {
-				return None;
-			} else if (rand < 0.5) {
-				return Good;
-			} else if (rand < 0.8) {
-				return VeryGood;
-			} else {
-				return MotherLode;
-			}
-		case MotherLode:
-			if (rand < 0.2) {
-				return None;
-			} else if (rand < 0.7) {
-				return VeryGood;
-			} else {
-				return MotherLode;
-			}
-		default:
-			throw new IllegalArgumentException("Unhandled LodeStatus");
+		} else if (rand < state.lowerProbability) {
+			return lower(state);
+		} else if (rand < state.notHigherProbability) {
+			return state;
+		} else {
+			return higher(state);
 		}
 	}
 }
