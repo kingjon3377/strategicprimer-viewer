kingjon3377/strategicprimer-viewer

commit abc04c95149542d87ebf060e867bad68c9066c59

Use lambdas to reduce duplication between first and third parts of for statements using ICLIHelper to ask the user to choose from a list.

src/controller/map/drivers/StatGeneratingCLIDriver.java
@@ -3,6 +3,7 @@ package controller.map.drivers;
 import controller.map.drivers.DriverUsage.ParamCount;
 import controller.map.misc.CLIHelper;
 import controller.map.misc.ICLIHelper;
+import controller.map.misc.ICLIHelper.ChoiceOperation;
 import controller.map.misc.IDFactory;
 import controller.map.misc.IDFactoryFiller;
 import java.io.File;
@@ -142,10 +143,11 @@ public final class StatGeneratingCLIDriver implements SimpleCLIDriver {
 		final String hdr = "Which player owns the worker in question?";
 		final String none = "There are no players shared by all the maps.";
 		final String prompt = "Player selection: ";
-		// TODO: Use lambda to simplify this loop
-		for (int playerNum = cli.chooseFromList(players, hdr, none, prompt, true);
+		final ChoiceOperation choice =
+				() -> cli.chooseFromList(players, hdr, none, prompt, true);
+		for (int playerNum = choice.choose();
 				(playerNum >= 0) && (playerNum < players.size());
-				playerNum = cli.chooseFromList(players, hdr, none, prompt, true)) {
+				playerNum = choice.choose()) {
 			enterStats(model, NullCleaner.assertNotNull(players.get(playerNum)), cli);
 		}
 	}
@@ -165,10 +167,9 @@ public final class StatGeneratingCLIDriver implements SimpleCLIDriver {
 		final String hdr = "Which unit contains the worker in question?";
 		final String none = "All that player's units already have stats.";
 		final String prompt = "Unit selection: ";
-		// TODO: Use lambda to simplify this loop
-		for (int unitNum = cli.chooseFromList(units, hdr, none, prompt, false);
-				(unitNum >= 0) && (unitNum < units.size());
-				unitNum = cli.chooseFromList(units, hdr, none, prompt, false)) {
+		final ChoiceOperation choice = () -> cli.chooseFromList(units, hdr, none, prompt, false);
+		for (int unitNum = choice.choose(); (unitNum >= 0) && (unitNum < units.size());
+				unitNum = choice.choose()) {
 			final IUnit unit = units.get(unitNum);
 			enterStats(model, unit, cli);
 			if (!hasUnstattedWorker(model, unit.getID())) {
@@ -229,12 +230,11 @@ public final class StatGeneratingCLIDriver implements SimpleCLIDriver {
 		final String hdr = "Which worker do you want to enter stats for?";
 		final String none = "There are no workers without stats in that unit.";
 		final String prompt = "Worker to modify: ";
-		// TODO: Use lambda to simplify this loop
-		for (int workerNum = cli.chooseFromList(workers, hdr, none, prompt,
-				false); (workerNum >= 0) && (workerNum < workers.size())
-								&& !workers.isEmpty();
-				workerNum = cli.chooseFromList(workers,
-						hdr, none, prompt, false)) {
+		final ChoiceOperation choice =
+				() -> cli.chooseFromList(workers, hdr, none, prompt, false);
+		for (int workerNum = choice.choose();
+				(workerNum >= 0) && (workerNum < workers.size()) && !workers.isEmpty();
+				choice.choose()) {
 			enterStats(model, workers.get(workerNum).getID(), cli);
 			workers.remove(workerNum);
 		}
@@ -338,10 +338,11 @@ public final class StatGeneratingCLIDriver implements SimpleCLIDriver {
 		final String hdr = "Which player owns the new worker(s)?";
 		final String none = "There are no players shared by all the maps.";
 		final String prompt = "Player selection: ";
-		// TODO: Use lambda to simplify this loop
-		for (int playerNum = cli.chooseFromList(players, hdr, none, prompt, false);
+		final ChoiceOperation choice =
+				() -> cli.chooseFromList(players, hdr, none, prompt, false);
+		for (int playerNum = choice.choose();
 				(playerNum >= 0) && (playerNum < players.size());
-				playerNum = cli.chooseFromList(players, hdr, none, prompt, false)) {
+				playerNum = choice.choose()) {
 			createWorkersForPlayer(model, idf,
 					NullCleaner.assertNotNull(players.get(playerNum)), cli);
 		}

src/controller/map/drivers/AdvancementCLIDriver.java
@@ -3,6 +3,7 @@ package controller.map.drivers;
 import controller.map.drivers.DriverUsage.ParamCount;
 import controller.map.misc.CLIHelper;
 import controller.map.misc.ICLIHelper;
+import controller.map.misc.ICLIHelper.ChoiceOperation;
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -95,16 +96,11 @@ public final class AdvancementCLIDriver implements SimpleCLIDriver {
 			final String hdr = "Available players:";
 			final String none = "No players found.";
 			final String prompt = "Chosen player: ";
-			// TODO: Make a lambda to reduce duplication in loop
-			for (int playerNum = cli.chooseFromList(playerList, hdr, none, prompt,
-					false); (playerNum >= 0)
-									&& (playerNum < playerList.size()); playerNum = cli
-																						.chooseFromList(
-																								playerList,
-																								hdr,
-																								none,
-																								prompt,
-																								false)) {
+			final ChoiceOperation choice =
+					() -> cli.chooseFromList(playerList, hdr, none, prompt, false);
+			for (int playerNum = choice.choose();
+					(playerNum >= 0) && (playerNum < playerList.size());
+					playerNum = choice.choose()) {
 				advanceWorkers(workerModel,
 						NullCleaner.assertNotNull(playerList.remove(playerNum)), cli);
 			}
@@ -187,11 +183,10 @@ public final class AdvancementCLIDriver implements SimpleCLIDriver {
 		final String hdr = "Jobs in worker:";
 		final String none = "No existing jobs.";
 		final String prompt = "Job to advance: ";
-		// TODO: Make a lambda to reduce duplication in loop
-		for (int jobNum = cli.chooseFromList(jobs, hdr, none, prompt, false); jobNum <=
-																					jobs
-																							.size();
-				jobNum = cli.chooseFromList(jobs, hdr, none, prompt, false)) {
+		final ChoiceOperation choice =
+				() -> cli.chooseFromList(jobs, hdr, none, prompt, false);
+		for (int jobNum = choice.choose(); jobNum <= jobs.size();
+				jobNum = choice.choose()) {
 			if ((jobNum < 0) || (jobNum == jobs.size())) {
 				worker.addJob(new Job(cli.inputString("Name of new Job: "), 0));
 				jobs.clear();
@@ -220,11 +215,10 @@ public final class AdvancementCLIDriver implements SimpleCLIDriver {
 		final String hdr = "Skills in Job:";
 		final String none = "No existing skills.";
 		final String prompt = "Skill to advance: ";
-		// TODO: Make a lambda to reduce duplication in loop
-		for (int skillNum = cli.chooseFromList(skills, hdr, none, prompt, false);
-				skillNum <= skills
-									.size();
-				skillNum = cli.chooseFromList(skills, hdr, none, prompt, false)) {
+		final ChoiceOperation choice =
+				() -> cli.chooseFromList(skills, hdr, none, prompt, false);
+		for (int skillNum = choice.choose(); skillNum <= skills.size();
+				skillNum = choice.choose()) {
 			if ((skillNum < 0) || (skillNum == skills.size())) {
 				job.addSkill(new Skill(cli.inputString("Name of new Skill: "), 0, 0));
 				skills.clear();

src/controller/map/drivers/ResourceAddingCLIDriver.java
@@ -1,5 +1,11 @@
 package controller.map.drivers;
 
+import controller.map.drivers.DriverUsage.ParamCount;
+import controller.map.misc.CLIHelper;
+import controller.map.misc.ICLIHelper;
+import controller.map.misc.ICLIHelper.ChoiceOperation;
+import controller.map.misc.IDFactory;
+import controller.map.misc.IDFactoryFiller;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,12 +15,6 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
-
-import controller.map.drivers.DriverUsage.ParamCount;
-import controller.map.misc.CLIHelper;
-import controller.map.misc.ICLIHelper;
-import controller.map.misc.IDFactory;
-import controller.map.misc.IDFactoryFiller;
 import model.map.Player;
 import model.map.fixtures.Implement;
 import model.map.fixtures.ResourcePile;
@@ -75,13 +75,11 @@ public class ResourceAddingCLIDriver implements SimpleCLIDriver {
 			final String desc = "Players in the maps:";
 			final String none = "No players found.";
 			final String prompt = "Player to add resources for: ";
-			// TODO: Use a lambda to simplify this loop
-			for (int playerNum =
-					cli.chooseFromList(NullCleaner.assertNotNull(players), desc,
-							none, prompt, false); (playerNum >= 0)
-									&& (playerNum < players.size()); playerNum =
-											cli.chooseFromList(players, desc,
-													none, prompt, false)) {
+			final ChoiceOperation choice =
+					() -> cli.chooseFromList(players, desc, none, prompt, false);
+			for (int playerNum = choice.choose();
+					(playerNum >= 0) && (playerNum < players.size());
+					playerNum = choice.choose()) {
 				final Player player = players.get(playerNum);
 				while (cli.inputBoolean("Keep going? ")) {
 					if (cli.inputBoolean("Enter a (quantified) resource? ")) {

src/controller/map/misc/ICLIHelper.java
@@ -104,4 +104,15 @@ public interface ICLIHelper extends Closeable {
 	 * @param text the string to print
 	 */
 	void print(String text);
+
+	/**
+	 * Interface for getting-an-int-from-the-user operations.
+	 */
+	@FunctionalInterface
+	interface ChoiceOperation {
+		/**
+		 * The operation.
+		 */
+		int choose() throws IOException;
+	}
 }

commit a9ca60b85ba8fa7d0638f1c88ec82b13c4b4e393

Fix infinite loop accidentally introduced in abc04c9514.

src/controller/map/drivers/StatGeneratingCLIDriver.java
@@ -234,7 +234,7 @@ public final class StatGeneratingCLIDriver implements SimpleCLIDriver {
 				() -> cli.chooseFromList(workers, hdr, none, prompt, false);
 		for (int workerNum = choice.choose();
 				(workerNum >= 0) && (workerNum < workers.size()) && !workers.isEmpty();
-				choice.choose()) {
+				workerNum = choice.choose()) {
 			enterStats(model, workers.get(workerNum).getID(), cli);
 			workers.remove(workerNum);
 		}