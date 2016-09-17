44cc103c9fa19d74c92d6716b455caf0ac8a0001
Jonathan Lovelace <kingjon3377@gmail.com>
2016-09-16 23:41:22 -0400

    run_tests.sh: Temporarily add locate command to debug 'classpath element does not exist' warning in log.

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 # This is for use by Travis CI, to reduce too-long lines in .travis.yml
+locate hamcrest-core-1.3.jar
 ant \
     -Dlaunch4j.dir=./launch4j \
     -Dnullness.jar.path=./org.eclipse.jdt.annotation-2.0.0.jar \
