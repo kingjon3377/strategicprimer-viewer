3d890e68b391cf0d1953918e8963064ad6854c2b
Jonathan Lovelace <kingjon3377@gmail.com>
2016-09-16 23:54:41 -0400

    Remove updatedb and locate calls, since they're still failing but the warning went away.

--- run_tests.sh
+++ run_tests.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # This is for use by Travis CI, to reduce too-long lines in .travis.yml
-updatedb
-locate hamcrest-core-1.3.jar
 ant \
     -Dlaunch4j.dir=./launch4j \
     -Dnullness.jar.path=./org.eclipse.jdt.annotation-2.0.0.jar \
