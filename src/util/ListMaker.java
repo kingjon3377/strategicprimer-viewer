0f8ebfa2f91de656442f820b04bfaaba7a2bdf5a
Jonathan Lovelace <kingjon3377@gmail.com>
2016-10-02 20:58:56 -0400

    ListMaker: Add class header comment.

--- src/util/ListMaker.java
+++ src/util/ListMaker.java
@@ -4,6 +4,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
+/**
+ * A utility class containing a method that turns any Iterable into a List.
+ *
+ * This is part of the Strategic Primer assistive programs suite developed by Jonathan
+ * Lovelace.
+ *
+ * Copyright (C) 2016 Jonathan Lovelace
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of version 3 of the GNU General Public License as published by the Free Software
+ * Foundation; see COPYING or
+ * <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+ *
+ * @author Jonathan Lovelace
+ */
 public final class ListMaker {
 	/**
 	 * No need to instantiate.
