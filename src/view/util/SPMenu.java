7f306263a485f8171aeace0a8dc4490ac0785261
Jonathan Lovelace <kingjon3377@gmail.com>
2016-12-07 21:41:07 -0500

    SPMenu: Remove unused import.

--- src/view/util/SPMenu.java
+++ src/view/util/SPMenu.java
@@ -12,7 +12,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import javax.swing.InputMap;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
