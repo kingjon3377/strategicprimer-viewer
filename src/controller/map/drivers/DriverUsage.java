5b2d678a63052e50d01de77123526df9fd573c2d
Jonathan Lovelace <kingjon3377@gmail.com>
2016-12-08 21:53:37 -0500

    Add missing license header to DriverUsage.

--- src/controller/map/drivers/DriverUsage.java
+++ src/controller/map/drivers/DriverUsage.java
@@ -8,6 +8,16 @@ import java.util.List;
  * A class to represent usage information for drivers, for use in the AppStarter and in
  * help text.
  *
+ * This is part of the Strategic Primer assistive programs suite developed by Jonathan
+ * Lovelace.
+ *
+ * Copyright (C) 2015-2016 Jonathan Lovelace
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms
+ * of version 3 of the GNU General Public License as published by the Free Software
+ * Foundation; see COPYING or
+ * <a href="http://www.gnu.org/licenses/">http://www.gnu.org/licenses/</a>.
+ *
  * @author Jonathan Lovelace
  */
 public class DriverUsage implements IDriverUsage {
