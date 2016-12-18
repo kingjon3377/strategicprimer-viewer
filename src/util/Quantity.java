382bfb530536f82fc77dab771bb8e7dd1c4f67ff
Jonathan Lovelace <kingjon3377@gmail.com>
2016-12-18 01:04:28 -0500

    Quantity: Drop @throws Javadoc tags for exceptions we don't throw.

--- src/util/Quantity.java
+++ src/util/Quantity.java
@@ -116,9 +116,6 @@ public final class Quantity implements Subsettable<Quantity>, Comparable<Quantit
 	 * @param obj the object to be compared.
 	 * @return a negative integer, zero, or a positive integer as this object is less
 	 * than, equal to, or greater than the specified object.
-	 * @throws NullPointerException if the specified object is null
-	 * @throws ClassCastException   if the specified object's type prevents it from being
-	 *                              compared to this object.
 	 */
 	@Override
 	public int compareTo(final Quantity obj) {
