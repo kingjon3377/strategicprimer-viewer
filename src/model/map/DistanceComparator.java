bb240b20722bfa787b86a75f5c9c39a42b5e2d5f
Jonathan Lovelace <kingjon3377@gmail.com>
2016-12-23 11:42:15 -0500

    DistanceComparator: Use Integer.compare() instead of reimplementing its algorithm.

--- src/model/map/DistanceComparator.java
+++ src/model/map/DistanceComparator.java
@@ -32,15 +32,7 @@ public final class DistanceComparator implements Comparator<@NonNull Point> {
 	@SuppressWarnings("ParameterNameDiffersFromOverriddenParameter")
 	@Override
 	public int compare(final Point firstPoint, final Point secondPoint) {
-		final int distOne = distance(firstPoint);
-		final int distTwo = distance(secondPoint);
-		if (distOne < distTwo) {
-			return -1;
-		} else if (distOne == distTwo) {
-			return 0;
-		} else {
-			return 1;
-		}
+		return Integer.compare(distance(firstPoint), distance(secondPoint));
 	}
 
 	/**
