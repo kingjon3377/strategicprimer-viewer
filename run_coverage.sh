1031b8656e7d142d258aecc96f7d929bab2bf6e0
Jonathan Lovelace <kingjon3377@gmail.com>
2017-01-14 00:39:28 -0500

    Add 'coverage' target to build.xml, its logic commented out as JDK-8144185 breaks it, and call it from .travis.yml via a helper script.

---
+++ run_coverage.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is for use by Travis CI, to reduce too-long lines in .travis.yml
+ant \
+    -Dlaunch4j.dir=./launch4j \
+    -Dnullness.jar.path=./org.eclipse.jdt.annotation-2.0.0.jar \
+    -Djunit.jar.path=/usr/share/java/junit4.jar \
+    -Dwindowmenu.jar.path=./WindowMenu.jar \
+    -Dhamcrest.jar.path=/usr/share/java/hamcrest-core-1.3.jar \
+    -Dapple.extensions.path=./orange-extensions-1.3.0.jar \
+    coverage
