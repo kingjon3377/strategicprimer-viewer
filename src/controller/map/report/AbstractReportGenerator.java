e204086c6f0a5a0f117a9eefd3479a4362a2778f
Jonathan Lovelace <kingjon3377@gmail.com>
2017-01-15 23:11:06 -0500

    AbstractReportGenerator: Use Collection.forEach() instead of stream().forEach().

--- src/controller/map/report/AbstractReportGenerator.java
+++ src/controller/map/report/AbstractReportGenerator.java
@@ -184,7 +184,7 @@ public abstract class AbstractReportGenerator<T> implements IReportGenerator<T> 
 								value -> value.length() + 15).sum());
 				try (final Formatter formatter = new Formatter(builder)) {
 					formatter.format("%s%n<ul>%n", header);
-					stream().forEach(item -> formatter.format("<li>%s</li>%n", item));
+					forEach(item -> formatter.format("<li>%s</li>%n", item));
 					formatter.format("</ul>%n");
 				}
 				return builder.toString();
