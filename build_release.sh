3f3acd350047e53a9024853443b358ff44558153
Jonathan Lovelace <kingjon3377@gmail.com>
2017-04-13 03:10:11 -0400

    build_release.sh, run_coverage.sh, run_tests.sh: Specify current-directory paths as absolute instead of relative.

--- build_release.sh
+++ build_release.sh
@@ -2,13 +2,13 @@
 # This is for use by Travis CI, to reduce too-long lines in .travis.yml
 ant \
     -lib /usr/share/java/ant-contrib.jar \
-    -lib ./launch4j \
-    -lib ./launch4j/lib \
-    -Dlaunch4j.dir=./launch4j \
-    -Dnullness.jar.path=./org.eclipse.jdt.annotation-2.0.0.jar \
+    -lib $(pwd)/launch4j \
+    -lib $(pwd)/launch4j/lib \
+    -Dlaunch4j.dir=$(pwd)/launch4j \
+    -Dnullness.jar.path=$(pwd)/org.eclipse.jdt.annotation-2.0.0.jar \
     -Djunit.jar.path=/usr/share/java/junit4.jar \
-    -Dwindowmenu.jar.path=./WindowMenu.jar \
-    -Dstub-script-path=./universalJavaApplicationStub-2.0.1/src/universalJavaApplicationStub \
-    -Dapple.extensions.path=./orange-extensions-1.3.0.jar \
-    -Dceylon.home=./ceylon-1.3.2 \
+    -Dwindowmenu.jar.path=$(pwd)/WindowMenu.jar \
+    -Dstub-script-path=$(pwd)/universalJavaApplicationStub-2.0.1/src/universalJavaApplicationStub \
+    -Dapple.extensions.path=$(pwd)/orange-extensions-1.3.0.jar \
+    -Dceylon.home=$(pwd)/ceylon-1.3.2 \
     release
--- run_tests.sh
+++ run_tests.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is for use by Travis CI, to reduce too-long lines in .travis.yml
 ant \
-    -Dlaunch4j.dir=./launch4j \
-    -Dnullness.jar.path=./org.eclipse.jdt.annotation-2.0.0.jar \
+    -Dlaunch4j.dir=$(pwd)/launch4j \
+    -Dnullness.jar.path=$(pwd)/org.eclipse.jdt.annotation-2.0.0.jar \
     -Djunit.jar.path=/usr/share/java/junit4.jar \
-    -Dwindowmenu.jar.path=./WindowMenu.jar \
+    -Dwindowmenu.jar.path=$(pwd)/WindowMenu.jar \
     -Dhamcrest.jar.path=/usr/share/java/hamcrest-core-1.3.jar \
-    -Dapple.extensions.path=./orange-extensions-1.3.0.jar \
-    -Dceylon.home=./ceylon-1.3.2 \
+    -Dapple.extensions.path=$(pwd)/orange-extensions-1.3.0.jar \
+    -Dceylon.home=$(pwd)/ceylon-1.3.2 \
     test-ceylon
