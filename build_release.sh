cd6e74c7e40714d8d4ed3099ef08b0255330750c
Jonathan Lovelace <kingjon3377@gmail.com>
2017-09-26 16:24:19 -0400

    Quote PWD in Bash files, to prevent breakage if it includes spaces.

--- build_release.sh
+++ build_release.sh
@@ -2,11 +2,11 @@
 # This is for use by Travis CI, to reduce too-long lines in .travis.yml
 ant \
     -lib /usr/share/java/ant-contrib.jar \
-    -lib $(pwd)/launch4j \
-    -lib $(pwd)/launch4j/lib \
-    -Dlaunch4j.dir=$(pwd)/launch4j \
-    -Dwindowmenu.jar.path=$(pwd)/pump-swing-1.0.00.jar \
-    -Dstub-script-path=$(pwd)/universalJavaApplicationStub-2.0.2/src/universalJavaApplicationStub \
-    -Dapple.extensions.path=$(pwd)/orange-extensions-1.3.0.jar \
+    -lib "$(pwd)/launch4j" \
+    -lib "$(pwd)/launch4j/lib" \
+    -Dlaunch4j.dir="$(pwd)/launch4j" \
+    -Dwindowmenu.jar.path="$(pwd)/pump-swing-1.0.00.jar" \
+    -Dstub-script-path="$(pwd)/universalJavaApplicationStub-2.0.2/src/universalJavaApplicationStub" \
+    -Dapple.extensions.path="$(pwd)/orange-extensions-1.3.0.jar" \
     -Dceylon.home=$(pwd)/ceylon-1.3.3 \
     release
--- run_tests.sh
+++ run_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is for use by Travis CI, to reduce too-long lines in .travis.yml
 ant \
-    -Dlaunch4j.dir=$(pwd)/launch4j \
-    -Dwindowmenu.jar.path=$(pwd)/pump-swing-1.0.00.jar \
-    -Dapple.extensions.path=$(pwd)/orange-extensions-1.3.0.jar \
-    -Dceylon.home=$(pwd)/ceylon-1.3.3 \
+    -Dlaunch4j.dir="$(pwd)/launch4j" \
+    -Dwindowmenu.jar.path="$(pwd)/pump-swing-1.0.00.jar" \
+    -Dapple.extensions.path="$(pwd)/orange-extensions-1.3.0.jar" \
+    -Dceylon.home="$(pwd)/ceylon-1.3.3" \
     test
