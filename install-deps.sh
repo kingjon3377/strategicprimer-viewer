a4889a1a63361d4abe27da081d0e94dea05fb4ca
Jonathan Lovelace <kingjon3377@gmail.com>
2017-11-09 02:22:27 -0500

    install-deps.sh: Set version property properly on four-component releases (e.g. 0.4.9014.2)

--- install-deps.sh
+++ install-deps.sh
@@ -14,7 +14,7 @@ wget https://github.com/tofi86/universalJavaApplicationStub/archive/v2.1.0.tar.g
 		universalJavaApplicationStub-2.1.0.tar.gz
 tar xzf universalJavaApplicationStub-2.1.0.tar.gz
 if test -n "${TRAVIS_TAG}"; then
-	echo "${TRAVIS_TAG}" | sed 's@^v[0-9]\.[0-9]\.\([0-9]*\|[0-9]*[-_]rc[0-9]*\)$@s:SNAPSHOT:\1:@' | \
+	echo "${TRAVIS_TAG}" | sed 's@^v[0-9]\.[0-9]\.\([0-9]*\|[0-9]*\.[0-9]*\|[0-9]*[-_]rc[0-9]*\)$@s:SNAPSHOT:\1:@' | \
 		sed -f - -i version.properties
 fi
 get_maven_url() {
