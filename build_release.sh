283d61b7078502b985b259be77fa9ffaa79c8942
Jonathan Lovelace <kingjon3377@gmail.com>
2017-12-07 21:58:43 -0500

    Work around changes to the Pumpernickel (WindowMenu) project upstream---hacks until it gets onto Maven Central or some such.

--- build_release.sh
+++ build_release.sh
@@ -6,7 +6,11 @@ ant \
     -lib "$(pwd)/launch4j/lib" \
     -Dlaunch4j.dir="$(pwd)/launch4j" \
     -Dwindowmenu.jar.path="$(pwd)/pump-swing-1.0.00.jar" \
+    -Dpump.common.path="$(pwd)/pump-common-1.0.00.jar" \
+    -Dpump.awt.path="$(pwd)/pump-awt-1.0.00.jar" \
+    -Dpump.image.path="$(pwd)/pump-image-1.0.00.jar" \
+    -Dpump.button.path="$(pwd)/pump-button-1.0.00.jar" \
     -Dstub-script-path="$(pwd)/universalJavaApplicationStub-2.0.2/src/universalJavaApplicationStub" \
     -Dapple.extensions.path="$(pwd)/orange-extensions-1.3.0.jar" \
-    -Dceylon.home=$(pwd)/ceylon-1.3.3 \
+    -Dceylon.home=$(pwd)/ceylon-1.3.3 \ # FIXME: Quote PWD
     release
