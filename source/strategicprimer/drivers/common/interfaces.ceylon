kingjon3377/strategicprimer-viewer

commit 554a16c035ffa8fedda219367bdb0c6a9727580f

SimpleDriver: Don't assert that there are more arguments when there aren't.

source/strategicprimer/drivers/common/interfaces.ceylon
@@ -230,6 +230,11 @@ shared interface SimpleDriver satisfies ISPDriver {
                 }
                 startDriverOnModel(cli, options, mapModel);
             }
+        } else if (args.size == 1) {
+            assert (exists arg = args.first);
+            IDriverModel mapModel = mapReaderAdapter.readMapModel(JPaths.get(arg), warningLevels.default);
+            turnFixer(mapModel.map);
+            startDriverOnModel(cli, options, mapModel);
         } else {
             assert (exists firstArg = args.first);
             assert (nonempty temp = args.map(Types.nativeString).sequence());

commit 4aec5064e17fa987edf57c33f5274408225be1b8

Fix build failure introduced in 554a16c035ff by careless cherry-picking.

source/strategicprimer/drivers/common/interfaces.ceylon
@@ -28,6 +28,7 @@ import strategicprimer.drivers.common {
     IMultiMapModel,
     IDriverModel,
     readMultiMapModel,
+    readMapModel,
     writeModel
 }
 import strategicprimer.drivers.common.cli {
@@ -232,7 +233,7 @@ shared interface SimpleDriver satisfies ISPDriver {
             }
         } else if (args.size == 1) {
             assert (exists arg = args.first);
-            IDriverModel mapModel = mapReaderAdapter.readMapModel(JPaths.get(arg), warningLevels.default);
+            IDriverModel mapModel = readMapModel(JPaths.get(arg), warningLevels.default);
             turnFixer(mapModel.map);
             startDriverOnModel(cli, options, mapModel);
         } else {