kingjon3377/strategicprimer-viewer

commit b42798e2321705a098c02403c8502cd5934038f8

SPMapNG: Use Set.superset() instead of Set.complement().empty

source/strategicprimer/model/map/SPMapNG.ceylon
@@ -380,10 +380,8 @@ shared class SPMapNG satisfies IMutableMapNG {
                         retval = false; // return false;
                     }
                 }
-                //if (!set(obj.rivers[point]) // TODO: syntax sugar
-                        //.complement(set(rivers[point])).empty) { // TODO: syntax sugar
-                if (!set(obj.rivers.get(point))
-                        .complement(set(rivers.get(point))).empty) {
+                //if (!set(obj.rivers[point]).superset(set(rivers[point]))) { // TODO: syntax sugar
+                if (!set(obj.rivers.get(point)).superset(set(rivers.get(point)))) {
                     localReport("Extra river(s)");
                     retval = false; // return false;
                     break;

commit aeeec4d20c098007c4e00315b4bcccb370ac5782

SPMapNG: Use subset() instead of superset() for river-set comparison.
Fixes bug introduced in b42798e2321705a098c02403c8502cd5934038f8.

source/strategicprimer/model/map/SPMapNG.ceylon
@@ -380,8 +380,8 @@ shared class SPMapNG satisfies IMutableMapNG {
                         retval = false; // return false;
                     }
                 }
-                //if (!set(obj.rivers[point]).superset(set(rivers[point]))) { // TODO: syntax sugar
-                if (!set(obj.rivers.get(point)).superset(set(rivers.get(point)))) {
+                //if (!set(obj.rivers[point]).subset(set(rivers[point]))) { // TODO: syntax sugar
+                if (!set(obj.rivers.get(point)).subset(set(rivers.get(point)))) {
                     localReport("Extra river(s)");
                     retval = false; // return false;
                     break;