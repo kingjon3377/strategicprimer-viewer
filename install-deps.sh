b9d3f705ce6a64f16baa32667716c92882e76b80
Jonathan Lovelace <kingjon3377@gmail.com>
2018-11-25 00:58:28 -0500

    install-deps.sh: Fix indentation.

--- install-deps.sh
+++ install-deps.sh
@@ -7,7 +7,7 @@ tar xzf launch4j-3.12-linux.tgz
 wget https://github.com/UltraMixer/JarBundler/releases/download/3.3.0/jarbundler-core-3.3.0.jar \
 		-O "${HOME}/.ant/lib/jarbundler-core-3.3.0.jar"
 for jar in pump-swing pump-common pump-awt pump-image pump-button;do
-    wget https://github.com/mickleness/pumpernickel/raw/master/pump-release/com/pump/${jar}/1.0.00/${jar}-1.0.00.jar
+	wget https://github.com/mickleness/pumpernickel/raw/master/pump-release/com/pump/${jar}/1.0.00/${jar}-1.0.00.jar
 done
 wget http://central.maven.org/maven2/com/yuvimasory/orange-extensions/1.3.0/orange-extensions-1.3.0.jar
 sudo apt-get update -qq
