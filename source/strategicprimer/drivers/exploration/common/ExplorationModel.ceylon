kingjon3377/strategicprimer-viewer

commit a8d2fb1c7291186548c20c4548616c7b7659ceb4

IExplorationModel: Add two methods to remove most mutable-map access from exploration apps.

source/strategicprimer/drivers/exploration/common/ExplorationModel.ceylon
@@ -12,11 +12,13 @@ import strategicprimer.drivers.common {
     SelectionChangeListener
 }
 import strategicprimer.model.common.map {
+    FakeFixture,
     IFixture,
     Player,
     HasOwner,
     MapDimensions,
     Point,
+    River,
     TileFixture,
     TileType,
     IMutableMapNG,
@@ -29,10 +31,12 @@ import strategicprimer.model.common.map.fixtures {
 }
 import strategicprimer.model.common.map.fixtures.mobile {
     Animal,
+    AnimalTracks,
     IUnit,
     MobileFixture
 }
 import strategicprimer.model.common.map.fixtures.resources {
+    CacheFixture,
     Grove,
     Meadow,
     Mine,
@@ -49,6 +53,7 @@ import ceylon.random {
     randomize
 }
 import lovelace.util.common {
+    anythingEqual,
     matchingValue,
     PathWrapper
 }
@@ -528,6 +533,72 @@ shared class ExplorationModel extends SimpleMultiMapModel satisfies IExploration
             indivMap.addFixture(location, unit); // FIXME: Check for existing matching unit there already
             if (!modifiedFlag) {
                 setModifiedFlag(indivMap, true);
+            }
+        }
+    }
+
+    "Copy the given fixture from the main map to subordinate maps. (It is found
+     in the main map by ID, rather than trusting the input.) If it is a cache,
+     remove it from the main map. If [[zero]], remove sensitive information
+     from the copies."
+    shared actual Boolean copyToSubMaps(Point location, TileFixture fixture, Boolean zero) {
+        TileFixture? matching;
+        variable Boolean retval = false;
+        if (is FakeFixture fixture) {
+            // Skip it! It'll corrupt the output XML!
+            return false;
+        } else if (is AnimalTracks fixture) {
+            matching = fixture;
+        } else {
+            matching = map.fixtures.get(location).find(matchingValue(fixture, TileFixture.id));
+        }
+        if (exists matching) {
+            for (subMap->[file, modified] in restrictedSubordinateMaps) {
+                retval = subMap.addFixture(location, matching.copy(zero)) || retval;
+                if (!modified) { // We do *not* use the return value because it returns false if an existing fixture was *replaced*
+                    setModifiedFlag(subMap, true);
+                }
+            }
+
+            if (is CacheFixture matching) {
+                restrictedMap.removeFixture(location, matching); // TODO: make removeFixture() return Boolean, true if anything was removed
+                retval = true;
+                mapModified = true;
+            }
+        }
+        return retval;
+    }
+
+    "Copy any terrain, mountain, rivers, and roads from the main map to subordinate maps."
+    shared actual void copyTerrainToSubMaps(Point location) {
+        for (subMap->[file, modified] in restrictedSubordinateMaps) {
+            variable Boolean modFlag = modified;
+            void setModFlag() {
+                if (!modFlag) {
+                    setModifiedFlag(subMap, true);
+                    modFlag = true;
+                }
+            }
+            if (map.mountainous.get(location), !subMap.mountainous.get(location)) { // TODO: syntax sugar
+                subMap.mountainous[location] = true;
+                setModFlag();
+            }
+            if (exists terrain = map.baseTerrain[location], !anythingEqual(terrain, subMap.baseTerrain[location])) {
+                subMap.baseTerrain[location] = terrain;
+                setModFlag();
+            }
+            if (!map.rivers.get(location).every(subMap.rivers.get(location).contains)) { // TODO: syntax sugar
+                subMap.addRivers(location, *map.rivers.get(location)); // TODO: syntax sugar
+                setModFlag();
+            }
+            value subRoads = subMap.roads[location] else emptyMap;
+            if (exists roads = map.roads[location], !roads.empty) {
+                for (road->quality in roads) {
+                    if ((subRoads[road] else -1) < quality) {
+                        subMap.setRoadLevel(location, road, quality);
+                        setModFlag();
+                    }
+                }
             }
         }
     }

source/strategicprimer/drivers/exploration/common/IExplorationModel.ceylon
@@ -58,4 +58,15 @@ shared interface IExplorationModel
 
     "Add the given [[unit]] at the given [[location]]."
     shared formal void addUnitAtLocation(IUnit unit, Point location);
+
+    "Copy the given fixture from the main map to subordinate maps. (It is found
+     in the main map by ID, rather than trusting the input, unless it is animal
+     tracks.) If it is a cache, remove it from the main map. If [[zero]],
+     remove sensitive information from the copies. Returns true if we think
+     this changed anything in any of the sub-maps."
+    shared formal Boolean copyToSubMaps(Point location, TileFixture fixture, Boolean zero = true);
+
+    "Copy terrain, including any mountain, rivers, and roads, from the main map
+     to subordinate maps." // FIXME: Is this really necessary?
+    shared formal void copyTerrainToSubMaps(Point location);
 }

source/strategicprimer/viewer/drivers/exploration/ExplorationCLIHelper.ceylon
@@ -27,9 +27,6 @@ import strategicprimer.model.common.map.fixtures.mobile {
     Animal,
     AnimalTracks
 }
-import strategicprimer.model.common.map.fixtures.resources {
-    CacheFixture
-}
 
 import strategicprimer.drivers.common.cli {
     ICLIHelper,
@@ -92,12 +89,7 @@ shared class ExplorationCLIHelper(IExplorationModel model, ICLIHelper cli)
             } else {
                 zero = false;
             }
-            for (map->[file, _] in model.subordinateMaps) {
-                map.addFixture(destPoint, fixture.copy(zero));
-            }
-            if (is CacheFixture fixture) {
-                model.map.removeFixture(destPoint, fixture);
-            }
+            model.copyToSubMaps(destPoint, fixture, zero);
         }
     }
 
@@ -247,12 +239,10 @@ shared class ExplorationCLIHelper(IExplorationModel model, ICLIHelper cli)
             //        if (map.mountainous[destPoint]) { // TODO: syntax sugar once compiler bug fixed
             if (map.mountainous.get(destPoint)) {
                 mtn = "mountainous ";
-                for (subMap->[file, _] in model.subordinateMaps) {
-                    subMap.mountainous[destPoint] = true;
-                }
             } else {
                 mtn = "";
             }
+            model.copyTerrainToSubMaps(destPoint);
 
             cli.print("The explorer comes to ``destPoint``, a ``mtn````
                 map.baseTerrain[destPoint] else "unknown-terrain"`` tile");

source/strategicprimer/viewer/drivers/exploration/ExplorationPanel.ceylon
@@ -38,9 +38,7 @@ import strategicprimer.viewer.drivers.map_viewer {
     FixtureListModel,
     fixtureList
 }
-import strategicprimer.model.common.map.fixtures.resources {
-    CacheFixture
-}
+
 import strategicprimer.model.common.map.fixtures.mobile {
     IUnit,
     AnimalTracks,
@@ -86,10 +84,8 @@ import strategicprimer.model.common.idreg {
     createIDFactory
 }
 import ceylon.collection {
-    MutableSet,
     ArrayList,
     MutableList,
-    HashSet,
     MutableMap,
     HashMap
 }
@@ -286,36 +282,22 @@ class ExplorationPanel(SpinnerNumberModel mpModel, ComboBoxModel<Speed> speedMod
             Player player = driverModel.selectedUnit ?. owner else
             PlayerImpl(- 1, "no-one");
 
-            MutableSet<CacheFixture> caches = HashSet<CacheFixture>();
-            for (map->[file, modifiedFlag] in driverModel.subordinateMaps) {
-                map.baseTerrain[destPoint] = driverModel.map
-//                    .baseTerrain[destPoint]; // TODO: syntax sugar once compiler bug fixed
-                    .baseTerrain.get(destPoint);
-                for (fixture in fixtures) {
-                    if (is FakeFixture fixture) {
-                        // Skip it! It'll corrupt the output XML!
-                        continue;
-                    //} else if (!map.fixtures[destPoint].any(fixture.equals)) { // TODO: syntax sugar once compiler bug fixed
-                    } else if (!map.fixtures.get(destPoint).any(fixture.equals)) {
-                        Boolean zero;
-                        if (is HasOwner fixture, (fixture.owner != player ||
-                                fixture is Village)) {
-                            zero = true;
-                        } else {
-                            zero = fixture is HasPopulation<Anything>|HasExtent<out Anything>;
-                        }
-                        map.addFixture(destPoint, fixture.copy(zero));
-                        if (is CacheFixture fixture) {
-                            caches.add(fixture);
-                        }
+            driverModel.copyTerrainToSubMaps(destPoint);
+            for (fixture in fixtures) {
+                if (is FakeFixture fixture) {
+                    // Skip it! It'll corrupt the output XML!
+                    continue;
+                //} else if (!map.fixtures[destPoint].any(fixture.equals)) { // TODO: syntax sugar once compiler bug fixed
+                } else {
+                    Boolean zero;
+                    if (is HasOwner fixture, (fixture.owner != player ||
+                            fixture is Village)) {
+                        zero = true;
+                    } else {
+                        zero = fixture is HasPopulation<Anything>|HasExtent<out Anything>;
                     }
+                    driverModel.copyToSubMaps(destPoint, fixture, zero);
                 }
-                if (!modifiedFlag) {
-                    driverModel.setModifiedFlag(map, true);
-                }
-            }
-            for (cache in caches) {
-                driverModel.map.removeFixture(destPoint, cache);
             }
         }
 

commit ad4f34f9fea34cf4e6ddee80690601eb8f716309

ExplorationModel: Fix copying of noticed fixtures to submaps.
We were mistakenly looking for a fixture in the main map that equaled
the given fixture's ID, instead of one whose ID equaled the given
fixture's ID. So of course nothing matched, and so nothing was ever
copied to submaps. Oops.

Bug introduced in a8d2fb1c7291186548c20c4548616c7b7659ceb4.

source/strategicprimer/drivers/exploration/common/ExplorationModel.ceylon
@@ -550,7 +550,7 @@ shared class ExplorationModel extends SimpleMultiMapModel satisfies IExploration
         } else if (is AnimalTracks fixture) {
             matching = fixture;
         } else {
-            matching = map.fixtures.get(location).find(matchingValue(fixture, TileFixture.id));
+            matching = map.fixtures.get(location).find(matchingValue(fixture.id, TileFixture.id));
         }
         if (exists matching) {
             for (subMap->[file, modified] in restrictedSubordinateMaps) {