c2c8a079472d3b2351b5a955c9fd54ccc7115584
Jonathan Lovelace <kingjon3377@gmail.com>
2021-03-07 13:36:06 -0500

    Port Accumulator back to Java.
    Had to make it an interface and make specialized implementations for integers and decimals,
    unfortunately; Ceylon's standard library's handling of math is what Java's should have been.

---
+++ lovelace-util/src/main/java/DecimalAccumulator.java
@@ -0,0 +1,22 @@
+import java.math.BigDecimal;
+
+/**
+ * An implementation of {@link Accumulator} for arbitrary-precision decimal numbers.
+ */
+public class DecimalAccumulator implements Accumulator<BigDecimal> {
+	private BigDecimal count;
+
+	public DecimalAccumulator(BigDecimal count) {
+		this.count = count;
+	}
+
+	@Override
+	public void add(final BigDecimal addend) {
+		count = count.add(addend);
+	}
+
+	@Override
+	public BigDecimal getSum() {
+		return count;
+	}
+}
---
+++ lovelace-util/src/main/java/IntAccumulator.java
@@ -0,0 +1,20 @@
+/**
+ * An implementation of {@link Accumulator} for integers.
+ */
+public class IntAccumulator implements Accumulator<Integer> {
+	private int count;
+
+	public IntAccumulator(int count) {
+		this.count = count;
+	}
+
+	@Override
+	public void add(final Integer addend) {
+		count += addend;
+	}
+
+	@Override
+	public Integer getSum() {
+		return count;
+	}
+}
