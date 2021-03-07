58191800d772958927d1786d7e25a88a29d487b9
Jonathan Lovelace <kingjon3377@gmail.com>
2021-03-07 13:48:52 -0500

    Port AlignedLabel back to Java.

---
+++ lovelace-util/src/main/java/AlignedLabel.java
@@ -0,0 +1,12 @@
+import javax.swing.JLabel;
+
+/**
+ * A {@link JLabel} that takes its alignment configuration as initializer parameters.
+ */
+public class AlignedLabel extends JLabel {
+	public AlignedLabel(String text, float alignmentX, float alignmentY) {
+		super(text);
+		super.setAlignmentX(alignmentX);
+		super.setAlignmentY(alignmentY);
+	}
+}
