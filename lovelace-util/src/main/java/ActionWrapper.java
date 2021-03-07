5302039259a3d3ec4ca7759e9c5e0d1c7b7400c3
Jonathan Lovelace <kingjon3377@gmail.com>
2021-03-07 13:42:47 -0500

    Port ActionWrapper back to Java.

---
+++ lovelace-util/src/main/java/ActionWrapper.java
@@ -0,0 +1,24 @@
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * A wrapper around an {@link ActionListener} (or equivalent lambda) that extends {@link AbstractAction}, for the
+ * exceedingly common case of a JDK method requiring an {@link Action} when we don't need more functionality than a
+ * single method accepting an {@link ActionEvent}.
+ * <p>
+ * TODO: Do we really need this in Java?
+ */
+public class ActionWrapper extends AbstractAction {
+	private final ActionListener wrapped;
+
+	public ActionWrapper(ActionListener wrappedListener) {
+		wrapped = wrappedListener;
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		wrapped.actionPerformed(event);
+	}
+}
