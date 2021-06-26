7c2e1c150f9f7a940496f5e0756623987b2678d6
Jonathan Lovelace <kingjon3377@gmail.com>
2021-06-26 15:37:16 -0400

    Make wget usage less verbose in CI logs.

--- install-deps.sh
+++ install-deps.sh
@@ -10,21 +10,21 @@ else
 	exit 1
 fi
 mkdir -p "${HOME}/.ant/lib"
-wget "https://download.sourceforge.net/launch4j/launch4j-${LAUNCH4J_MAJOR}/${LAUNCH4J_VERSION}/launch4j-${LAUNCH4J_VERSION}-linux-x64.tgz" \
+wget -nv "https://download.sourceforge.net/launch4j/launch4j-${LAUNCH4J_MAJOR}/${LAUNCH4J_VERSION}/launch4j-${LAUNCH4J_VERSION}-linux-x64.tgz" \
 		-O "launch4j-${LAUNCH4J_VERSION}-linux-x64.tgz"
 tar xzf "launch4j-${LAUNCH4J_VERSION}-linux-x64.tgz"
-wget "https://github.com/UltraMixer/JarBundler/releases/download/${JARBUNDLER_VERSION}/jarbundler-core-${JARBUNDLER_VERSION}.jar" \
+wget -nv "https://github.com/UltraMixer/JarBundler/releases/download/${JARBUNDLER_VERSION}/jarbundler-core-${JARBUNDLER_VERSION}.jar" \
 		-O "${HOME}/.ant/lib/jarbundler-core-${JARBUNDLER_VERSION}.jar"
-wget "https://github.com/mickleness/pumpernickel/raw/master/release/jars/Pumpernickel.jar"
-wget "https://repo.maven.apache.org/maven2/com/yuvimasory/orange-extensions/${ORANGE_VERSION}/orange-extensions-${ORANGE_VERSION}.jar"
-wget "https://github.com/tofi86/universalJavaApplicationStub/archive/v${APP_STUB_VERSION}.tar.gz" -O \
+wget -nv "https://github.com/mickleness/pumpernickel/raw/master/release/jars/Pumpernickel.jar"
+wget -nv "https://repo.maven.apache.org/maven2/com/yuvimasory/orange-extensions/${ORANGE_VERSION}/orange-extensions-${ORANGE_VERSION}.jar"
+wget -nv "https://github.com/tofi86/universalJavaApplicationStub/archive/v${APP_STUB_VERSION}.tar.gz" -O \
 		"universalJavaApplicationStub-${APP_STUB_VERSION}.tar.gz"
 tar xzf "universalJavaApplicationStub-${APP_STUB_VERSION}.tar.gz"
 if test -n "${GITHUB_REF}"; then
 	echo "${GITHUB_REF}" | sed 's@^refs/tags/v\([0-9]\.[0-9]\.[0-9]*\|[0-9]*\.[0-9]*\|[0-9]*[-_]rc[0-9]*\)$@s:SNAPSHOT:\1:@' | \
 		sed -f - -i version.properties
 fi
-wget "https://ceylon-lang.org/download/dist/$(echo "${CEYLON_VERSION}"|sed 's@\.@_@g')" \
+wget -nv "https://ceylon-lang.org/download/dist/$(echo "${CEYLON_VERSION}"|sed 's@\.@_@g')" \
 	--output-document=ceylon.zip
 unzip ceylon.zip
 # Work around eclipse/ceylon#7462
