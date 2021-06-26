5d5c5695465cbd92db1a1ce7be8bcd0ccf7ef20c
Jonathan Lovelace <kingjon3377@gmail.com>
2021-06-26 13:14:14 -0400

    Use Pip-installed gdown instead of youtube-dl.
    Youtube-dl installation fails, probably because it's too heavy a dependency.

--- .github/scripts/populate-cache.sh
+++ .github/scripts/populate-cache.sh
@@ -3,7 +3,8 @@ if curl -s --head --request GET \
 		'https://modules.ceylon-lang.org/repo/1/ceylon/collection/1.3.3/ceylon.collection-1.3.3.car' | \
 		grep -v -q "200 OK"; then
 	# Herd is down
+	pip install gdown
 	echo "Cache URL we have is '${CEYLON_DEPS_CACHE_URL}'"
-	youtube-dl "${CEYLON_DEPS_CACHE_URL}" || exit 1
+	gdown "${CEYLON_DEPS_CACHE_URL}" || exit 1
 	tar xzf ceylon-deps.tar.gz
 fi
