8d5c7151cdce4d366fb305c578171bdc0882f24e
Jonathan Lovelace <kingjon3377@gmail.com>
2021-06-26 12:50:51 -0400

    Try to fix downloading deps cache.

--- .github/scripts/populate-cache.sh
+++ .github/scripts/populate-cache.sh
@@ -3,7 +3,7 @@ if curl -s --head --request GET \
 		'https://modules.ceylon-lang.org/repo/1/ceylon/collection/1.3.3/ceylon.collection-1.3.3.car' | \
 		grep -v -q "200 OK"; then
 	# Herd is down
-	echo "Cache URL we have is '${CEYLON_DEPS_CACHE}'"
-	wget "${CEYLON_DEPS_CACHE}" -O ceylon-deps.tar.gz || exit 1
+	echo "Cache URL we have is '${CEYLON_DEPS_CACHE_URL}'"
+	wget "${CEYLON_DEPS_CACHE_URL}" -O ceylon-deps.tar.gz || exit 1
 	tar xzf ceylon-deps.tar.gz
 fi
