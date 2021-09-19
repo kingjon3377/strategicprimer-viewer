29164fa54079fadd2c15dcade2523664ab63e861
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-19 14:13:06 -0400

    Port Reorderable and ReorderableListModel back to Java.

---
+++ lovelace-util/src/main/java/ReorderableListModel.java
@@ -0,0 +1,27 @@
+import javax.swing.DefaultListModel;
+
+/**
+ * An extension of the {@link DefaultListModel} class to add an implementation
+ * of the {@link Reorderable} interface. For the convenience of callers, the
+ * class also takes its initial elements as constructor parameters.
+ */
+public class ReorderableListModel<Element> extends DefaultListModel<Element> implements Reorderable {
+	private static final long serialVersionUID = 1;
+	@SafeVarargs
+	public ReorderableListModel(Element... initialElements) {
+		for (Element element : initialElements) {
+			addElement(element);
+		}
+	}
+
+	@Override
+	public void reorder(int fromIndex, int toIndex) {
+		if (fromIndex != toIndex) {
+			if (fromIndex > toIndex) {
+				add(toIndex, remove(fromIndex));
+			} else {
+				add(toIndex - 1, remove(fromIndex));
+			}
+		}
+	}
+}
