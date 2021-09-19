13688d931c3a586fe5726074404833727bf091d3
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-19 14:19:48 -0400

    Port IteratorWrapper back to Java.

---
+++ lovelace-util/src/main/java/IteratorWrapper.java
@@ -0,0 +1,18 @@
+import java.util.Iterator;
+
+/**
+ * A wrapper around an {@link Iterator} to let it be used in for-each loops. XML
+ * parsing in particular always seems to hand me an iterator.
+ */
+public final class IteratorWrapper<Element> implements Iterable<Element> {
+	private final Iterator<Element> wrapped;
+
+	public IteratorWrapper(Iterator<Element> wrapped) {
+		this.wrapped = wrapped;
+	}
+
+	@Override
+	public Iterator<Element> iterator() {
+		return wrapped;
+	}
+}
