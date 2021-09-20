df0fb74190037c035ce1c65ebfe67d8e228c578c
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-19 22:07:15 -0400

    Port ImprovedComboBox and MemoizedComboBox back to Java.

---
+++ lovelace-util/src/main/java/lovelace/util/ImprovedComboBox.java
@@ -0,0 +1,53 @@
+package lovelace.util;
+
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+import java.awt.Component;
+
+import javax.swing.JComboBox;
+import javax.swing.ComboBoxModel;
+
+/**
+ * An extension to {@link JComboBox} to improve it by making the Tab key do what one expects.
+ */
+public class ImprovedComboBox<Element> extends JComboBox<Element> {
+	private static final long serialVersionUID = 1;
+	public ImprovedComboBox() {
+		setEditable(true);
+	}
+	public ImprovedComboBox(ComboBoxModel<Element> boxModel) {
+		super(boxModel);
+		setEditable(true);
+	}
+
+	/**
+	 * Handle a key-press. If Tab is pressed when the pop-up list is
+	 * visible, treat it like Enter.
+	 *
+	 * @author http://stackoverflow.com/a/24336768
+	 */
+	@Override
+	public void processKeyEvent(final KeyEvent event) {
+		if (event.getID() != KeyEvent.KEY_PRESSED || event.getKeyCode() != KeyEvent.VK_TAB) {
+			super.processKeyEvent(event);
+			return;
+		}
+		if (isPopupVisible()) {
+	    		Object source = event.getSource();
+			if (source instanceof Component) {
+				super.processKeyEvent(new KeyEvent((Component) source, event.getID(),
+					event.getWhen(), 0, KeyEvent.VK_ENTER, KeyEvent.CHAR_UNDEFINED));
+			} else {
+				throw new IllegalStateException("Source is not a component");
+			}
+		}
+		// I *think* I deliberately chose to use == here so pressing 'shift+ctrl+tab' wouldn't
+		// have any effect on which field has the focus.
+		if (event.getModifiers() == 0) {
+			transferFocus();
+		} else if (event.getModifiers() == InputEvent.SHIFT_MASK) {
+			transferFocusBackward();
+		}
+	}
+}
---
+++ lovelace-util/src/main/java/lovelace/util/MemoizedComboBox.java
@@ -0,0 +1,97 @@
+package lovelace.util;
+
+import javax.swing.JTextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.function.Consumer;
+import java.util.HashSet;
+import java.util.logging.Logger;
+import java.util.Set;
+
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * Extends {@link ImprovedComboBox} to keep a running collection of values.
+ */
+public class MemoizedComboBox extends ImprovedComboBox<String> {
+
+	private static final Logger LOGGER = Logger.getLogger(MemoizedComboBox.class.getName());
+
+	private final Consumer<String> logger;
+
+	public MemoizedComboBox(final Consumer<String> logger) {
+		this.logger = logger;
+	}
+
+	public MemoizedComboBox() {
+		this(LOGGER::severe);
+	}
+
+	/**
+	 * The values we've had in the past.
+	 */
+	private final Set<String> values = new HashSet<>();
+
+	/**
+	 * Clear the combo box, but if its value was one we haven't had
+	 * previously, add it to the drop-down list.
+	 */
+	public void checkAndClear() {
+		final Object selectedItem = getSelectedItem();
+		if (selectedItem == null) {
+			return;
+		} else if (selectedItem instanceof String) {
+			String item = ((String) selectedItem).trim();
+			if (!item.isEmpty() && !values.contains(item)) {
+				values.add(item);
+				addItem(item);
+			}
+			setSelectedItem(null);
+		} else {
+			throw new IllegalStateException("Only supports Strings");
+		}
+	}
+
+	@Override
+	@Nullable
+	public String getSelectedItem() {
+		Object retval = super.getSelectedItem();
+		if (retval == null) {
+			return null;
+		} else if (retval instanceof String) {
+			return ((String) retval).trim();
+		} else {
+			return retval.toString().trim();
+		}
+	}
+
+	@Override
+	public void setSelectedItem(@Nullable final Object selectedItem) {
+		if (selectedItem instanceof String || selectedItem == null) {
+			super.setSelectedItem(selectedItem);
+		} else {
+			logger.accept("Failed to set selectedItem: must be a String.");
+		}
+	}
+
+	public String getSelectedString() {
+		Object inner = getEditor().getEditorComponent();
+		if (inner instanceof JTextField) {
+			String text = ((JTextField) inner).getText().trim();
+			if (!text.isEmpty()) {
+				setSelectedItem(text);
+				return text;
+			}
+		}
+		return getSelectedItem();
+	}
+
+	public void addSubmitListener(final ActionListener listener) {
+		Object inner = getEditor().getEditorComponent();
+		if (inner instanceof JTextField) {
+			((JTextField) inner).addActionListener(listener);
+		} else {
+			logger.accept("Editor wasn't a text field, but a " + inner.getClass().getName());
+		}
+	}
+}
