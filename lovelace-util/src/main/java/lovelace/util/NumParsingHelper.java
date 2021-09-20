c0b7c4ff7ae7470bbd977d2221d8d41b1d15671d
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-20 18:08:49 -0400

    Port isNumeric() and parseInt() back to Java.
    With their tests.

---
+++ lovelace-util/src/main/java/lovelace/util/NumParsingHelper.java
@@ -0,0 +1,43 @@
+package lovelace.util;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+import java.util.OptionalInt;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public final class NumParsingHelper {
+	private static final Logger LOGGER = Logger.getLogger(NumParsingHelper.class.getName());
+
+	private static final NumberFormat PARSER = NumberFormat.getIntegerInstance();
+
+	/**
+	 * Whether the given string contains numeric data, such as will be successfully parsed by {@link parseInt}.
+	 */
+	public static boolean isNumeric(String string) {
+		try {
+			PARSER.parse(string);
+			return true;
+		} catch (NumberFormatException|ParseException ignored) {
+			return false;
+		}
+	}
+
+	// TODO: Should probably just return 'int', or 'long', and declare as throwing ParseException.
+	/**
+	 * Parse an integer, which may contain commas, returning an empty Optional if non-numeric.
+	 */
+	public static OptionalInt parseInt(String string) {
+		try {
+			return OptionalInt.of(PARSER.parse(string).intValue());
+		} catch (NumberFormatException|ParseException except) {
+			LOGGER.log(Level.FINE, except,
+				() -> "Failed to parse a number from the following input: " + string);
+			return OptionalInt.empty();
+		}
+	}
+}
+
+
