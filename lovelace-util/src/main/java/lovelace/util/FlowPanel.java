4e09ea4b81c0b67527ebe6be2017e36d21169fa8
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-19 22:42:42 -0400

    Port FlowPanel back to Java.

---
+++ lovelace-util/src/main/java/lovelace/util/FlowPanel.java
@@ -0,0 +1,16 @@
+package lovelace.util;
+
+import java.awt.Component;
+import javax.swing.JPanel;
+/**
+ * A {@link JPanel} using the default {@link java.awt.FlowLayout} and taking
+ * children to add as constructor parameters.
+ */
+public class FlowPanel extends JPanel {
+	private static final long serialVersionUID = 1;
+	public FlowPanel(Component... components) {
+		for (Component component : components) {
+			add(component);
+		}
+	}
+}
