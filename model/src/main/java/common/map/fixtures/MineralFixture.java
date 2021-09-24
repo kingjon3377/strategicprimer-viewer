556f33cb35f14129fa338a191226c4ca5475c0ed
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-23 22:36:49 -0400

    Port MineralFixture and TerrainFixture back to Java.

---
+++ model/src/main/java/common/map/fixtures/MineralFixture.java
@@ -0,0 +1,14 @@
+package common.map.fixtures;
+
+import common.map.TileFixture;
+import common.map.HasKind;
+
+/**
+ * An (at present marker) interface for tile fixtures representing some kind of
+ * "mineral" (as opposed to organic) resource or thing: bare rock or the
+ * ground, stone deposit, mineral vein, mine, etc.
+ *
+ * TODO: Any members?
+ */
+public interface MineralFixture extends TileFixture, HasKind {
+}
