d1a89cbd01721ce6041aec9e62b0a6bd34379a56
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-25 00:29:05 -0400

    Port ExplorableFixture, Cave, and Battlefield back to Java.

---
+++ model/src/main/java/common/map/fixtures/explorable/Battlefield.java
@@ -0,0 +1,114 @@
+package common.map.fixtures.explorable;
+
+import common.map.IFixture;
+
+/**
+ * "There are the signs of a long-ago battle here"
+ */
+public class Battlefield implements ExplorableFixture {
+	public Battlefield(int dc, int id) {
+		this.dc = dc;
+		this.id = id;
+	}
+
+	/**
+	 * The required Perception check result to discover the battlefield.
+	 */
+	private int dc;
+
+	/**
+	 * The required Perception check result to discover the battlefield.
+	 */
+	@Override
+	public int getDC() {
+		return dc;
+	}
+
+	/**
+	 * A unique ID.
+	 */
+	private final int id;
+
+	/**
+	 * A unique ID.
+	 */
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	private String image = "";
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * Set the filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * Clone the object.
+	 */
+	@Override
+	public Battlefield copy(boolean zero) {
+		Battlefield retval = new Battlefield((zero) ? 0 : dc, id);
+		retval.setImage(image);
+		return retval;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj instanceof Battlefield) {
+			return ((Battlefield) obj).getId() == id;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	/**
+	 * If we ignore ID, all Battlefields are equal.
+	 */
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		return fixture instanceof Battlefield;
+	}
+
+	@Override
+	public String getDefaultImage() {
+		return "battlefield.png";
+	}
+
+	@Override
+	public String getPlural() {
+		return "Battlefields";
+	}
+
+	@Override
+	public String getShortDescription() {
+		return "signs of a long-ago battle";
+	}
+
+	@Override
+	public String toString() {
+		return "An ancient battlefield with DC " + dc;
+	}
+}
---
+++ model/src/main/java/common/map/fixtures/explorable/Cave.java
@@ -0,0 +1,118 @@
+package common.map.fixtures.explorable;
+
+import common.map.IFixture;
+
+/**
+ * "There are extensive caves beneath this tile".
+ */
+public class Cave implements ExplorableFixture {
+	public Cave(int dc, int id) {
+		this.dc = dc;
+		this.id = id;
+	}
+
+	/**
+	 * The required Perception check result to discover the caves.
+	 */
+	private int dc;
+
+	/**
+	 * The required Perception check result to discover the caves.
+	 */
+	@Override
+	public int getDC() {
+		return dc;
+	}
+
+	/**
+	 * A unique ID.
+	 */
+	private final int id;
+
+	/**
+	 * A unique ID.
+	 */
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	private String image = "";
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * Set the filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * Clone the object.
+	 */
+	@Override
+	public Cave copy(boolean zero) {
+		Cave retval = new Cave((zero) ? 0 : dc, id);
+		retval.setImage(image);
+		return retval;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj instanceof Cave) {
+			return ((Cave) obj).getId() == id;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return "Caves with DC " + dc;
+	}
+
+	/**
+	 * If we ignore ID (and DC), all caves are equal.
+	 */
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		return fixture instanceof Cave;
+	}
+
+	/**
+	 * @author MrBeast 
+	 * @see http://opengameart.org/content/cave-tileset-0
+	 */
+	@Override
+	public String getDefaultImage() {
+		return "cave.png";
+	}
+
+	@Override
+	public String getPlural() {
+		return "Caves";
+	}
+
+	@Override
+	public String getShortDescription() {
+		return "caves underground";
+	}
+}
