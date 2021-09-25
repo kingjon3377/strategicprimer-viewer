4eb98e3c014856470bfa14c42b4b9032a3249355
Jonathan Lovelace <kingjon3377@gmail.com>
2021-09-25 11:33:36 -0400

    Port MobileFixture and ProxyFor back to Java.

---
+++ model/src/main/java/common/map/fixtures/mobile/ProxyFor.java
@@ -0,0 +1,62 @@
+package common.map.fixtures.mobile;
+
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+/**
+ * An interface for "proxy" implementations.
+ */
+public interface ProxyFor<Type> /* implements Type */ {
+	/**
+	 * Add another object to be proxied.
+	 */
+	void addProxied(Type item);
+
+	/**
+	 * Get the proxied items. This should probably only ever be used in tests, or in proxies managing nexted proxies.
+	 */
+	Iterable<Type> getProxied();
+
+	/**
+	 * If there is consensus among proxied items on the given property,
+	 * return it; otherwise return null.
+	 *
+	 * TODO: Return Optional instead?
+	 */
+	@Nullable
+	default <MemberType> MemberType getConsensus(Function<Type, MemberType> accessor) {
+		@Nullable MemberType retval = null;
+		for (Type proxied : getProxied()) {
+			MemberType item = accessor.apply(proxied);
+			if (retval != null) {
+				if (!retval.equals(item)) {
+					return null;
+				}
+			} else {
+				retval = item;
+			}
+		}
+		return retval;
+	}
+
+	/**
+	 * If there is consensus on the given property among proxied items that
+	 * define it, return it; otherwise return null.
+	 * @deprecated Use getConsensus
+	 */
+	@Deprecated
+	@Nullable
+	default <MemberType> MemberType getNullableConsensus(Function<Type, MemberType> accessor) {
+		return getConsensus(accessor);
+	}
+
+	/**
+	 * Whether this should be considered (if true) a proxy for multiple
+	 * representations of the same item (such as in different maps), or (if
+	 * false) a proxy for multiple related items (such as all workers in a
+	 * single unit).
+	 * TODO: If we keep proxies around, convert to enum.
+	 */
+	boolean isParallel();
+}
