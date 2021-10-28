e8b4581bdddee733439705d083965319a0a68281
Jonathan Lovelace <kingjon3377@gmail.com>
2021-10-28 19:45:28 -0400

    Port Warning back to Java.

---
+++ model/src/main/java/common/xmlio/Warning.java
@@ -0,0 +1,86 @@
+package common.xmlio;
+
+import common.idreg.DuplicateIDException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.function.Consumer;
+import common.xmlio.SPFormatException;
+
+/**
+ * A slightly-customizable warning-handling interface.
+ *
+ * TODO: Move these to lovelace.util.common (figure out how to make the
+ * filtering in {@link WarningLevels#WARN} work first ...
+ */
+public final class Warning {
+	/**
+	 * A logger.
+	 */
+	private static final Logger LOGGER = Logger.getLogger(Warning.class.getName());
+
+	/**
+	 * Don't do anything with warnings.
+	 */
+	public static final Warning IGNORE = new Warning(x -> {});
+
+	/**
+	 * Log each warning, but let them pass.
+	 */
+	public static final Warning WARN = new Warning(Warning::warn);
+
+	/**
+	 * Treat warnings as errors.
+	 */
+	public static final Warning DIE = new Warning(Warning::die);
+
+	/**
+	 * The default warning handler. This is provided so that it can in
+	 * theory be changed later in one place rather than everywhere.
+	 */
+	private static Warning defaultHandler = WARN;
+
+	public static Warning getDefaultHandler() {
+		return defaultHandler;
+	}
+
+	public static void setDefaultHandler(Warning handler) {
+		defaultHandler = handler;
+	}
+
+	private final Consumer<Throwable> impl;
+
+	/**
+	 * Handle a warning, such as if a particular map-format construct is
+	 * deprecated.
+	 */
+	public void handle(Throwable warning) {
+		impl.accept(warning);
+	}
+
+	public Warning(Consumer<Throwable> impl) {
+		this.impl = impl;
+	}
+
+	public Warning(Consumer<String> handler, boolean ignored) {
+		this.impl = t -> {
+			if (t instanceof SPFormatException) {
+				handler.accept("SP format warning: " + t.getMessage());
+			} else {
+				handler.accept("Warning: " + t.getMessage());
+			}
+		};
+	}
+
+	private static void warn(Throwable warning) {
+		if (warning instanceof SPFormatException || warning instanceof DuplicateIDException) {
+			LOGGER.warning(warning.getMessage());
+		} else {
+			LOGGER.log(Level.WARNING, "Warning: ", warning);
+		}
+	}
+
+	private static void die(Throwable warning) {
+		throw new RuntimeException(warning);
+	}
+}
+
