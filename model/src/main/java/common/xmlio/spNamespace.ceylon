75dda6cb6b5ccac27b9fb58c8650d6c94a71e1cd
Jonathan Lovelace <kingjon3377@gmail.com>
2021-10-28 20:18:28 -0400

    Port ISPReader back to Java.
    And the global constants that were placed in this interface before we
    ported to Ceylon.

--- model/src/main/java/common/xmlio/spNamespace.ceylon
+++
@@ -1,2 +0,0 @@
-"The namespace that SP XML will use."
-shared String spNamespace = "https://github.com/kingjon3377/strategicprimer-viewer";
---
+++ model/src/main/java/impl/xmlio/ISPReader.java
@@ -0,0 +1,45 @@
+package impl.xmlio;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import common.xmlio.Warning;
+
+import common.xmlio.SPFormatException;
+import lovelace.util.MalformedXMLException;
+
+
+/**
+ * An interface for readers of any SP model type.
+ */
+public interface ISPReader {
+	/**
+	 * Tags we expect to use in the future; they are skipped for now and
+	 * we'll warn if they're used.
+	 */
+	public static final List<String> FUTURE_TAGS =
+		Collections.unmodifiableList(Arrays.asList("future",
+			"explorer", "building", "resource", "changeset",
+			"change", "move", "work", "discover", "submap",
+			"futuretag", "futureTag", "science"));
+
+	/**
+	 * The namespace that SP XML will use.
+	 */
+	public static final String SP_NAMESPACE =
+		"https://github.com/kingjon3377/strategicprimer-viewer";
+
+	/**
+	 * Read an object from XML.
+	 *
+	 * @param file The name of the file being read from
+	 * @param istream The reader from which to read the XML
+	 * @param warner "The Warning instance to use for warnings
+	 */
+	<Element> Element readXML(Path file, Reader istream, Warning warner)
+		throws SPFormatException, MalformedXMLException, IOException;
+}
