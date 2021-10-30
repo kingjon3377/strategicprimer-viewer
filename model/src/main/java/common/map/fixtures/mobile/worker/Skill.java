7373b6d32030e90d7522a9c9f5e6cda9c44c30e9
Jonathan Lovelace <kingjon3377@gmail.com>
2021-10-30 14:03:18 -0400

    Port Skill and interfaces back to Java.

---
+++ model/src/main/java/common/map/fixtures/mobile/worker/Skill.java
@@ -0,0 +1,97 @@
+package common.map.fixtures.mobile.worker;
+
+import java.util.function.Consumer;
+
+/**
+ * A skill a worker has experience or training in.
+ */
+public final class Skill implements IMutableSkill {
+	public Skill(String name, int skillLevel, int time) {
+		this.name = name;
+		this.skillLevel = skillLevel;
+		this.time = time;
+	}
+
+	/**
+	 * The name of the skill.
+	 */
+	private final String name;
+
+	/**
+	 * The name of the skill.
+	 */
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * How many levels the worker has in the skill.
+	 */
+	private int skillLevel;
+
+	/**
+	 * How many levels the worker has in the skill.
+	 */
+	@Override
+	public int getLevel() {
+		return skillLevel;
+	}
+
+	/**
+	 * How many hours of experience the worker has gained since last leveling up.
+	 */
+	private int time;
+
+	/**
+	 * How many hours of experience the worker has gained since last leveling up.
+	 */
+	@Override
+	public int getHours() {
+		return time;
+	}
+
+	/**
+	 * Clone the object.
+	 */
+	@Override
+	public Skill copy() {
+		return new Skill(name, skillLevel, time);
+	}
+
+	/**
+	 * An object is equal iff it is a Skill with the same name, level, and number of hours.
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof ISkill) {
+			return name.equals(((ISkill) obj).getName()) &&
+				skillLevel == ((ISkill) obj).getLevel() &&
+				time == ((ISkill) obj).getHours();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	/**
+	 * Add hours of training or experience.
+	 */
+	@Override
+	public void addHours(int hours, int condition) {
+		time += hours;
+		if (condition <= time) {
+			skillLevel++;
+			time = 0;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s (%d)", name, skillLevel);
+	}
+}
