c12954bfdc28a60b9a684660b7c3708dd9568387
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-02 23:30:52 -0400

    Port ProxyMember back to Java.

---
+++ model/src/main/java/common/map/fixtures/mobile/ProxyMember.java
@@ -0,0 +1,94 @@
+package common.map.fixtures.mobile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import common.map.IFixture;
+import common.map.fixtures.UnitMember;
+
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+/**
+ * A proxy for non-worker unit members.
+ */
+class ProxyMember implements UnitMember, ProxyFor<UnitMember> {
+	private static final Logger LOGGER = Logger.getLogger(ProxyMember.class.getName());
+	private final List<UnitMember> proxiedMembers = new ArrayList<>();
+
+	private ProxyMember() {
+	}
+
+	public ProxyMember(UnitMember member) {
+		proxiedMembers.add(member);
+	}
+
+	@Override
+	public void addProxied(UnitMember item) {
+		proxiedMembers.add(item);
+	}
+
+	@Override
+	public ProxyMember copy(boolean zero) {
+		ProxyMember retval = new ProxyMember();
+		for (UnitMember member : proxiedMembers) {
+			retval.addProxied(member.copy(zero));
+		}
+		return retval;
+	}
+
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		LOGGER.warning("ProxyMember.equalsIgnoringID() called");
+		if (fixture instanceof ProxyMember) {
+			return ((ProxyMember) fixture).proxiedMembers.equals(proxiedMembers); // TODO: Shouldn't depend on order, right?
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean isSubset(IFixture fixture, Consumer<String> report) {
+		report.accept("isSubset called on ProxyMember");
+		return false;
+	}
+
+	@Override
+	public Iterable<UnitMember> getProxied() {
+		return new ArrayList<>(proxiedMembers);
+	}
+
+	@Override
+	public boolean isParallel() {
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		if (proxiedMembers.isEmpty()) {
+			return "a proxy for no unit members";
+		}
+		String retval = getConsensus(UnitMember::toString);
+		if (retval == null) {
+			return "a proxy for a variety of unit members";
+		} else {
+			return retval;
+		}
+	}
+
+	/**
+	 * Returns the ID number shared by all the proxied members, or -1 if
+	 * either there are no proxied members or some have a different ID.
+	 */
+	@Override
+	public int getId() {
+		Integer retval = getConsensus(UnitMember::getId);
+		return (retval == null) ? -1 : retval;
+	}
+
+	@Override
+	public String getPlural() {
+		String retval = getConsensus(UnitMember::getPlural);
+		return (retval == null) ? "Various Unit Members" : retval;
+	}
+}
