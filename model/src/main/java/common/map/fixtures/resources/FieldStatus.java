2386789e7a1ba6b35ece24a0e99e9179d30046c5
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-04 22:12:41 -0400

    Port FieldStatus back to Java.

---
+++ model/src/main/java/common/map/fixtures/resources/FieldStatus.java
@@ -0,0 +1,51 @@
+package common.map.fixtures.resources;
+
+import java.util.Random;
+
+/**
+ * Possible status of fields (and meadows, and orchards ...) Fields should
+ * rotate between these, at a rate determined by the kind of field.
+ *
+ * TODO: Implement that
+ */
+public enum FieldStatus {
+	/**
+	 * Fallow: waiting to be planted, or waiting to start growing.
+	 */
+	Fallow("fallow"),
+	/**
+	 * Seeding: being planted, by human or animal activity.
+	 */
+	Seeding("seeding"),
+	/**
+	 * Growing.
+	 */
+	Growing("growing"),
+	/**
+	 * Bearing: ready to be harvested.
+	 */
+	Bearing("bearing");
+	private final String string;
+	private FieldStatus(String str) {
+		string = str;
+	}
+	@Override
+	public String toString() {
+		return string;
+	}
+
+	public static FieldStatus random(int seed) {
+		FieldStatus[] statuses = FieldStatus.values();
+		return statuses[new Random(seed).nextInt(statuses.length)];
+	}
+
+	public static FieldStatus parse(String status) {
+		// TODO: Have HashMap cache to speed this up?
+		for (FieldStatus val : values()) {
+			if (status.equals(val.toString())) {
+				return val;
+			}
+		}
+		throw new IllegalArgumentException("Failed to parse FieldStatus from " + status);
+	}
+}
