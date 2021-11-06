fc8c064871f87be341ae97fddaef0badde8fcf9e
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-05 22:18:56 -0400

    Port CacheFixture and Shrub back to Java.

---
+++ model/src/main/java/common/map/fixtures/resources/CacheFixture.java
@@ -0,0 +1,160 @@
+package common.map.fixtures.resources;
+
+import common.map.IFixture;
+
+/**
+ * A cache (of vegetables, or a hidden treasure, or ...) on a tile.
+ */
+public class CacheFixture implements HarvestableFixture {
+	public CacheFixture(String kind, String contents, int id) {
+		this.kind = kind;
+		this.contents = contents;
+		this.id = id;
+	}
+
+	/**
+	 * What kind of things this is a cache of.
+	 *
+	 * TODO: Should perhaps be enumerated, so we can make images more granular.
+	 */
+	private final String kind;
+
+	/**
+	 * What kind of things this is a cache of.
+	 *
+	 * TODO: Should perhaps be enumerated, so we can make images more granular.
+	 */
+	@Override
+	public String getKind() {
+		return kind;
+	}
+
+	/**
+	 * The contents of this cache.
+	 *
+	 * TODO: Should be turned into objects (serialized as children) as part
+	 * of the general Resource framework.
+	 */
+	private final String contents;
+
+	/**
+	 * The contents of this cache.
+	 *
+	 * TODO: Should be turned into objects (serialized as children) as part
+	 * of the general Resource framework.
+	 */
+	public String getContents() {
+		return contents;
+	}
+
+	/**
+	 * ID number
+	 */
+	private final int id;
+
+	/**
+	 * ID number
+	 */
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	private String image = "";
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * Set the filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * Clone the object.
+	 */
+	@Override
+	public CacheFixture copy(boolean zero) {
+		CacheFixture retval = new CacheFixture(kind, contents, id);
+		retval.setImage(image);
+		return retval;
+	}
+
+	/**
+	 * The filename of the image to use as the default icon for caches.
+	 *
+	 * TODO: Should be more granular
+	 */
+	@Override
+	public String getDefaultImage() {
+		return "cache.png";
+	}
+
+	@Override
+	public String toString() {
+		return String.format("a cache of %s containing %s", kind, contents);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof CacheFixture) {
+			return ((CacheFixture) obj).getId() == id &&
+				kind.equals(((CacheFixture) obj).getKind()) &&
+				contents.equals(((CacheFixture) obj).getContents());
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		if (fixture instanceof CacheFixture) {
+			return ((CacheFixture) fixture).getKind().equals(kind) &&
+				((CacheFixture) fixture).getContents().equals(contents);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String getPlural() {
+		return "Caches";
+	}
+
+	@Override
+	public String getShortDescription() {
+		return "a cache of " + kind;
+	}
+
+	/**
+	 * TODO: Make variable (loaded from XML) or otherwise more granular?
+	 */
+	@Override
+	public int getDC() {
+		return 25;
+	}
+
+	/**
+	 * Caches are *moved* to player maps when discovered.
+	 */
+	@Override
+	public boolean subsetShouldSkip() {
+		return true;
+	}
+}
---
+++ model/src/main/java/common/map/fixtures/resources/Shrub.java
@@ -0,0 +1,181 @@
+package common.map.fixtures.resources;
+
+import common.map.IFixture;
+import common.map.HasPopulation;
+
+import java.util.function.Consumer;
+
+/**
+ * A {@link strategicprimer.model.common.map.TileFixture} to represent shrubs,
+ * or their aquatic equivalents, on a tile.
+ */
+public class Shrub implements HarvestableFixture, HasPopulation<Shrub> {
+	public Shrub(String kind, int id, int population) {
+		this.kind = kind;
+		this.id = id;
+		this.population = population;
+	}
+
+	public Shrub(String kind, int id) {
+		this(kind, id, -1);
+	}
+
+	/**
+	 * What kind of shrub this is
+	 */
+	private final String kind;
+
+	/**
+	 * What kind of shrub this is
+	 */
+	@Override
+	public String getKind() {
+		return kind;
+	}
+
+	/**
+	 * The ID number.
+	 */
+	private final int id;
+
+	/**
+	 * The ID number.
+	 */
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	private String image = "";
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * How many individual plants are in this planting of this shrub, or on this tile.
+	 */
+	private final int population;
+
+	/**
+	 * How many individual plants are in this planting of this shrub, or on this tile.
+	 */
+	@Override
+	public int getPopulation() {
+		return population;
+	}
+
+	@Override
+	public Shrub copy(boolean zero) {
+		final Shrub retval = new Shrub(kind, id, (zero) ? -1 : population);
+		retval.setImage(image);
+		return retval;
+	}
+
+	@Override
+	public Shrub reduced(int newPopulation, int newId) {
+		return new Shrub(kind, newId, newPopulation);
+	}
+
+	@Override
+	public Shrub combined(Shrub addend) {
+		return new Shrub(kind, id,
+			Math.max(0, population) + Math.max(0, addend.getPopulation()));
+	}
+
+	@Override
+	public String getDefaultImage() {
+		return "shrub.png";
+	}
+
+	@Override
+	public String toString() {
+		return kind;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Shrub) {
+			return ((Shrub) obj).getId() == id &&
+				((Shrub) obj).getKind().equals(kind) &&
+				population == ((Shrub) obj).getPopulation();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		if (fixture instanceof Shrub) {
+			return kind.equals(((Shrub) fixture).getKind()) &&
+				population == ((Shrub) fixture).getPopulation();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String getPlural() {
+		return "Shrubs";
+	}
+
+	@Override
+	public String getShortDescription() {
+		if (population < 1) {
+			return kind;
+		} else {
+			return String.format("%d %s", population, kind);
+		}
+	}
+
+	@Override
+	public boolean isSubset(IFixture other, Consumer<String> report) {
+		if (other.getId() != id) {
+			report.accept("Different IDs");
+			return false;
+		} else if (other instanceof Shrub) {
+			if (!((Shrub) other).getKind().equals(kind)) {
+				report.accept(String.format("In shrub with ID #%d:\tKinds differ", id));
+				return false;
+			} else if (((Shrub) other).getPopulation() > population) {
+				report.accept(String.format("In shrub %s (#%d):\tHas higher count than we do", kind, id));
+				return false;
+			} else {
+				return true;
+			}
+		} else {
+			report.accept("Different types for ID #" + id);
+			return false;
+		}
+	}
+
+	/**
+	 * The required Perception check for an explorer to find the fixture.
+	 *
+	 * TODO: Should this vary, either loading from XML or by kind?
+	 */
+	@Override
+	public int getDC() {
+		return 15;
+	}
+}
