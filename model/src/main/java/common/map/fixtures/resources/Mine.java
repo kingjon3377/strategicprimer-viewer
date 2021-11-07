d8eb1630c06ba932a0444419c398ec0e63b9561d
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-06 20:13:01 -0400

    Port Mine, MineralVein, and StoneDeposit back to Java.

---
+++ model/src/main/java/common/map/fixtures/resources/Mine.java
@@ -0,0 +1,146 @@
+package common.map.fixtures.resources;
+
+import java.util.function.Consumer;
+
+import common.map.IFixture;
+import common.map.fixtures.MineralFixture;
+import common.map.fixtures.towns.TownStatus;
+
+/**
+ * A mine---a source of mineral resources.
+ */
+public class Mine implements HarvestableFixture, MineralFixture {
+	public Mine(String kind, TownStatus status, int id) {
+		this.kind = kind;
+		this.status = status;
+		this.id = id;
+	}
+
+	/**
+	 * What the mine produces.
+	 */
+	private final String kind;
+
+	/**
+	 * What the mine produces.
+	 */
+	@Override
+	public String getKind() {
+		return kind;
+	}
+
+	/**
+	 * The status of the mine.
+	 */
+	private final TownStatus status;
+
+	/**
+	 * The status of the mine.
+	 */
+	public TownStatus getStatus() {
+		return status;
+	}
+
+	/**
+	 * The ID number of the fixture.
+	 */
+	private final int id;
+
+	/**
+	 * The ID number of the fixture.
+	 */
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	private String image = "";
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * Set the filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * The default icon filename.
+	 */
+	@Override
+	public String getDefaultImage() {
+		return "mine.png";
+	}
+
+	@Override
+	public String getPlural() {
+		return "Mines";
+	}
+
+	/**
+	 * Clone the object.
+	 */
+	@Override
+	public Mine copy(boolean zero) {
+		final Mine retval = new Mine(kind, status, id);
+		retval.setImage(image);
+		return retval;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("%s mine of %s", status.toString(), kind);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof Mine) {
+			return kind.equals(((Mine) obj).getKind()) &&
+				status.equals(((Mine) obj).getStatus()) &&
+				id == ((Mine) obj).getId();
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		if (fixture instanceof Mine) {
+		return kind.equals(((Mine) fixture).getKind()) &&
+			status.equals(((Mine) fixture).getStatus());
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String getShortDescription() {
+		return String.format("%s %s mine", status.toString(), kind);
+	}
+
+	/**
+	 * The required Perception check for an explorer to find this fixture.
+	 *
+	 * TODO: Should perhaps be variable and loaded from XML
+	 */
+	@Override
+	public int getDC() {
+		return (TownStatus.Active.equals(status)) ? 15 : 25;
+	}
+}
---
+++ model/src/main/java/common/map/fixtures/resources/StoneDeposit.java
@@ -0,0 +1,144 @@
+package common.map.fixtures.resources;
+
+import java.util.function.Consumer;
+
+import common.map.IFixture;
+import common.map.fixtures.MineralFixture;
+
+/**
+ * A deposit (always exposed for now) of stone.
+ *
+ * TODO: Support non-exposed deposits
+ */
+public class StoneDeposit implements HarvestableFixture, MineralFixture {
+	public StoneDeposit(StoneKind stone, int dc, int id) {
+		this.stone = stone;
+		this.dc = dc;
+		this.id = id;
+	}
+
+	/**
+	 * What kind of stone this deposit is.
+	 */
+	private final StoneKind stone;
+
+	/**
+	 * What kind of stone this deposit is.
+	 */
+	public StoneKind getStone() {
+		return stone;
+	}
+
+	/**
+	 * The DC to discover the deposit.
+	 *
+	 * TODO: Reasonable defaults
+	 */
+	private final int dc;
+
+	/**
+	 * The DC to discover the deposit.
+	 *
+	 * TODO: Reasonable defaults
+	 */
+	@Override
+	public int getDC() {
+		return dc;
+	}
+
+	/**
+	 * ID number.
+	 */
+	private final int id;
+
+	/**
+	 * ID number.
+	 */
+	@Override
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	private String image = "";
+
+	/**
+	 * The filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * Set the filename of an image to use as an icon for this instance.
+	 */
+	@Override
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * Clone the object.
+	 */
+	@Override
+	public StoneDeposit copy(boolean zero) {
+		final StoneDeposit retval = new StoneDeposit(stone, (zero) ? 0 : dc, id);
+		retval.setImage(image);
+		return retval;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj instanceof StoneDeposit) {
+			return ((StoneDeposit) obj).getStone().equals(stone) &&
+				((StoneDeposit) obj).getId() == id;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("A %s deposit, of DC %d", stone.toString(), dc);
+	}
+
+	/**
+	 * The default icon filename.
+	 */
+	@Override
+	public String getDefaultImage() {
+		return "stone.png";
+	}
+
+	@Override
+	public boolean equalsIgnoringID(IFixture fixture) {
+		if (fixture instanceof StoneDeposit) {
+			return ((StoneDeposit) fixture).getStone().equals(stone);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String getKind() {
+		return stone.toString();
+	}
+
+	@Override
+	public String getPlural() {
+		return "Stone deposits";
+	}
+
+	@Override
+	public String getShortDescription() {
+		return "an exposed ``stone`` deposit";
+	}
+}
