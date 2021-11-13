434cf7a2df98756d3944d266ffe407464cdfae19
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-12 19:13:08 -0500

    Port SPWriter back to Java.

---
+++ model/src/main/java/impl/xmlio/SPWriter.java
@@ -0,0 +1,38 @@
+package impl.xmlio;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+
+import common.map.IMapNG;
+
+import lovelace.util.MalformedXMLException;
+
+/**
+ * An interface for map (and other SP XML) writers.
+ */
+public interface SPWriter {
+	/**
+	 * Write a map to file
+	 * @param file The file or stream to write to.
+	 * @param map The map to write
+	 */
+	void write(Path file, IMapNG map) throws MalformedXMLException, IOException;
+
+	/**
+	 * Write a map to a stream.
+	 * @param stream The file or stream to write to.
+	 * @param map The map to write
+	 */
+	void write(Consumer<String> stream, IMapNG map) throws MalformedXMLException;
+
+	/**
+	 * Write an object to a file.
+	 */
+	void writeSPObject(Path file, Object obj) throws MalformedXMLException, IOException;
+
+	/**
+	 * Write an object to a file or stream.
+	 */
+	void writeSPObject(Consumer<String> stream, Object obj) throws MalformedXMLException;
+}
