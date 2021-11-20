bbc1503f28e60be12097b646fbe2b8bdaf8b598c
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-20 15:28:18 -0500

    Port YAReader interface back to Java.

---
+++ model/src/main/java/impl/xmlio/yaxml/YAReader.java
@@ -0,0 +1,61 @@
+package impl.xmlio.yaxml;
+
+import common.xmlio.SPFormatException;
+import lovelace.util.MalformedXMLException;
+
+import java.io.IOException;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+
+/**
+ * An interface for XML readers that can read multiple related types, in the
+ * sixth generation of SP XML I/O ("yet another SP XML reader").
+ */
+public interface YAReader<Item, Value> {
+	@FunctionalInterface
+	static interface IOConsumer<Type> {
+		void accept(Type item) throws IOException;
+	}
+
+	/**
+	 * Read an object from XML.
+	 *
+	 * @throws SPFormatException on SP format errors
+	 * @param element The element (XML tag) being parsed
+	 * @param parent The parent tag
+	 * @param stream The stream of XML events to read more from
+	 */
+	Item read(StartElement element, QName parent, Iterable<XMLEvent> stream)
+		throws SPFormatException, MalformedXMLException;
+
+	/**
+	 * Write an object to the stream.
+	 *
+	 * @throws IOException on I/O error in writing
+	 * @param ostream The stream to write to
+	 * @param obj The object to write
+	 * @param indentation The current indentation level
+	 */
+	void write(IOConsumer<String> ostream, Value obj, int indentation) throws IOException;
+
+	/**
+	 * Whether we can read the given tag.
+	 */
+	boolean isSupportedTag(String tag);
+
+	/**
+	 * Whether we can write the given object.
+	 */
+	boolean canWrite(Object obj); // Should be "obj instanceof Value"
+
+	/**
+	 * Write the given object, when the caller knows the object is the
+	 * right type but the typechecker doesn't. This will probably cause a
+	 * ClassCastException if the types don't in fact match.
+	 */
+	default void writeRaw(IOConsumer<String> ostream, Object obj, int indentation) throws IOException {
+		write(ostream, (Value) obj, indentation);
+	}
+}
