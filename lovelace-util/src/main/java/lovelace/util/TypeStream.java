28430d5412bb59a0cb55a347e6a466234c371f5f
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-20 13:06:45 -0500

    Port TypeStream back to Java.

---
+++ lovelace-util/src/main/java/lovelace/util/TypeStream.java
@@ -0,0 +1,69 @@
+package lovelace.util;
+
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.List;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * A stream of all the types that a given object satisfies.
+ */
+public class TypeStream implements Iterable<Class<?>> {
+	private final Object obj;
+	private final List<Class<?>> cache = new LinkedList<>();
+
+	public TypeStream(Object obj) {
+		this.obj = obj;
+	}
+
+	@Override
+	public Iterator<Class<?>> iterator() {
+		return new TypeIterator(obj, cache);
+	}
+
+	private static class TypeIterator implements Iterator<Class<?>> {
+		private final Object obj;
+		private final LinkedList<Class<?>> ourCopy;
+		private final List<Class<?>> cache;
+		private final Set<Class<?>> classes = new HashSet<>();
+		private final Queue<Class<?>> queue = new LinkedList<>();
+
+		public TypeIterator(Object obj, List<Class<?>> cache) {
+			this.obj = obj;
+			this.cache = cache;
+			ourCopy = new LinkedList<>(cache);
+			queue.add(obj.getClass());
+		}
+
+		@Override
+		public boolean hasNext() {
+			return (!ourCopy.isEmpty()) || (!queue.isEmpty());
+		}
+
+		@Override
+		public Class<?> next() {
+			if (!ourCopy.isEmpty()) {
+				return ourCopy.remove();
+			}
+			while (!queue.isEmpty()) {
+				Class<?> item = queue.remove();
+				if (!classes.contains(item)) {
+					classes.add(item);
+					Class<?> superclass = item.getSuperclass();
+					if (superclass != null) {
+						queue.add(superclass);
+					}
+					for (Class<?> interf : item.getInterfaces()) {
+						queue.add(interf);
+					}
+					cache.add(item);
+					return item;
+				}
+			}
+			throw new NoSuchElementException();
+		}
+	}
+}
