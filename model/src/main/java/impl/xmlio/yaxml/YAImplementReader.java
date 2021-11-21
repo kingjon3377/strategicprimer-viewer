bc05e66384eb56bf4c825d29c78884e7fa11b54e
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-20 23:38:47 -0500

    Port YAImplementReader and YATextReader back to Java.

---
+++ model/src/main/java/impl/xmlio/yaxml/YAImplementReader.java
@@ -0,0 +1,56 @@
+package impl.xmlio.yaxml;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+
+import common.xmlio.SPFormatException;
+import static impl.xmlio.yaxml.YAReader.IOConsumer;
+
+import common.idreg.IDRegistrar;
+import common.map.fixtures.Implement;
+import common.xmlio.Warning;
+
+/**
+ * A reader for {@link Implement}s.
+ */
+/* package */ class YAImplementReader extends YAAbstractReader<Implement, Implement> {
+	public YAImplementReader(Warning warning, IDRegistrar idRegistrar) {
+		super(warning, idRegistrar);
+	}
+
+	@Override
+	public Implement read(StartElement element, QName parent, Iterable<XMLEvent> stream) 
+			throws SPFormatException {
+		requireTag(element, parent, "implement");
+		expectAttributes(element, "kind", "id", "image");
+		Implement retval = new Implement(getParameter(element, "kind"),
+			getOrGenerateID(element), getIntegerParameter(element, "count", 1));
+		spinUntilEnd(element.getName(), stream);
+		retval.setImage(getParameter(element, "image", ""));
+		return retval;
+	}
+
+	@Override
+	public boolean isSupportedTag(String tag) {
+		return "implement".equalsIgnoreCase(tag);
+	}
+
+	@Override
+	public void write(IOConsumer<String> ostream, Implement obj, int indent) throws IOException{
+		writeTag(ostream, "implement", indent);
+		writeProperty(ostream, "kind", obj.getKind());
+		writeProperty(ostream, "id", obj.getId());
+		if (obj.getCount() > 1) {
+			writeProperty(ostream, "count", obj.getCount());
+		}
+		writeImageXML(ostream, obj);
+		closeLeafTag(ostream);
+	}
+
+	@Override
+	public boolean canWrite(Object obj) {
+		return obj instanceof Implement;
+	}
+}
---
+++ model/src/main/java/impl/xmlio/yaxml/YATextReader.java
@@ -0,0 +1,67 @@
+package impl.xmlio.yaxml;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import javax.xml.stream.events.Characters;
+import java.io.IOException;
+
+import common.xmlio.SPFormatException;
+import static impl.xmlio.yaxml.YAReader.IOConsumer;
+
+import common.idreg.IDRegistrar;
+import common.map.fixtures.TextFixture;
+import common.xmlio.Warning;
+import impl.xmlio.exceptions.UnwantedChildException;
+
+/**
+ * A reader for arbitrary-text notes.
+ */
+/* package */ class YATextReader extends YAAbstractReader<TextFixture, TextFixture> {
+	public YATextReader(Warning warning, IDRegistrar idRegistrar) {
+		super(warning, idRegistrar);
+	}
+
+	@Override
+	public boolean isSupportedTag(String tag) {
+		return "text".equalsIgnoreCase(tag);
+	}
+
+	@Override
+	public TextFixture read(StartElement element, QName parent, Iterable<XMLEvent> stream) 
+			throws SPFormatException {
+		requireTag(element, parent, "text");
+		expectAttributes(element, "turn", "image");
+		StringBuilder builder = new StringBuilder();
+		for (XMLEvent event : stream) {
+			if (event instanceof StartElement) {
+				throw new UnwantedChildException(element.getName(), (StartElement) event);
+			} else if (event instanceof Characters) {
+				builder.append(((Characters) event).getData());
+			} else if (isMatchingEnd(element.getName(), event)) {
+				break;
+			}
+		}
+		TextFixture fixture = new TextFixture(builder.toString().trim(),
+			getIntegerParameter(element, "turn", -1));
+		fixture.setImage(getParameter(element, "image", ""));
+		return fixture;
+	}
+
+	@Override
+	public void write(IOConsumer<String> ostream, TextFixture obj, int indent) throws IOException {
+		writeTag(ostream, "text", indent);
+		if (obj.getTurn() != -1) {
+			writeProperty(ostream, "turn", obj.getTurn());
+		}
+		writeImageXML(ostream, obj);
+		ostream.accept(">");
+		ostream.accept(obj.getText().trim());
+		closeTag(ostream, 0, "text");
+	}
+
+	@Override
+	public boolean canWrite(Object obj) {
+		return obj instanceof TextFixture;
+	}
+}
