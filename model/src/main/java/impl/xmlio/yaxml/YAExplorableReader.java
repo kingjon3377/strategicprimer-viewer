77830618f283a0c21ca20daee1b37aefde6604fc
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-20 22:58:08 -0500

    Port YAExplorableReader and YAPlayerReader back to Java.

---
+++ model/src/main/java/impl/xmlio/yaxml/YAExplorableReader.java
@@ -0,0 +1,76 @@
+package impl.xmlio.yaxml;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+
+import common.xmlio.SPFormatException;
+import common.idreg.IDRegistrar;
+import common.map.fixtures.explorable.Battlefield;
+import common.map.fixtures.explorable.ExplorableFixture;
+import common.map.fixtures.explorable.Cave;
+import common.xmlio.Warning;
+import impl.xmlio.exceptions.UnsupportedTagException;
+
+import static impl.xmlio.yaxml.YAReader.IOConsumer;
+
+/**
+ * A reader for Caves and Battlefields.
+ *
+ * FIXME: Extract interface for these two classes so we don't have to pass
+ * the too-general ExplorableFixture as the type parameter here.
+ */
+/* package */ class YAExplorableReader extends YAAbstractReader<ExplorableFixture, ExplorableFixture> {
+	public YAExplorableReader(Warning warning, IDRegistrar idRegistrar) {
+		super(warning, idRegistrar);
+	}
+
+	@Override
+	public boolean isSupportedTag(String tag) {
+		return "cave".equalsIgnoreCase(tag) || "battlefield".equalsIgnoreCase(tag);
+	}
+
+	@Override
+	public ExplorableFixture read(StartElement element, QName parent, Iterable<XMLEvent> stream)
+			throws SPFormatException {
+		requireTag(element, parent, "battlefield", "cave");
+		expectAttributes(element, "id", "dc", "image");
+		int idNum = getOrGenerateID(element);
+		ExplorableFixture retval;
+		switch (element.getName().getLocalPart().toLowerCase()) {
+		case "battlefield":
+			retval = new Battlefield(getIntegerParameter(element, "dc"), idNum);
+			break;
+		case "cave":
+			retval = new Cave(getIntegerParameter(element, "dc"), idNum);
+			break;
+		default:
+			throw UnsupportedTagException.future(element);
+		}
+		spinUntilEnd(element.getName(), stream);
+		retval.setImage(getParameter(element, "image", ""));
+		return retval;
+	}
+
+	@Override
+	public void write(IOConsumer<String> ostream, ExplorableFixture obj, int indent) 
+			throws IOException {
+		if (obj instanceof Battlefield) {
+			writeTag(ostream, "battlefield", indent);
+		} else if (obj instanceof Cave) {
+			writeTag(ostream, "cave", indent);
+		} else {
+			throw new IllegalArgumentException("Only supports Battlefields and Caves");
+		}
+		writeProperty(ostream, "dc", obj.getDC());
+		writeProperty(ostream, "id", obj.getId());
+		writeImageXML(ostream, obj);
+		closeLeafTag(ostream);
+	}
+
+	@Override
+	public boolean canWrite(Object obj) {
+		return obj instanceof Cave || obj instanceof Battlefield;
+	}
+}
---
+++ model/src/main/java/impl/xmlio/yaxml/YAPlayerReader.java
@@ -0,0 +1,67 @@
+package impl.xmlio.yaxml;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.events.StartElement;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+
+import common.xmlio.SPFormatException;
+import common.idreg.IDRegistrar;
+import common.map.Player;
+import common.map.PlayerImpl;
+import common.xmlio.Warning;
+
+import static impl.xmlio.yaxml.YAReader.IOConsumer;
+
+/**
+ * A reader for {@link Player}s."
+/* package */ class YAPlayerReader extends YAAbstractReader<Player, Player> {
+	public YAPlayerReader(Warning warning, IDRegistrar idRegistrar) {
+		super(warning, idRegistrar);
+	}
+
+	@Override
+	public Player read(StartElement element, QName parent, Iterable<XMLEvent> stream)
+			throws SPFormatException {
+		requireTag(element, parent, "player");
+		expectAttributes(element, "number", "code_name", "portrait", "country");
+		requireNonEmptyParameter(element, "number", true);
+		requireNonEmptyParameter(element, "code_name", true);
+		String countryRaw = getParameter(element, "country", "");
+		String country = countryRaw.isEmpty() ? null : countryRaw;
+		// We're thinking about storing "standing orders" in the XML under the <player>
+		// tag; so as to not require players to upgrade to even read their maps once we
+		// start doing so, we *now* only *warn* instead of *dying* if the XML contains
+		// that idiom.
+		spinUntilEnd(element.getName(), stream, "orders", "results", "science");
+		Player retval = new PlayerImpl(getIntegerParameter(element, "number"),
+			getParameter(element, "code_name"), country);
+		retval.setPortrait(getParameter(element, "portrait", ""));
+		return retval;
+	}
+
+	@Override
+	public boolean isSupportedTag(String tag) {
+		return "player".equalsIgnoreCase(tag);
+	}
+
+	@Override
+	public void write(IOConsumer<String> ostream, Player obj, int indent) throws IOException {
+		if (!obj.getName().isEmpty()) {
+			writeTag(ostream, "player", indent);
+			writeProperty(ostream, "number", obj.getPlayerId());
+			writeProperty(ostream, "code_name", obj.getName());
+			writeNonemptyProperty(ostream, "portrait", obj.getPortrait());
+			String country = obj.getCountry();
+			if (country != null) {
+				writeNonemptyProperty(ostream, "country", country);
+			}
+			closeLeafTag(ostream);
+		}
+	}
+
+	@Override
+	public boolean canWrite(Object obj) {
+		return obj instanceof Player;
+	}
+}
