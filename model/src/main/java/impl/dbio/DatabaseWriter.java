d69be581051ac791b29ad59e6f617d3c36ba1be6
Jonathan Lovelace <kingjon3377@gmail.com>
2021-11-28 17:37:08 -0500

    Port DatabaseWriter back to Java.
    Adding a new dependency to wrap JDBC; it looks like it should be pretty
    similar to ceylon.jdbc as an API.

---
+++ model/src/main/java/impl/dbio/DatabaseWriter.java
@@ -0,0 +1,35 @@
+package impl.dbio;
+
+import buckelieg.jdbc.fn.DB;
+
+interface DatabaseWriter<Item, Context> {
+	/**
+	 * Set up the tables that this writer uses on the given connection.
+	 * Should be a no-op if called with the same Sql again.
+	 */
+	void initialize(DB db);
+
+	/**
+	 * Write an object to the database.
+	 */
+	void write(DB db, Item obj, Context context);
+
+	/**
+	 * Whether we can write the given object. Should generally be equivalent to 
+	 * <code>obj instanceof Item &amp;&amp; context instanceof
+	 * Context</code>, but we can't write that here in Java as type
+	 * parameters aren't reified.
+	 *
+	 * TODO: Maybe make AbstractDatabaseWriter take Class parameters and use Class::isInstance there?
+	 */
+	boolean canWrite(Object obj, Object context);
+
+	/**
+	 * Write the given object, when the caller knows the object is the
+	 * right type but the typechecker doesn't. This will probably crash the
+	 * program if the types don't in fact match.
+	 */
+	default void writeRaw(DB db, Object obj, Object context) {
+		write(db, (Item) obj, (Context) context);
+	}
+}
