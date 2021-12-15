91e921ec794039895faa88686664d6b106d0573a
Jonathan Lovelace <kingjon3377@gmail.com>
2021-12-14 22:15:40 -0500

    Port SPOptions, SPOptionsImpl, and EmptyOptions back to Java.

---
+++ drivers.common/src/main/java/drivers/common/SPOptionsImpl.java
@@ -0,0 +1,72 @@
+package drivers.common;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Iterator;
+
+/**
+ * The command-line options passed by the user. At this point we assume that if
+ * any option is passed to an app more than once, the subsequent option
+ * overrides the previous, and any option passed without argument has an
+ * implied argument of "true".
+ *
+ * TODO: Make class final?
+ */
+public class SPOptionsImpl implements SPOptions {
+	Map<String, String> options = new HashMap<String, String>();
+
+	// TODO: Take Map<String, String> instead and use options.putAll()?
+	@SafeVarargs
+	public SPOptionsImpl(Map.Entry<String, String>... existing) {
+		for (Map.Entry<String, String> entry : existing) {
+			options.put(entry.getKey(), entry.getValue());
+		}
+	}
+
+	public void addOption(String option) {
+		addOption(option, "true");
+	}
+
+	public void addOption(String option, String argument) {
+		if ("false".equals(argument)) {
+			options.remove(option);
+		} else {
+			options.put(option, argument);
+		}
+	}
+
+	@Override
+	public boolean hasOption(String option) {
+		return options.containsKey(option);
+	}
+
+	@Override
+	public String getArgument(String option) {
+		return options.getOrDefault(option, "false");
+	}
+
+	@Override
+	public SPOptionsImpl copy() {
+		SPOptionsImpl retval = new SPOptionsImpl();
+		retval.options.putAll(options);
+		return retval;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		for (Map.Entry<String, String> entry : options.entrySet()) {
+			builder.append(entry.getKey());
+			if (!"true".equals(entry.getValue())) {
+				builder.append('=').append(entry.getValue());
+			}
+			builder.append(System.lineSeparator());
+		}
+		return builder.toString();
+	}
+
+	@Override
+	public Iterator<Map.Entry<String, String>> iterator() {
+		return options.entrySet().iterator();
+	}
+}
