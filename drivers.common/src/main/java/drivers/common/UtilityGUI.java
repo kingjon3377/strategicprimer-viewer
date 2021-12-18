58a8d0abcbaecf7a4dd2f84bff6c7a2f9d1da365
Jonathan Lovelace <kingjon3377@gmail.com>
2021-12-17 20:49:24 -0500

    Port UtilityGUI and WorkerGUI back to Java.

---
+++ drivers.common/src/main/java/drivers/common/UtilityGUI.java
@@ -0,0 +1,10 @@
+package drivers.common;
+
+import java.nio.file.Path;
+
+/**
+ * An interface for utility GUI apps that can respond to an "open" menu item.
+ */
+public interface UtilityGUI extends UtilityDriver {
+	void open(Path path);
+}
