698f190a14b1d0199eaee42201484c6064ffee49
Jonathan Lovelace <kingjon3377@gmail.com>
2021-12-17 21:23:36 -0500

    Port DriverFactory and sub-interfaces back to Java.

---
+++ drivers.common/src/main/java/drivers/common/ModelDriverFactory.java
@@ -0,0 +1,30 @@
+package drivers.common;
+
+import common.map.IMutableMapNG;
+import drivers.common.cli.ICLIHelper;
+
+/**
+ * An interface for factories for drivers that operate on map models rather
+ * than directly on files, which must also produce the models that their
+ * drivers consume.
+ *
+ * TODO: Take a more specific model interface as a type parameter.
+ */
+public interface ModelDriverFactory extends DriverFactory {
+	/**
+	 * Create a new instance of the driver with the given environment.
+	 * @param cli The interface to interact with the user, either on the
+	 * console or in a window emulating a console
+	 * @param options Any (already-processed) command-line options
+	 * @param model The driver-model that should be used by the app.
+	 */
+	ModelDriver createDriver(ICLIHelper cli, SPOptions options, IDriverModel model);
+
+	/**
+	 * Create a model to pass to {@link createDriver}. The 'modified' flag is set to false.
+	 * @param map The map
+	 */
+	default IDriverModel createModel(IMutableMapNG map) {
+		return new SimpleDriverModel(map);
+	}
+}
---
+++ drivers.common/src/main/java/drivers/common/UtilityDriverFactory.java
@@ -0,0 +1,16 @@
+package drivers.common;
+
+import drivers.common.cli.ICLIHelper;
+
+/**
+ * An interface for factories for drivers that operate on files rather than pre-parsed maps.
+ */
+public interface UtilityDriverFactory extends DriverFactory {
+	/**
+	 * The driver.
+	 * @param cli The interface to interact with the user, either on the
+	 * console or in a window emulating a console
+	 * @param options Any (already-processed) command-line options
+	 */
+	UtilityDriver createDriver(ICLIHelper cli, SPOptions options);
+}
