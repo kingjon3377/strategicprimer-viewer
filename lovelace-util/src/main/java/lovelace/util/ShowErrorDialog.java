9197bac1de2152d63112e8984712f9ed1ddbd030
Jonathan Lovelace <kingjon3377@gmail.com>
2022-01-08 12:33:24 -0500

    Port showErrorDialog() back to Java.

---
+++ lovelace-util/src/main/java/lovelace/util/ShowErrorDialog.java
@@ -0,0 +1,22 @@
+package lovelace.util;
+
+import javax.swing.JOptionPane;
+
+import java.awt.Component;
+
+import org.jetbrains.annotations.Nullable;
+
+public class ShowErrorDialog {
+	/**
+	 * Show an error dialog to the user.
+	 * @deprecated Inline into callers
+	 * @param parent The parent component for the dialog. {@link JOptionPane} doesn't seem to care
+	 * if it is null.
+	 * @param title What to title the dialog.
+	 * @param message The error message to show the user.
+	 */
+	@Deprecated
+	public static void showErrorDialog(@Nullable Component parent, String title, String message) {
+		JOptionPane.showMessageDialog(parent, message, title, JOptionPane.ERROR_MESSAGE);
+	}
+}
