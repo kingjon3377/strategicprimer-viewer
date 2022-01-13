8a7cc0787e6b31a9c3b2c95ef10c89a68cceaf95
Jonathan Lovelace <kingjon3377@gmail.com>
2022-01-12 19:11:22 -0500

    Port landRaces and SimpleTerrain to Java.

---
+++ drivers.utility/src/main/java/utility/LandRaces.java
@@ -0,0 +1,20 @@
+package utility;
+
+import java.util.Collections;
+import java.util.Arrays;
+import java.util.List;
+
+/* package */ class LandRaces {
+	private LandRaces() {}
+	/**
+	 * List of non-aquatic races.
+	 *
+	 * Left outside {@link MapCheckerCLI} because it's also used in {@link TodoFixerCLI}.
+	 *
+	 * TODO: Move into one of those classes, since unlike in Ceylon it has
+	 * to be in a class and making a separate class just for this seems
+	 * silly.
+	 */
+	public static final List<String> LAND_RACES = Collections.unmodifiableList(
+		Arrays.asList("Danan", "dwarf", "elf", "half-elf", "gnome", "human"));
+}
