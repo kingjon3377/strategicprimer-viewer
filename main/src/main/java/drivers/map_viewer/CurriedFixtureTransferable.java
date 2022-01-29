504d19e96d77ed99fc2aea859f69fdebd50af71f
Jonathan Lovelace <kingjon3377@gmail.com>
2022-01-29 09:39:26 -0500

    Port FixtureTransferable and CurriedFixtureTransferable back to Java.

---
+++ main/src/main/java/drivers/map_viewer/CurriedFixtureTransferable.java
@@ -0,0 +1,59 @@
+package drivers.map_viewer;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.datatransfer.Transferable;
+
+import common.map.TileFixture;
+
+import java.util.List;
+import java.util.Collections;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
+/**
+ * A class to transfer a list of TileFixtures by drag-and-drop.
+ *
+ * TODO: Generalize, rename to CurriedTransferable, move to lovelace.util
+ */
+/* package */ class CurriedFixtureTransferable implements Transferable {
+	public static final DataFlavor FLAVOR =
+		new DataFlavor(CurriedFixtureTransferable.class, "CurriedTransferable");
+
+	private List<Transferable> payload;
+
+	public CurriedFixtureTransferable(TileFixture... list) {
+		payload = Collections.unmodifiableList(Stream.of(list)
+			.map(FixtureTransferable::new).collect(Collectors.toList()));
+	}
+
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		return new DataFlavor[] { FLAVOR };
+	}
+
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor candidate) {
+		return FLAVOR.equals(candidate);
+	}
+
+	@Override
+	public List<Transferable> getTransferData(DataFlavor candidate) throws UnsupportedFlavorException {
+		if (FLAVOR.equals(candidate)) {
+			return payload;
+		} else {
+			throw new UnsupportedFlavorException(candidate);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return String.format("CurriedFixtureTransferable with payload containing %d elements",
+			payload.size());
+	}
+
+	@Override
+	public int hashCode() {
+		return payload.hashCode();
+	}
+}
---
+++ main/src/main/java/drivers/map_viewer/FixtureTransferable.java
@@ -0,0 +1,55 @@
+package drivers.map_viewer;
+
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.UnsupportedFlavorException;
+import java.awt.datatransfer.Transferable;
+
+import common.map.TileFixture;
+
+/**
+ * A class to transfer a TileFixture by drag-and-drop.
+ */
+/* package */ class FixtureTransferable implements Transferable {
+	public static final DataFlavor FLAVOR = new DataFlavor(TileFixture.class, "TileFixture");
+
+	private final TileFixture payload;
+
+	public FixtureTransferable(TileFixture data) {
+		payload = data;
+	}
+
+	@Override
+	public DataFlavor[] getTransferDataFlavors() {
+		return new DataFlavor[] { FLAVOR };
+	}
+
+	@Override
+	public boolean isDataFlavorSupported(DataFlavor candidate) {
+		return FLAVOR.equals(candidate);
+	}
+
+	@Override
+	public TileFixture getTransferData(DataFlavor candidate) throws UnsupportedFlavorException {
+		if (FLAVOR.equals(candidate)) {
+			return payload;
+		} else {
+			throw new UnsupportedFlavorException(candidate);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "FixtureTransferable transferring " + payload.getShortDescription();
+	}
+
+	@Override
+	public boolean equals(Object that) {
+		return that instanceof FixtureTransferable &&
+			((FixtureTransferable) that).payload.equals(payload);
+	}
+
+	@Override
+	public int hashCode() {
+		return payload.hashCode();
+	}
+}
