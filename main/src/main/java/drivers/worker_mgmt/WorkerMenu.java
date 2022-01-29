e0b702c79c0aab9ef3670f1272d85f1429909068
Jonathan Lovelace <kingjon3377@gmail.com>
2022-01-29 11:57:00 -0500

    Port WorkerMenu back to Java.

---
+++ main/src/main/java/drivers/worker_mgmt/WorkerMenu.java
@@ -0,0 +1,26 @@
+package drivers.worker_mgmt;
+
+import javax.swing.JMenuBar;
+import java.awt.event.ActionListener;
+import drivers.common.ISPDriver;
+import drivers.gui.common.SPMenu;
+import java.awt.Component;
+
+/**
+ * A set of menus for the worker GUI (and other related apps).
+ */
+public class WorkerMenu {
+	private WorkerMenu() {}
+	/**
+	 * @param handler The broker that handles menu items, or arranges for them to be handled
+	 * @param component Any component in the window this is to be attached
+	 * to, which should close on "Close"
+	 * @param driver The current driver
+	 */
+	public static JMenuBar workerMenu(ActionListener handler, Component component,
+			ISPDriver driver) {
+		return SPMenu.forWindowContaining(component, SPMenu.createFileMenu(handler, driver),
+			SPMenu.disabledMenu(SPMenu.createMapMenu(handler, driver)),
+			SPMenu.createViewMenu(handler, driver));
+	}
+}
