7328cbf59aa09bde66cdd0a3d4787207fc989084
Jonathan Lovelace <kingjon3377@gmail.com>
2022-01-29 20:22:27 -0500

    Make Range implement Iterator<Integer>.

--- lovelace-util/src/main/java/lovelace/util/Range.java
+++ lovelace-util/src/main/java/lovelace/util/Range.java
@@ -1,10 +1,14 @@
 package lovelace.util;
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import org.jetbrains.annotations.NotNull;
+
 /**
  * A class encapsulating a range of integers, to fluently test whether other
  * integers are in the range. (Replacement for Ceylon's <code>Range&lt;Integer&gt;</code>.)
  */
-public class Range {
+public class Range implements Iterable<Integer> {
 	public Range(int lowerBound, int upperBound) {
 		if (upperBound < lowerBound) {
 			throw new IllegalArgumentException(
@@ -31,5 +35,35 @@ public class Range {
 
 	public int size() {
 		return upperBound - lowerBound + 1;
+	}
+
+	@NotNull
+	@Override
+	public Iterator<Integer> iterator() {
+		return new RangeIterator(this);
+	}
+
+	private static class RangeIterator implements Iterator<Integer> {
+		int current;
+		int upperBound;
+		public RangeIterator(Range range) {
+			this.current = range.lowerBound - 1;
+			this.upperBound = range.upperBound;
+		}
+
+		@Override
+		public boolean hasNext() {
+			return current < upperBound;
+		}
+
+		@Override
+		public Integer next() {
+			if (current < upperBound) {
+				current++;
+				return current;
+			} else {
+				throw new NoSuchElementException();
+			}
+		}
 	}
 }
