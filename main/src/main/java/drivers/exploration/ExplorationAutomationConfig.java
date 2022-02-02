7c9fe09f2d28e85782d61e8ed29de81c62c87804
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-01 20:09:46 -0500

    Port ExplorationCLIHelper to Java.

---
+++ main/src/main/java/drivers/exploration/ExplorationAutomationConfig.java
@@ -0,0 +1,178 @@
+package drivers.exploration;
+
+import common.map.IMapNG;
+import common.map.Player;
+import common.map.Point;
+import common.map.TileFixture;
+import common.map.fixtures.mobile.IUnit;
+import common.map.fixtures.mobile.Immortal;
+import common.map.fixtures.towns.AbstractTown;
+import common.map.fixtures.towns.IFortress;
+import common.map.fixtures.towns.TownStatus;
+import common.map.fixtures.towns.Village;
+import drivers.common.cli.ICLIHelper;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import org.jetbrains.annotations.Nullable;
+
+/* package */ class ExplorationAutomationConfig {
+	public ExplorationAutomationConfig(Player player) {
+		this.player = player;
+		conditions = Collections.unmodifiableList(
+			Arrays.asList(new Condition<IFortress>("at others' fortresses",
+					fixture -> "a fortress belonging to " + fixture.getOwner(), IFortress.class),
+				new Condition<AbstractTown>("at active towns",
+					fixture -> String.format("a %s active %s", fixture.getTownSize(), fixture.getKind()),
+					AbstractTown.class, t -> TownStatus.Active.equals(t.getStatus())),
+				new Condition<AbstractTown>("at inactive towns",
+					fixture -> String.format("a %s %s %s", fixture.getTownSize(), AbstractTown.class,
+						fixture.getStatus(), fixture.getKind()),
+					AbstractTown.class, t -> !TownStatus.Active.equals(t.getStatus())),
+				new Condition<Village>("at independent villages", "an independent village",
+					Village.class, v -> v.getOwner().isIndependent()),
+				new Condition<Village>("at other players' villages", "another player's village",
+					Village.class, v -> !v.getOwner().equals(this.player), v -> !v.getOwner().isIndependent()),
+				// TODO: For "your villages" (and perhaps other towns), include name in stop message
+				new Condition<Village>("at villages sworn to you", "one of your villages",
+					Village.class, v -> v.getOwner().equals(this.player)),
+				new Condition<IUnit>("on meeting other players' units",
+					unit -> "a unit belonging to " + unit.getOwner(), IUnit.class,
+					u -> !u.getOwner().equals(this.player), u -> !u.getOwner().isIndependent()),
+				new Condition<IUnit>("on meeting independent units", "an independent unit", IUnit.class,
+					// TODO: Provide helper default methods isIndependent() and sameOwner() in HasOwner?
+					u -> u.getOwner().isIndependent()),
+				new Condition<Immortal>("on meeting an immortal", i -> "a(n) " + i.getShortDescription(),
+					Immortal.class)));
+	}
+
+	private final Player player;
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	// TODO: What does this need to become static?
+	private class Condition<Type extends TileFixture> {
+		/**
+		 * @param configExplanation A description to use in the question asking whether to stop for this condition.
+		 * @param stopExplanation A description to use when stopping because of this condition.
+		 * @param conditions Returns true when a tile fixture matches all of these conditions.
+		 */
+		public Condition(String configExplanation, String stopExplanation, Class<Type> cls,
+				Predicate<Type>... conditions) {
+			this.configExplanation = configExplanation;
+			this.stopExplanation = ignored -> stopExplanation;
+			this.conditions = Collections.unmodifiableList(Arrays.asList(conditions));
+			this.cls = cls;
+		}
+
+		/**
+		 * @param configExplanation A description to use in the question asking whether to stop for this condition.
+		 * @param stopExplanation A factory for a description to use when stopping because of this condition.
+		 * @param conditions Returns true when a tile fixture matches all of these conditions.
+		 */
+		public Condition(String configExplanation, Function<Type, String> stopExplanation,
+				Class<Type> cls, Predicate<Type>... conditions) {
+			this.configExplanation = configExplanation;
+			this.stopExplanation = stopExplanation;
+			this.conditions = Collections.unmodifiableList(Arrays.asList(conditions));
+			this.cls = cls;
+		}
+
+		private final Class<Type> cls;
+
+		/**
+		 * A description to use in the question asking whether to stop for this condition.
+		 */
+		private final String configExplanation;
+
+		/**
+		 * A description to use in the question asking whether to stop for this condition.
+		 */
+		public String getConfigExplanation() {
+			return configExplanation;
+		}
+
+		/**
+		 * A factory for a description to use when stopping because of this condition.
+		 */
+		private final Function<Type, String> stopExplanation;
+
+		/**
+		 * A factory for a description to use when stopping because of this condition.
+		 */
+		public Function<Type, String> getStopExplanation() {
+			return stopExplanation;
+		}
+
+		/**
+		 * Returns true when a tile fixture matches all of these conditions.
+		 */
+		private final List<Predicate<Type>> conditions;
+
+		private boolean allConditions(TileFixture fixture) {
+			if (cls.isInstance(fixture)) {
+				for (Predicate<Type> condition : conditions) {
+					if (!condition.test((Type) fixture)) {
+						return false;
+					}
+				}
+				return true;
+			} else {
+				return false;
+			}
+		}
+
+		/**
+		 * Returns true when the given tile matches this condition.
+		 */
+		public boolean matches(IMapNG map, Point point) {
+			return map.getFixtures(point).stream().anyMatch(this::allConditions);
+		}
+	}
+
+	private final List<Condition<? extends TileFixture>> conditions;
+
+	@Nullable
+	private List<Condition<? extends TileFixture>> enabledConditions = null;
+
+	// TODO: inline into callers
+	private BiPredicate<IMapNG, Point> matchesCondition(Condition<? extends TileFixture> condition) {
+		return condition::matches;
+	}
+
+	public boolean stopAtPoint(ICLIHelper cli, IMapNG map, Point point) {
+		List<Condition<? extends TileFixture>> localEnabledConditions;
+		if (enabledConditions != null) { // TODO: invert
+			localEnabledConditions = enabledConditions;
+		} else {
+			List<Condition<? extends TileFixture>> temp = new ArrayList<>();
+			for (Condition<? extends TileFixture> condition : conditions) {
+				Boolean resp = cli.inputBooleanInSeries("Stop for instructions " +
+					condition.getConfigExplanation() + "?");
+				if (resp == null) {
+					// EOF, so we want to abort the caller's loop
+					return true;
+				} else if (resp) {
+					temp.add(condition);
+				}
+			}
+			localEnabledConditions = Collections.unmodifiableList(temp);
+			enabledConditions = localEnabledConditions;
+		}
+		Condition<? extends TileFixture> matchingCondition = localEnabledConditions.stream()
+			.filter(c -> c.matches(map, point)).findFirst().orElse(null);
+		if (matchingCondition != null) {
+			cli.println(String.format("There is %s here, so the explorer stops.",
+				matchingCondition.getStopExplanation()));
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
---
+++ main/src/main/java/drivers/exploration/ExplorationCLIHelper.java
@@ -0,0 +1,373 @@
+package drivers.exploration;
+
+import common.map.Direction;
+import common.map.HasExtent;
+import common.map.HasOwner;
+import common.map.HasPopulation;
+import common.map.IMapNG;
+import common.map.Point;
+import common.map.River;
+import common.map.TileFixture;
+import common.map.TileType;
+import common.map.fixtures.mobile.Animal;
+import common.map.fixtures.mobile.AnimalTracks;
+import common.map.fixtures.mobile.IUnit;
+import common.map.fixtures.towns.Village;
+import drivers.common.SelectionChangeListener;
+import drivers.common.cli.AppletChooser;
+import drivers.common.cli.ICLIHelper;
+import drivers.common.cli.SimpleApplet;
+import either.Either;
+import exploration.common.HuntingModel;
+import exploration.common.IExplorationModel;
+import exploration.common.MovementCostListener;
+import exploration.common.Pathfinder;
+import exploration.common.PathfinderFactory;
+import exploration.common.SimpleMovementModel;
+import exploration.common.Speed;
+import exploration.common.TraversalImpossibleException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import org.javatuples.Pair;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * The logic split out of {@link ExplorationCLI}, some also used in {@link drivers.turnrunning.TurnRunningCLI}
+ */
+public class ExplorationCLIHelper implements MovementCostListener, SelectionChangeListener {
+	private static final Logger LOGGER = Logger.getLogger(ExplorationCLIHelper.class.getName());
+	private final IExplorationModel model;
+	private final ICLIHelper cli;
+	private final HuntingModel huntingModel;
+	public ExplorationCLIHelper(IExplorationModel model, ICLIHelper cli) {
+		this.model = model;
+		this.cli = cli;
+		this.huntingModel = new HuntingModel(model.getMap());
+		pather = PathfinderFactory.pathfinder(model.getSubordinateMaps().iterator().hasNext() ?
+			model.getSubordinateMaps().iterator().next() : model.getMap());
+		automationConfig = new ExplorationAutomationConfig(model.getMap()
+			.getCurrentPlayer());
+		appletChooser = new AppletChooser(this.cli,
+			new SimpleApplet(model::swearVillages, "Swear any village here to the player", "swear"),
+			new SimpleApplet(model::dig, "Dig to expose some ground here", "dig"));
+	}
+
+	/**
+	 * The explorer's current movement speed.
+	 */
+	private Speed speed = Speed.Normal;
+
+	/**
+	 * Let the user change the explorer's speed
+	 */
+	private void changeSpeed() {
+		Speed temp = cli.chooseFromList(Arrays.asList(Speed.values()),
+			"Possible Speeds:", "No speeds available", "Chosen Speed: ", true).getValue1();
+		if (temp != null) {
+			speed = temp;
+		}
+	}
+
+	/**
+	 * Copy the given fixture to subordinate maps and print it to the output stream.
+	 */
+	private void printAndTransferFixture(Point destPoint, @Nullable TileFixture fixture, HasOwner mover,
+	                                     boolean automatic) {
+		if (fixture != null) {
+			if (automatic) {
+				cli.print(fixture.toString());
+				cli.println(" (automatically)");
+			} else {
+				cli.println(fixture.toString());
+			}
+			boolean zero;
+			if (fixture instanceof HasOwner && (!((HasOwner) fixture).getOwner().equals(mover.getOwner())
+					|| fixture instanceof Village)) {
+				zero = true;
+			} else if (fixture instanceof HasPopulation || fixture instanceof HasExtent) {
+				zero = true;
+			} else {
+				zero = false;
+			}
+			model.copyToSubMaps(destPoint, fixture, zero);
+		}
+	}
+
+	private int totalMP = 0;
+	private int runningTotal = 0;
+
+	public int getMovement() {
+		return runningTotal;
+	}
+
+	@Override
+	public void deduct(int cost) {
+		runningTotal -= cost;
+	}
+
+	private final LinkedList<Point> proposedPath = new LinkedList<Point>();
+
+	private ExplorationAutomationConfig automationConfig;
+
+	private static final List<String> COMMANDS = Collections.unmodifiableList(Arrays.asList("Set Speed", "SW", "S", "SE",
+		"W", "Linger", "E", "NW", "N", "NE", "Toward Point", "Quit"));
+
+	private final String usage = IntStream.range(0, COMMANDS.size())
+		.mapToObj(i -> i + ": " + COMMANDS.get(i)).collect(Collectors.joining(", "));
+
+	/**
+	 * When the selected unit changes, print the unit's details and ask how many MP the unit has.
+	 */
+	@Override
+	public void selectedUnitChanged(@Nullable IUnit old, @Nullable IUnit newSelection) {
+		if (newSelection != null) { // TODO What if old == newSelection?
+			cli.print("Details of the unit (apparently at ");
+			cli.print(model.getSelectedUnitLocation().toString());
+			cli.println("):");
+			cli.println(newSelection.getVerbose());
+			Integer number = cli.inputNumber("MP the unit has: ");
+			if (number != null) {
+				runningTotal = totalMP = number;
+			}
+			if (!automationConfig.getPlayer().equals(newSelection.getOwner())) {
+				automationConfig = new ExplorationAutomationConfig(newSelection.getOwner());
+			}
+		}
+	}
+
+	private final AppletChooser<SimpleApplet> appletChooser;
+
+	private final Pathfinder pather;
+
+	/**
+	 * If the unit has a proposed path, move one more tile along it;
+	 * otherwise, ask the user for directions once and make that move, then
+	 * return to the caller.
+	 */
+	// No need to set the 'modified' flag anywhere in this method, as
+	// ExplorationModel.move() always sets it.
+	public void moveOneStep() {
+		IUnit mover = model.getSelectedUnit();
+		if (mover != null) {
+			Point point = model.getSelectedUnitLocation();
+			Direction direction;
+			Point proposedDestination = proposedPath.pollFirst();
+			if (proposedDestination != null) { // TODO: invert
+				direction = Stream.of(Direction.values())
+					.filter(d -> proposedDestination.equals(model.getDestination(point, d)))
+					.findAny().orElse(Direction.Nowhere);
+				if (proposedDestination.equals(point)) {
+					return;
+				} else if (Direction.Nowhere.equals(direction)) {
+					cli.println(String.format("Next step %s isn't adjacent to %s",
+						proposedDestination, point));
+					return;
+				}
+				cli.println(String.format("%d/%d MP remaining. Current speed: %s.",
+					runningTotal, totalMP, speed.getShortName()));
+			} else {
+				cli.println(String.format("%d/%d MP remaining. Current speed: %s.",
+					runningTotal, totalMP, speed.getShortName()));
+				cli.printlnAtInterval(usage);
+				int directionNum = Optional.ofNullable(cli.inputNumber("Direction to move: ")).orElse(-1);
+				switch (directionNum) {
+				case 0:
+					changeSpeed();
+					return;
+				case 1:
+					direction = Direction.Southwest;
+					break;
+				case 2:
+					direction = Direction.South;
+					break;
+				case 3:
+					direction = Direction.Southeast;
+					break;
+				case 4:
+					direction = Direction.West;
+					break;
+				case 5:
+					direction = Direction.Nowhere;
+					break;
+				case 6:
+					direction = Direction.East;
+					break;
+				case 7:
+					direction = Direction.Northwest;
+					break;
+				case 8:
+					direction = Direction.North;
+					break;
+				case 9:
+					direction = Direction.Northeast;
+					break;
+				case 10:
+					Point destination = cli.inputPoint("Location to move toward: ");
+					if (destination != null) { // TODO: invert
+						Pair<Integer, Iterable<Point>> pair =
+							pather.getTravelDistance(point, destination);
+						int cost = pair.getValue0();
+						Iterable<Point> path = pair.getValue1();
+						if (!path.iterator().hasNext()) {
+							cli.println(
+								"S/he doesn't know how to get there from here.");
+						} else {
+							path.forEach(proposedPath::addLast);
+						}
+						return;
+					} else {
+						// EOF
+						runningTotal = 0;
+						return;
+					}
+				default:
+					runningTotal = 0;
+					return;
+				}
+			}
+
+			Point destPoint = model.getDestination(point, direction);
+			try {
+				model.move(direction, speed);
+			} catch (TraversalImpossibleException except) {
+				LOGGER.fine("Attempted movement to impossible destination");
+				cli.println("That direction is impassable; we've made sure all maps show that at a cost of 1 MP");
+				return;
+			}
+
+			List<TileFixture> constants = new ArrayList<>();
+			IMapNG map = model.getMap();
+			List<TileFixture> allFixtures = new ArrayList<>();
+
+			for (TileFixture fixture : map.getFixtures(destPoint)) {
+				if (SimpleMovementModel.shouldAlwaysNotice(mover, fixture)) {
+					constants.add(fixture);
+				} else if (SimpleMovementModel.shouldSometimesNotice(mover, speed, fixture)) {
+					allFixtures.add(fixture);
+				}
+			}
+
+			/*Animal|AnimalTracks|HuntingModel.NothingFound*/ TileFixture tracksAnimal;
+
+			// Since not-visible terrain is impassable, by this point we know the tile is visible.
+			TileType terrain = map.getBaseTerrain(destPoint);
+			if (TileType.Ocean.equals(terrain)) {
+				tracksAnimal = huntingModel.fish(destPoint).iterator().next().getValue1();
+			} else {
+				tracksAnimal = huntingModel.hunt(destPoint).iterator().next().getValue1();
+			}
+
+			if (tracksAnimal instanceof Animal) {
+				allFixtures.add(new AnimalTracks((((Animal) tracksAnimal).getKind())));
+			} else if (tracksAnimal instanceof AnimalTracks) {
+				allFixtures.add(tracksAnimal.copy(false));
+			}
+
+			if (Direction.Nowhere.equals(direction)) {
+				while (true) {
+				Boolean response = cli.inputBooleanInSeries("Take an action here?");
+				if (response == null) {
+					// EOF
+					runningTotal = 0;
+					return;
+				} else if (!response) {
+					break;
+				}
+				Either<SimpleApplet, Boolean> choice = appletChooser.chooseApplet();
+				SimpleApplet applet = choice.fromLeft().orElse(null);
+				Boolean bool = choice.fromRight().orElse(null);
+				if (applet == null) {
+					if (bool == null) {
+						// EOF
+						runningTotal = 0;
+						return;
+					} else if (!bool) {
+						break;
+					}
+				} else {
+					applet.invoke();
+				}
+			}
+		}
+
+		String mtn;
+		if (map.isMountainous(destPoint)) {
+			mtn = "mountainous ";
+		} else {
+			mtn = "";
+		}
+		model.copyTerrainToSubMaps(destPoint);
+
+		cli.print(String.format("The explorer comes to %s, a %s%s tile", destPoint, mtn,
+			Optional.ofNullable(map.getBaseTerrain(destPoint)).map(TileType::toString)
+				.orElse("unknown-terrain")));
+			Collection<River> rivers = map.getRivers(destPoint);
+			boolean anyRivers;
+			if (rivers.contains(River.Lake)) {
+				anyRivers = true;
+				if (rivers.stream().anyMatch(r -> !River.Lake.equals(r))) {
+					cli.print(" with a lake and (a) river(s) flowing ");
+				} else {
+					cli.print(" with a lake");
+				}
+			} else if (!rivers.isEmpty()) {
+				anyRivers = true;
+				cli.print(" with (a) river(s) flowing ");
+			} else {
+				anyRivers = false;
+			}
+			cli.println(rivers.stream().filter(r -> !River.Lake.equals(r)).map(River::toString)
+				.collect(Collectors.joining(", ")));
+
+			if (!map.getRoads(destPoint).isEmpty()) {
+				if (anyRivers) {
+					cli.print(". There are (a) road(s) to the ");
+				} else {
+					cli.print(" with (a) road(s) to the ");
+				}
+				cli.println(map.getRoads(destPoint).keySet().stream().map(Direction::toString)
+					.collect(Collectors.joining(", "))); // TODO: Report on road quality
+			}
+			Iterable<TileFixture> noticed = SimpleMovementModel.selectNoticed(allFixtures, Function.identity(),
+				mover, speed);
+
+			if (!constants.isEmpty() || noticed.iterator().hasNext()) {
+				cli.println("The following were noticed:");
+				for (TileFixture fixture : constants) {
+					printAndTransferFixture(destPoint, fixture, mover, true);
+				}
+				for (TileFixture fixture : noticed) {
+					printAndTransferFixture(destPoint, fixture, mover, false);
+				}
+			}
+
+			if (!proposedPath.isEmpty() && automationConfig.stopAtPoint(cli,
+					model.getSubordinateMaps().iterator().hasNext() ?
+						model.getSubordinateMaps().iterator().next() : model.getMap(),
+					destPoint)) {
+				proposedPath.clear();
+			}
+		} else {
+			cli.println("No unit is selected");
+		}
+	}
+
+	@Override
+	public void selectedPointChanged(@Nullable Point previousSelection, Point newSelection) {}
+
+	@Override
+	public void interactionPointChanged() {}
+
+	@Override
+	public void cursorPointChanged(@Nullable Point previousCursor, Point newCursor) {}
+}
