3dc5800b7fb20987b928a88e518a54e7f56389a3
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-12 10:57:07 -0500

    ActionWrapper: Remove abandoned TODO comment.

--- lovelace-util/src/main/java/lovelace/util/ActionWrapper.java
+++ lovelace-util/src/main/java/lovelace/util/ActionWrapper.java
@@ -9,8 +9,6 @@ import java.awt.event.ActionListener;
  * A wrapper around an {@link ActionListener} (or equivalent lambda) that extends {@link AbstractAction}, for the
  * exceedingly common case of a JDK method requiring an {@link Action} when we don't need more functionality than a
  * single method accepting an {@link ActionEvent}.
- * <p>
- * TODO: Do we really need this in Java?
  */
 public class ActionWrapper extends AbstractAction {
 	private final ActionListener wrapped;
