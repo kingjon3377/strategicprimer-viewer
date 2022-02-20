cf64bb24f1f698fd1e2e72719eac725c3d4a07a7
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-19 23:04:05 -0500

    FunctionalPopupMenu: Make class final.

--- lovelace-util/src/main/java/lovelace/util/FunctionalPopupMenu.java
+++ lovelace-util/src/main/java/lovelace/util/FunctionalPopupMenu.java
@@ -6,7 +6,7 @@ import javax.swing.JMenuItem;
 /**
  * A {@link JPopupMenu} that takes its menu items as constructor parameters.
  */
-public class FunctionalPopupMenu extends JPopupMenu {
+public final class FunctionalPopupMenu extends JPopupMenu {
 	private static final long serialVersionUID = 1;
 	public FunctionalPopupMenu(final JMenuItem... items) {
 		for (final JMenuItem item : items) {
