48e4d9cb3ba785fded84c9fb5a1d431a88a0e4ec
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-19 23:04:23 -0500

    ListenedButton: Make class final.

--- lovelace-util/src/main/java/lovelace/util/ListenedButton.java
+++ lovelace-util/src/main/java/lovelace/util/ListenedButton.java
@@ -7,7 +7,7 @@ import javax.swing.JButton;
 /**
  * A button that takes its listeners as constructor parameters.
  */
-public class ListenedButton extends JButton {
+public final class ListenedButton extends JButton {
 	private static final long serialVersionUID = 1;
 	/**
 	 * @param text The text to put on the button
