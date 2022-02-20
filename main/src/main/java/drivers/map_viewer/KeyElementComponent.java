ffdeaddf51c51c09edac8774c36e611dc4eace46
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-19 23:12:11 -0500

    KeyElementComponent: Make class final.

--- main/src/main/java/drivers/map_viewer/KeyElementComponent.java
+++ main/src/main/java/drivers/map_viewer/KeyElementComponent.java
@@ -9,7 +9,7 @@ import javax.swing.JComponent;
 /**
  * The part of the key showing a tile's color.
  */
-/* package */ class KeyElementComponent extends JComponent {
+/* package */ final class KeyElementComponent extends JComponent {
 	private static final long serialVersionUID = 1L;
 	public KeyElementComponent(final Color color, final Dimension minimum, final Dimension preferred,
 	                           final Dimension maximum) {
