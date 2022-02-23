8b16d8ea7f18006903c128b629e757eadb47bc85
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-23 18:56:36 -0500

    CountingCLI: Include unit/fortress members, worker equipment/mounts more robustly.
    This previously didn't include equipment at all, but because unit
    members were only handled specially as an operation on the units, that
    never got caught.
    
    Includes code needed (once this is being done at all) because of #45.
    
    "Cherry-picked" (hand-ported) from
    1d0314730131446ccdacdc9dfe0121b31a6f9f94.

--- drivers.utility/src/main/java/utility/CountingCLI.java
+++ drivers.utility/src/main/java/utility/CountingCLI.java
@@ -1,7 +1,14 @@
 package utility;
 
+import common.map.IFixture;
 import common.map.fixtures.FixtureIterable;
+import common.map.fixtures.IResourcePile;
+import common.map.fixtures.Implement;
+import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+import org.eclipse.jdt.annotation.Nullable;
 import org.javatuples.Pair;
 import java.util.Comparator;
 import drivers.common.IDriverModel;
@@ -89,7 +96,7 @@ import common.map.fixtures.mobile.AnimalTracks;
 
 	private <Base, Key, Count extends Number&Comparable<Count>> void printSummary(
 			final MappedCounter<Base, Key, Count> counter, final String total) {
-		printSummary(counter, total, CountingCLI::parameterizedCountSpaceKey);
+		printSummary(counter, total, CountingCLI::parameterizedCountSpaceKey, 0);
 	}
 
 	private <Base, Key, Count extends Number&Comparable<Count>> void printSummary(
@@ -99,12 +106,34 @@ import common.map.fixtures.mobile.AnimalTracks;
 
 	private <Base, Key, Count extends Number&Comparable<Count>> void printSummary(
 			final MappedCounter<Base, Key, Count> counter, final String total,
-			final Function<Pair<Key, Count>, String> each) {
+			final int indent) {
+		printSummary(counter, total, CountingCLI::parameterizedCountSpaceKey, indent);
+	}
+
+	private <Base, Key, Count extends Number&Comparable<Count>> void printSummary(
+			final MappedCounter<Base, Key, Count> counter, final String total,
+			final Function<Pair<Key, Count>, String> each, final int indent) {
 		if (counter.getTotal().doubleValue() > 0.0) {
-			cli.println(total);
-			cli.println();
-			counter.stream().map(each).forEach(cli::println);
-			cli.println();
+			if (indent > 0) {
+				// TODO: Once in Java 11, change to print("  ".repeat(indent - 1));
+				for (int i = 0; i < indent - 1; i++) {
+					cli.print("  ");
+				}
+				cli.print("- ");
+			}
+			if (indent <= 0) {
+				cli.println(total);
+			}
+			// TODO: Once in Java 11, use "  ".repeat(indent) to prepend indentation and stream().map(each) ...
+			for (Pair<Key, Count> pair : counter) {
+				for (int i = 0; i < indent; i++) {
+					cli.print("  ");
+				}
+				cli.println(each.apply(pair));
+			}
+			if (indent <= 0) {
+				cli.println(total);
+			}
 		}
 	}
 
@@ -173,7 +202,7 @@ import common.map.fixtures.mobile.AnimalTracks;
 		return t -> !pred.test(t);
 	}
 
-	private static Predicate<TileFixture> notA(final Class<? extends TileFixture> cls) {
+	private static Predicate<IFixture> notA(final Class<? extends IFixture> cls) {
 		return negate(cls::isInstance);
 	}
 
@@ -185,6 +214,29 @@ import common.map.fixtures.mobile.AnimalTracks;
 			return (BigDecimal) number;
 		} else {
 			return BigDecimal.valueOf(number.doubleValue());
+		}
+	}
+
+	private static Stream<?> flatten(@Nullable Object item) {
+		// Note that workers are counted separately, so while we include their equipment and mounts we don't include them.
+		if (item instanceof IWorker) {
+			return Stream.concat(Stream.concat(StreamSupport.stream(((IWorker) item).spliterator(), false),
+						((IWorker) item).getEquipment().stream()), Stream.of(((IWorker) item).getMount()))
+					.filter(Objects::nonNull);
+		} else if (item instanceof Iterable) {
+			return Stream.concat(StreamSupport.stream(((Iterable<?>) item).spliterator(), false).flatMap(CountingCLI::flatten),
+					Stream.of(item));
+		} else {
+			return Stream.of(item);
+		}
+	}
+
+	private static String resourcePileKeyExtractor(IResourcePile pile) {
+		if (pile.getContents().contains(pile.getQuantity().getUnits()) ||
+				    pile.getQuantity().getUnits().contains(pile.getContents())) {
+			return pile.getContents();
+		} else {
+			return pile.getQuantity().getUnits() + " " + pile.getContents();
 		}
 	}
 
@@ -203,7 +255,8 @@ import common.map.fixtures.mobile.AnimalTracks;
 				.map(entry -> String.format("- %d are %s", entry.getValue1(), entry.getValue0()))
 				.forEach(cli::println);
 		cli.println();
-		final List<TileFixture> allFixtures = map.streamAllFixtures().collect(Collectors.toList());
+		final List<IFixture> allFixtures = map.streamAllFixtures().flatMap(CountingCLI::flatten)
+				.filter(IFixture.class::isInstance).map(IFixture.class::cast).collect(Collectors.toList());
 		final MappedCounter<Forest, String, BigDecimal> forests = new MappedCounter<>(Forest::getKind,
 			f -> decimalize(f.getAcres()), DecimalAccumulator::new, BigDecimal.ZERO);
 		allFixtures.stream().filter(Forest.class::isInstance).map(Forest.class::cast)
@@ -239,7 +292,7 @@ import common.map.fixtures.mobile.AnimalTracks;
 			.forEach(ground::add);
 		printSummary(ground,
 			"Ground (bedrock) (counting exposed/not separately):",
-			CountingCLI::countTilesWithKind);
+			CountingCLI::countTilesWithKind, 0);
 
 		countSimply(StoneDeposit.class, allFixtures, "Stone deposits:", StoneDeposit::getKind);
 		countSimply(MineralVein.class, allFixtures, "Mineral veins:", MineralVein::getKind);
@@ -248,7 +301,7 @@ import common.map.fixtures.mobile.AnimalTracks;
 		final MappedCounter<CacheFixture, String, Integer> caches = simpleCounter(CacheFixture::getKind);
 		allFixtures.stream().filter(CacheFixture.class::isInstance).map(CacheFixture.class::cast)
 			.forEach(caches::add);
-		printSummary(caches, "Caches:", CountingCLI::countOfKind);
+		printSummary(caches, "Caches:", CountingCLI::countOfKind, 0);
 
 		final MappedCounter<AdventureFixture, String, Integer> adventures =
 			simpleCounter(AdventureFixture::getBriefDescription);
@@ -260,8 +313,9 @@ import common.map.fixtures.mobile.AnimalTracks;
 			.filter(Battlefield.class::isInstance).count());
 		adventures.addDirectly("Cave system", (int) allFixtures.stream()
 			.filter(Cave.class::isInstance).count());
-		printSummary(adventures, "Adventure Hooks and Portals:", CountingCLI::kindColonCount);
+		printSummary(adventures, "Adventure Hooks and Portals:", CountingCLI::kindColonCount, 0);
 
+		// TODO: We'd like to count active towns' populations.
 		cli.println("Active Communities:");
 		cli.println();
 		cli.println(String.format("- %d fortresses",
@@ -274,7 +328,7 @@ import common.map.fixtures.mobile.AnimalTracks;
 		final MappedCounter<Village, String, Integer> villages = simpleCounter(Village::getRace);
 		allFixtures.stream().filter(Village.class::isInstance).map(Village.class::cast)
 			.forEach(villages::add);
-		printSummary(villages, "- Villages, grouped by race:", CountingCLI::countSpaceKind);
+		printSummary(villages, "- Villages, grouped by race:", CountingCLI::countSpaceKind, 0);
 
 		final MappedCounter<AbstractTown, String, Integer> inactiveTowns = simpleCounter(
 			CountingCLI::townSummary);
@@ -308,20 +362,30 @@ import common.map.fixtures.mobile.AnimalTracks;
 			new MappedCounter<>(Animal::getKind, Animal::getPopulation, IntAccumulator::new, 0);
 		allFixtures.stream().filter(Animal.class::isInstance).map(Animal.class::cast)
 			.filter(a -> !a.isTalking()).forEach(animals::add);
-		allFixtures.stream().filter(IUnit.class::isInstance).map(IUnit.class::cast)
-			.flatMap(FixtureIterable::stream)
-			.filter(Animal.class::isInstance).map(Animal.class::cast).forEach(animals::add);
-		allFixtures.stream().filter(IFortress.class::isInstance).map(IFortress.class::cast)
-			.flatMap(FixtureIterable::stream)
-			.filter(IUnit.class::isInstance).map(IUnit.class::cast)
-			.flatMap(FixtureIterable::stream)
-			.filter(Animal.class::isInstance).map(Animal.class::cast).forEach(animals::add);
 		animals.addDirectly("various talking animals", (int) allFixtures.stream()
 			.filter(Animal.class::isInstance).map(Animal.class::cast).filter(Animal::isTalking)
 			.count());
 		printSummary(animals, "Animals");
 
-		final List<TileFixture> remaining = allFixtures.stream().filter(notA(Animal.class))
+		final MappedCounter<Implement, String, Integer> equipment = new MappedCounter<>(Implement::getKind,
+				Implement::getPopulation, IntAccumulator::new, 0);
+		allFixtures.stream().filter(Implement.class::isInstance).map(Implement.class::cast).forEach(equipment::add);
+
+		final Map<String, List<IResourcePile>> groupedResources = allFixtures.stream().filter(IResourcePile.class::isInstance)
+				.map(IResourcePile.class::cast).collect(Collectors.groupingBy(IResourcePile::getKind));
+
+		if (!groupedResources.isEmpty()) {
+			cli.println("Resources:");
+			for (Map.Entry<String, List<IResourcePile>> entry : groupedResources.entrySet()) {
+				MappedCounter<IResourcePile, String, BigDecimal> counter = new MappedCounter<>(
+						CountingCLI::resourcePileKeyExtractor, r -> decimalize(r.getQuantity().getNumber()),
+						DecimalAccumulator::new, BigDecimal.ZERO);
+				entry.getValue().forEach(counter::add);
+				printSummary(counter, entry.getKey(), 1);
+			}
+		}
+
+		final List<IFixture> remaining = allFixtures.stream().filter(notA(Animal.class))
 			.filter(notA(Shrub.class)).filter(notA(Grove.class)).filter(notA(Meadow.class))
 			.filter(notA(Immortal.class)).filter(notA(IFortress.class)).filter(notA(IUnit.class))
 			.filter(notA(AbstractTown.class)).filter(notA(Village.class))
@@ -330,14 +394,19 @@ import common.map.fixtures.mobile.AnimalTracks;
 			.filter(notA(MineralVein.class)).filter(notA(StoneDeposit.class))
 			.filter(notA(Ground.class)).filter(notA(Forest.class)).filter(notA(Hill.class))
 			.filter(notA(Oasis.class)).filter(notA(AnimalTracks.class)).filter(notA(Cave.class))
-			.filter(notA(Battlefield.class)).collect(Collectors.toList());
+			.filter(notA(Battlefield.class)).filter(notA(Implement.class)).filter(notA(IResourcePile.class))
+			.collect(Collectors.toList());
 
 		if (!remaining.isEmpty()) {
 			cli.println();
 			cli.println("Remaining fixtures:");
 			cli.println();
-			for (final TileFixture fixture : remaining) {
-				cli.println(String.format("- %s", fixture.getShortDescription()));
+			for (final IFixture fixture : remaining) {
+				if (fixture instanceof TileFixture) {
+					cli.println(String.format("- %s", ((TileFixture) fixture).getShortDescription())); // TODO: Move getShortDescription up to IFixture?
+				} else {
+					cli.println(String.format("- %s", fixture.toString()));
+				}
 			}
 		}
 	}
