2f87a8ad5d45d71f46f9c5f88f6d1986273a4c9f
Jonathan Lovelace <kingjon3377@gmail.com>
2022-02-27 14:04:29 -0500

    AbstractTurnApplet#getFoodFor: Actually check owner.

--- main/src/main/java/drivers/turnrunning/applets/AbstractTurnApplet.java
+++ main/src/main/java/drivers/turnrunning/applets/AbstractTurnApplet.java
@@ -117,7 +117,7 @@ public abstract class AbstractTurnApplet implements TurnApplet {
 			for (final TileFixture fix : map.getFixtures(loc)) {
 				if (fix instanceof IFortress) {
 					for (final FortressMember member : (IFortress) fix) {
-						if (member instanceof IResourcePile) {
+						if (member instanceof IResourcePile && ((IFortress) fix).getOwner().equals(player)) {
 							final IResourcePile pile = (IResourcePile) member;
 							if ("food".equals(pile.getKind()) &&
 									"pounds".equals(pile.getQuantity()
@@ -125,7 +125,7 @@ public abstract class AbstractTurnApplet implements TurnApplet {
 									pile.getCreated() <= turn) {
 								retval.add(pile);
 							}
-						} else if (member instanceof IUnit) {
+						} else if (member instanceof IUnit && ((IUnit) member).getOwner().equals(player)) {
 							for (final UnitMember inner : (IUnit) member) {
 								if (inner instanceof IResourcePile) {
 									final IResourcePile pile =
@@ -142,7 +142,7 @@ public abstract class AbstractTurnApplet implements TurnApplet {
 							}
 						}
 					}
-				} else if (fix instanceof IUnit) {
+				} else if (fix instanceof IUnit && ((IUnit) fix).getOwner().equals(player)) {
 					for (final UnitMember inner : (IUnit) fix) {
 						if (inner instanceof IResourcePile) {
 							final IResourcePile pile = (IResourcePile) inner;
