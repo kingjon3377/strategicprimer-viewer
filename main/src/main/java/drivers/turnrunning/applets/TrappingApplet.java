f3cac9c3ccf04432f7b43cebf7bc2ea04e5d745c
Jonathan Lovelace <kingjon3377@gmail.com>
2023-10-08 22:01:26 -0400

    TrappiingApplet: Remove redundant casts.

--- main/src/main/java/drivers/turnrunning/applets/TrappingApplet.java
+++ main/src/main/java/drivers/turnrunning/applets/TrappingApplet.java
@@ -109,7 +109,8 @@ import org.jetbrains.annotations.Nullable;
 		}
 		int time = startingTime;
 		while (time > 0) {
-			final TrapperCommand command = ((Pair<Integer, ? extends @Nullable TrapperCommand>) cli.chooseFromList((List<? extends TrapperCommand>) trapperCommands, prompt, "Oops! No commands", "Next action: ", ICLIHelper.ListChoiceBehavior.ALWAYS_PROMPT)).getValue1();
+			final TrapperCommand command = cli.chooseFromList(trapperCommands, prompt,
+				"Oops! No commands", "Next action: ", ICLIHelper.ListChoiceBehavior.ALWAYS_PROMPT).getValue1();
 			if (command == null || TrapperCommand.Quit == command) {
 				break;
 			}
