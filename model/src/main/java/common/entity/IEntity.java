0615422b03ef386b761a64dc0da19b57df4ddc2c
Jonathan Lovelace <kingjon3377@gmail.com>
2024-01-01 11:01:07 -0500

    IEntity: Add documentation comment.

--- model/src/main/java/common/entity/IEntity.java
+++ model/src/main/java/common/entity/IEntity.java
@@ -7,6 +7,9 @@ import org.jetbrains.annotations.Nullable;
  * An interface for game-world entities.
  *
  * TODO: Add EntityListener support, that setters in implementations will notify when properties are changed.
+ *
+ * TODO: Maybe add a currentTick field, so the engine can find the entity with the earliest "current time" and process
+ * its orders next.
  */
 public interface IEntity {
 	@NotNull Location getLocation();
