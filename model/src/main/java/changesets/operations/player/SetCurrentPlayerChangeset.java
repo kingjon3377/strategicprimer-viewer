12b8eabbb9587dc2d2a9f692c585e6981849b9b5
Jonathan Lovelace <kingjon3377@gmail.com>
2024-01-11 21:52:23 -0500

    SetCurrentPlayerChangeset: Fix too-long lines.

--- model/src/main/java/changesets/operations/player/SetCurrentPlayerChangeset.java
+++ model/src/main/java/changesets/operations/player/SetCurrentPlayerChangeset.java
@@ -36,7 +36,8 @@ public class SetCurrentPlayerChangeset implements Changeset {
                 if (player.current()) {
                     matchingOld = player;
                 } else {
-                    throw new PreconditionFailureException("Can't change current player when 'old current' player isn't current");
+                    throw new PreconditionFailureException(
+						"Can't change current player when 'old current' player isn't current");
                 }
             } else if (player.current() && player.playerId() != oldCurrent.playerId()) {
                 throw new PreconditionFailureException("Can't change current player when unexpected player is current");
@@ -60,12 +61,14 @@ public class SetCurrentPlayerChangeset implements Changeset {
 		final @NotNull String name1 = matchingOld.getName();
 		final @Nullable String country1 = matchingOld.country();
 		final @NotNull String portrait1 = matchingOld.portrait();
-		final Player matchingOldCopy = new PlayerImpl(playerId1, name1, Objects.requireNonNullElse(country1, ""), false, portrait1);
+		final Player matchingOldCopy = new PlayerImpl(playerId1, name1, Objects.requireNonNullElse(country1, ""), false,
+			portrait1);
 		final int playerId = matchingNew.playerId();
 		final @NotNull String name = matchingNew.getName();
 		final @Nullable String country = matchingNew.country();
 		final @NotNull String portrait = matchingNew.portrait();
-		final Player matchingNewCopy = new PlayerImpl(playerId, name, Objects.requireNonNullElse(country, ""), true, portrait);
+		final Player matchingNewCopy = new PlayerImpl(playerId, name, Objects.requireNonNullElse(country, ""), true,
+			portrait);
 		// TODO: try-catch here, in case the first operation fails?
 		map.replacePlayer(matchingOld, matchingOldCopy);
 		map.replacePlayer(matchingNew, matchingNewCopy);
