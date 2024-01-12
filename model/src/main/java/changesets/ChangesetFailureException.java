e0dfd71f8f2c0bfd5fade85e33a75d6a44aff726
Jonathan Lovelace <kingjon3377@gmail.com>
2024-01-11 21:46:28 -0500

    Add serialVersionUID to ChangesetFailureException, PreconditionFailureException

--- model/src/main/java/changesets/ChangesetFailureException.java
+++ model/src/main/java/changesets/ChangesetFailureException.java
@@ -2,10 +2,14 @@ package changesets;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serial;
+
 /**
  * An exception class to represent failures to apply a changeset.
  */
 public abstract class ChangesetFailureException extends Exception {
+	@Serial
+	private static final long serialVersionUID = 1L;
 	protected ChangesetFailureException(final @NotNull String message, final @NotNull Throwable cause) {
 		super(message, cause);
 	}
--- model/src/main/java/changesets/PreconditionFailureException.java
+++ model/src/main/java/changesets/PreconditionFailureException.java
@@ -2,12 +2,15 @@ package changesets;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.Serial;
 import java.util.List;
 
 /**
  * An exception for when a changeset cannot be applied because its preconditions are not met.
  */
 public class PreconditionFailureException extends ChangesetFailureException {
+	@Serial
+	private static final long serialVersionUID = 1L;
 	public PreconditionFailureException(final @NotNull String message, final @NotNull String... failedPreconditions) {
 		super(message);
 		this.failedPreconditions = List.of(failedPreconditions);
