e4bfdf543ff7a40a6665b01593e13093236f5377
Jonathan Lovelace <kingjon3377@gmail.com>
2024-01-13 17:45:41 -0500

    Add EntityProperty implementation class.

--- model/src/main/java/common/entity/EntityProperty.java
+++ model/src/main/java/common/entity/EntityProperty.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 
 /**
  * An interface for strongly-typed entity properties. Basically an object and its type, for use in serialization, but
- * for ease of use also including the property name.
+ * for ease of use also including the property name. TODO: Convert to record, combining with PropertyImpl?
  */
 public interface EntityProperty<T> {
 	@NotNull String getPropertyName();
