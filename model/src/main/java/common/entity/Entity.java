227e2bd93f13379b91d52a7cf3b915142ffb0988
Jonathan Lovelace <kingjon3377@gmail.com>
2024-04-09 21:32:31 -0400

    Entity: Declare class final; remove final from methods.

--- model/src/main/java/common/entity/Entity.java
+++ model/src/main/java/common/entity/Entity.java
@@ -8,7 +8,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Entity implements IMutableEntity {
+public final class Entity implements IMutableEntity {
 	public Entity(final @NotNull EntityIdentifier id, final @NotNull Location location, final @NotNull String type) {
 		this.location = location;
 		this.id = id;
@@ -24,55 +24,55 @@ public class Entity implements IMutableEntity {
 	private final Map<String, EntityProperty<?>> properties = new HashMap<>();
 
 	@Override
-	public final @NotNull Location getLocation() {
+	public @NotNull Location getLocation() {
 		return location;
 	}
 
-	public final void setLocation(final @NotNull Location location) {
+	public void setLocation(final @NotNull Location location) {
 		this.location = location;
 	}
 
 	@Override
-	public final @NotNull EntityIdentifier getId() {
+	public @NotNull EntityIdentifier getId() {
 		return id;
 	}
 
 	@Override
-	public final @NotNull String getType() {
+	public @NotNull String getType() {
 		return type;
 	}
 
 	@Override
-	public final boolean hasProperty(final @NotNull String propertyName) {
+	public boolean hasProperty(final @NotNull String propertyName) {
 		return properties.containsKey(propertyName);
 	}
 
 	@Override
-	public final @Nullable EntityProperty<?> getProperty(final @NotNull String propertyName) {
+	public @Nullable EntityProperty<?> getProperty(final @NotNull String propertyName) {
 		return properties.get(propertyName);
 	}
 
-	public final void setProperty(final @NotNull EntityProperty<?> property) {
+	public void setProperty(final @NotNull EntityProperty<?> property) {
 		properties.put(property.propertyName(), property);
 	}
 
-	public final void removeProperty(final @NotNull String propertyName) {
+	public void removeProperty(final @NotNull String propertyName) {
 		properties.remove(propertyName);
 	}
 
-	public final void removeProperty(final @NotNull EntityProperty<?> property) {
+	public void removeProperty(final @NotNull EntityProperty<?> property) {
 		properties.remove(property.propertyName(), property);
 	}
 
 	@Override
-	public final @NotNull Entity copy() {
+	public @NotNull Entity copy() {
 		final Entity retval = new Entity(id, location, type);
 		properties.values().forEach(retval::setProperty);
 		return retval;
 	}
 
 	@Override
-	public final @NotNull Collection<EntityProperty<?>> getAllProperties() {
+	public @NotNull Collection<EntityProperty<?>> getAllProperties() {
 		return Collections.unmodifiableCollection(properties.values());
 	}
 
