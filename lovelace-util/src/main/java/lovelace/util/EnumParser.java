9ecbb23a8a02733e175ff24ce8f053a3878d91e9
Jonathan Lovelace <kingjon3377@gmail.com>
2025-09-12 20:23:24 -0400

    Add EnumParser class
    To ease implementation of parse() method of enum classes.

---
+++ lovelace-util/src/main/java/lovelace/util/EnumParser.java
@@ -0,0 +1,38 @@
+package lovelace.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A class to parse enum values from their corresponding toString values.
+ * @param <T> the enum type.
+ */
+public final class EnumParser<T extends Enum<T>> implements ThrowingFunction<String, T, IllegalArgumentException> {
+	private final String string;
+	private final Map<String, T> cache;
+
+	public EnumParser(final Class<T> classType, final int enumCount) {
+		string = "EnumParser<%s>".formatted(classType.getSimpleName());
+		cache = new HashMap<>(enumCount);
+		for (final T item : classType.getEnumConstants()) {
+			if (cache.containsKey(item.toString())) {
+				throw new IllegalStateException("EnumParser used for type with non-unique toString");
+			}
+			cache.put(item.toString(), item);
+		}
+	}
+
+	@Override
+	public T apply(final String input) throws IllegalArgumentException {
+		final T retval = cache.get(input);
+		if (retval == null) {
+			throw new IllegalArgumentException("Failed to parse value from " + input);
+		}
+		return retval;
+	}
+
+	@Override
+	public String toString() {
+		return string;
+	}
+}
